e72f7229e06aaaad116bd638f9f507e8879b3da2
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2020-10-19 17:35:50 -0400

    [] removing unneeded template transformation

--- docker/database/migrations/config/V1_2_0__add_mdi_config_setting_tables_and_schema.sql
+++ docker/database/migrations/config/V1_2_0__add_mdi_config_setting_tables_and_schema.sql
@@ -17,141 +17,141 @@
 CREATE SCHEMA mdi;
 
 
-create table mdi.process
+CREATE TABLE mdi.process
 (
-	pr_process_name text not null
-		constraint process_pk
-			primary key,
-	pr_sp_name text,
-	pr_description text,
-	pr_last_updated timestamp with time zone default now() not null
+	pr_process_name TEXT NOT NULL
+		CONSTRAINT process_pk
+			PRIMARY KEY,
+	pr_sp_name TEXT,
+	pr_description TEXT,
+	pr_last_updated TIMESTAMP WITH TIME ZONE DEFAULT now( ) NOT NULL
 );
 
 
 
-create unique index process_pr_process_name_uindex
-	on mdi.process (pr_process_name);
+CREATE UNIQUE INDEX process_pr_process_name_uindex
+	ON mdi.process (pr_process_name);
 
-create table mdi.step_metadata_csv_file_input
+CREATE TABLE mdi.step_metadata_csv_file_input
 (
-	smcfi_step_definition_id bigserial not null
-		constraint step_csv_file_input_pk
-			primary key,
-	smcfi_filename text,
-	smcfi_header_present text,
-	smcfi_csv_delimiter text,
-	smcfi_enclosure text,
-	smcfi_buffersize integer,
-	smcfi_lazy_conversion text,
-	smcfi_newline_possible text,
-	smcfi_add_filename_result text,
-	smcfi_file_format text,
-	smcfi_file_encoding text,
-	smcfi_include_filename text,
-	smcfi_process_in_parallel text,
-	smcfi_process_name text,
-	smcfi_filename_field text,
-	smcfi_row_num_field text
+	smcfi_step_definition_id BIGSERIAL NOT NULL
+		CONSTRAINT step_csv_file_input_pk
+			PRIMARY KEY,
+	smcfi_filename TEXT,
+	smcfi_header_present TEXT,
+	smcfi_csv_delimiter TEXT,
+	smcfi_enclosure TEXT,
+	smcfi_buffersize INTEGER,
+	smcfi_lazy_conversion TEXT,
+	smcfi_newline_possible TEXT,
+	smcfi_add_filename_result TEXT,
+	smcfi_file_format TEXT,
+	smcfi_file_encoding TEXT,
+	smcfi_include_filename TEXT,
+	smcfi_process_in_parallel TEXT,
+	smcfi_process_name TEXT,
+	smcfi_filename_field TEXT,
+	smcfi_row_num_field TEXT
 );
 
 
 
-create table mdi.row_metadata_csv_file_input
+CREATE TABLE mdi.row_metadata_csv_file_input
 (
-	rmcfi_row_definition_id bigserial not null
-		constraint step_data_csv_file_input_pk
-			primary key,
-	rmcfi_field_name text,
-	rmcfi_field_type text,
-	rmcfi_field_format text,
-	rmcfi_field_length text,
-	rmcfi_field_precision text,
-	rmcfi_field_currency text,
-	rmcfi_field_decimal text,
-	rmcfi_field_group text,
-	rmcfi_field_trim_type text,
-	rmcfi_step_definition_id bigserial not null,
-	rmcfi_field_order numeric not null
+	rmcfi_row_definition_id BIGSERIAL NOT NULL
+		CONSTRAINT step_data_csv_file_input_pk
+			PRIMARY KEY,
+	rmcfi_field_name TEXT,
+	rmcfi_field_type TEXT,
+	rmcfi_field_format TEXT,
+	rmcfi_field_length TEXT,
+	rmcfi_field_precision TEXT,
+	rmcfi_field_currency TEXT,
+	rmcfi_field_decimal TEXT,
+	rmcfi_field_group TEXT,
+	rmcfi_field_trim_type TEXT,
+	rmcfi_step_definition_id BIGSERIAL NOT NULL,
+	rmcfi_field_order NUMERIC NOT NULL
 );
 
 
 
-create table mdi.step_metadata_table_output
+CREATE TABLE mdi.step_metadata_table_output
 (
-	smto_step_definition_id bigserial not null
-		constraint step_definition_table_output_pk
-			primary key,
-	smto_target_schema text,
-	smto_target_table text,
-	smto_commit_size text,
-	smto_partitioning_field text,
-	smto_table_name_field text,
-	smto_auto_generated_key_field text,
-	smto_partition_data_per text,
-	smto_table_name_defined_in_field text,
-	smto_return_auto_generated_key_field text,
-	smto_truncate_table text,
-	smto_connectionname text,
-	smto_partition_over_tables text,
-	smto_specify_database_fields text,
-	smto_ignore_insert_errors text,
-	smto_use_batch_update text,
-	smto_process_name text
+	smto_step_definition_id BIGSERIAL NOT NULL
+		CONSTRAINT step_definition_table_output_pk
+			PRIMARY KEY,
+	smto_target_schema TEXT,
+	smto_target_table TEXT,
+	smto_commit_size TEXT,
+	smto_partitioning_field TEXT,
+	smto_table_name_field TEXT,
+	smto_auto_generated_key_field TEXT,
+	smto_partition_data_per TEXT,
+	smto_table_name_defined_in_field TEXT,
+	smto_return_auto_generated_key_field TEXT,
+	smto_truncate_table TEXT,
+	smto_connectionname TEXT,
+	smto_partition_over_tables TEXT,
+	smto_specify_database_fields TEXT,
+	smto_ignore_insert_errors TEXT,
+	smto_use_batch_update TEXT,
+	smto_process_name TEXT
 );
 
 
 
-create table mdi.row_metadata_table_output
+CREATE TABLE mdi.row_metadata_table_output
 (
-	rmto_row_definition_id bigserial not null
-		constraint step_data_table_output_pk
-			primary key,
-	rmto_field_name_in_db text,
-	rmto_field_name_in_stream text,
-	rmto_step_definition_id bigint,
-	rmto_field_order numeric not null
+	rmto_row_definition_id BIGSERIAL NOT NULL
+		CONSTRAINT step_data_table_output_pk
+			PRIMARY KEY,
+	rmto_field_name_in_db TEXT,
+	rmto_field_name_in_stream TEXT,
+	rmto_step_definition_id BIGINT,
+	rmto_field_order NUMERIC NOT NULL
 );
 
 
 
-create table mdi.step_metadata_excel_input
+CREATE TABLE mdi.step_metadata_excel_input
 (
-	smei_step_id bigserial not null
-		constraint step_metadata_excel_input_pk
-			primary key,
-	smei_spreadsheet_type text,
-	smei_process_name text not null,
-	smei_filename text,
-	smei_filemask text,
-	smei_exclude_filemask text,
-	smei_file_required text,
-	smei_include_subfolders text,
-	smei_sheet_name text,
-	smei_sheet_start_row integer,
-	smei_sheet_start_col integer
+	smei_step_id BIGSERIAL NOT NULL
+		CONSTRAINT step_metadata_excel_input_pk
+			PRIMARY KEY,
+	smei_spreadsheet_type TEXT,
+	smei_process_name TEXT NOT NULL,
+	smei_filename TEXT,
+	smei_filemask TEXT,
+	smei_exclude_filemask TEXT,
+	smei_file_required TEXT,
+	smei_include_subfolders TEXT,
+	smei_sheet_name TEXT,
+	smei_sheet_start_row INTEGER,
+	smei_sheet_start_col INTEGER
 );
 
 
 
-create unique index step_metadata_excel_input_smei_step_id_uindex
-	on mdi.step_metadata_excel_input (smei_step_id);
+CREATE UNIQUE INDEX step_metadata_excel_input_smei_step_id_uindex
+	ON mdi.step_metadata_excel_input (smei_step_id);
 
-create table mdi.row_metadata_excel_input
+CREATE TABLE mdi.row_metadata_excel_input
 (
-	rmei_row_definition_id bigserial not null
-		constraint row_metadata_excel_input_pk
-			primary key,
-	rmei_field_name text,
-	rmei_field_type text,
-	rmei_field_format text,
-	rmei_field_length text,
-	rmei_field_precision text,
-	rmei_field_currency text,
-	rmei_field_decimal text,
-	rmei_field_group text,
-	rmei_field_trim_type text,
-	rmei_step_definition_id bigserial not null,
-	rmei_field_order numeric
+	rmei_row_definition_id BIGSERIAL NOT NULL
+		CONSTRAINT row_metadata_excel_input_pk
+			PRIMARY KEY,
+	rmei_field_name TEXT,
+	rmei_field_type TEXT,
+	rmei_field_format TEXT,
+	rmei_field_length TEXT,
+	rmei_field_precision TEXT,
+	rmei_field_currency TEXT,
+	rmei_field_decimal TEXT,
+	rmei_field_group TEXT,
+	rmei_field_trim_type TEXT,
+	rmei_step_definition_id BIGSERIAL NOT NULL,
+	rmei_field_order NUMERIC
 );
 
 
