2062bfa332fb097216b1059b5d775077f123c869
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2020-10-20 15:25:47 -0400

    [#1198065847802233] added csv file input auto and manual config checks

--- pentaho/mdi/configuration_checks/automatic/table_output_auto_checks.sql
+++ pentaho/mdi/configuration_checks/automatic/table_output_auto_checks.sql
@@ -28,3 +28,74 @@ WHERE
 	-- 'ERROR: if smto_table_name_defined_in_field=Y then smto_table_name_field should not be blank'
 	OR (smto.smto_table_name_defined_in_field = 'Y' AND (smto.smto_table_name_field != '' OR smto.smto_table_name_field IS NOT NULL))
 ;
+
+
+
+
+--
+-- Verify all rows in row_metadata_csv_file_input are configured correctly based on the data type of the row compared to what else is configured in the table
+--
+SELECT
+	'mdi.row_metadata_csv_file_input' AS "Misconfigured Table Name"
+	, smcfi.smcfi_process_name as "Misconfigured Table Name"
+	, smcfi.smcfi_step_definition_id as "Step Definition ID"
+	, rmcfi.rmcfi_field_name as "Misconfigured Field Name"
+	, rmcfi.rmcfi_field_type as "Misconfigured Field Type"
+	, 'ERROR: The settings in row_metadata_csv_file_input.rmcfi_field_name=''' || rmcfi.rmcfi_field_name || ''' are not correct for process ''' || smcfi.smcfi_process_name || '''' AS "Error Message"
+
+FROM
+	mdi.row_metadata_csv_file_input AS rmcfi
+		JOIN mdi.step_metadata_csv_file_input AS smcfi
+		ON smcfi.smcfi_step_definition_id = rmcfi.rmcfi_step_definition_id
+WHERE
+	(rmcfi.rmcfi_field_type NOT IN ('String', 'Integer', 'Date', 'Boolean', 'Number', 'Timestamp', 'BigNumber', 'Boolean'))
+	OR
+	-- if the row is a String, then other columns should be configured with standard values
+	(
+				rmcfi.rmcfi_field_type = 'String' AND (
+				rmcfi.rmcfi_field_format IS NOT NULL
+				OR rmcfi.rmcfi_field_length IS NOT NULL -- don't limit the length of strings that are loaded from a csv
+				OR rmcfi.rmcfi_field_precision IS NOT NULL
+				OR rmcfi.rmcfi_field_currency != '$'
+				OR rmcfi.rmcfi_field_decimal != '.'
+				OR rmcfi.rmcfi_field_group != ','
+				-- OR rmcfi.rmcfi_field_trim_type != 'none' -- we do not want to trim raw input data
+				OR rmcfi.rmcfi_field_trim_type != 'both' -- TODO: remove this line and uncomment line above before moving to prod
+			)
+		)
+	OR
+	-- if the row is a Integer, then other columns should be configured with standard values
+	(
+				rmcfi.rmcfi_field_type = 'Integer' AND (
+				rmcfi.rmcfi_field_format IS NOT NULL
+				OR rmcfi.rmcfi_field_decimal != '.'
+				OR rmcfi.rmcfi_field_group != ','
+				OR rmcfi.rmcfi_field_trim_type != 'none' -- we do not want to trim raw input data
+			)
+		)
+	OR
+	-- if the row is a Number, then other columns should be configured with standard values
+	(
+				rmcfi.rmcfi_field_type = 'Number' AND (
+					rmcfi.rmcfi_field_decimal != '.'
+				OR rmcfi.rmcfi_field_group != ','
+				OR rmcfi.rmcfi_field_trim_type != 'none' -- we do not want to trim raw input data
+			)
+		)
+	OR
+	-- if the row is a Date, then other columns should be configured with standard values
+	(
+				rmcfi.rmcfi_field_type = 'Date' AND (
+				rmcfi.rmcfi_field_format IS NULL
+				OR rmcfi.rmcfi_field_length IS NOT NULL
+				OR rmcfi.rmcfi_field_precision IS NOT NULL
+				OR rmcfi.rmcfi_field_currency != '$'
+				OR rmcfi.rmcfi_field_decimal != '.'
+				OR rmcfi.rmcfi_field_group != ','
+				OR rmcfi.rmcfi_field_trim_type != 'none' -- we do not want to trim raw input data
+			)
+		)
+ORDER BY
+	smcfi.smcfi_process_name DESC
+	, rmcfi_field_order DESC
+;
