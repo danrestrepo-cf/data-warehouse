9ab88c6dfb460a828288ea280362beb5805abbf1
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2020-10-22 11:43:26 -0400

    [#1198065847802233] added sql file for mdi schema

--- docker/database/migrations/config/V1_2_0__add_mdi_config_setting_tables_and_schema.sql
+++ docker/database/migrations/config/V1_2_0__add_mdi_config_setting_tables_and_schema.sql
@@ -17,142 +17,165 @@
 CREATE SCHEMA mdi;
 
 
-CREATE TABLE mdi.process
+--
+-- Types: create domain values for configuration settings
+--
+CREATE TYPE PENTAHO_FIELD_TYPE AS ENUM ('String'
+    , 'Date', 'Integer', 'Number', 'BigNumber', 'Binary', 'Timestamp', 'Internet Address');
+CREATE TYPE PENTAHO_FIELD_CURRENCY AS ENUM ('$');
+CREATE TYPE PENTAHO_FIELD_DECIMAL AS ENUM ('.');
+CREATE TYPE PENTAHO_TRIM_TYPE AS ENUM ('both', 'right', 'left', 'none');
+CREATE TYPE PENTAHO_Y_OR_N AS ENUM ('Y', 'N');
+CREATE TYPE PENTAHO_SPREADSHEET_TYPE AS ENUM ('JXL', 'POI', 'SAX_POI', 'ODS');
+CREATE TYPE PENTAHO_FIELD_FORMAT AS ENUM ('mixed', 'DOS', 'Unix');
+CREATE TYPE PENTAHO_DB_CONNECTION_NAME AS ENUM ('Ingress DB Connection', 'Config DB Connection', 'Staging DB Connection');
+
+--
+-- Tables: create tables needed for mdi process
+--
+CREATE TABLE process
 (
-	pr_process_name TEXT NOT NULL
+	process_name TEXT NOT NULL
 		CONSTRAINT process_pk
 			PRIMARY KEY,
-	pr_sp_name TEXT,
-	pr_description TEXT,
-	pr_last_updated TIMESTAMP WITH TIME ZONE DEFAULT now( ) NOT NULL
+	description TEXT NOT NULL
 );
 
-
-
-CREATE UNIQUE INDEX process_pr_process_name_uindex
-	ON mdi.process (pr_process_name);
-
-CREATE TABLE mdi.step_metadata_csv_file_input
+CREATE TABLE csv_file_input_step
 (
-	smcfi_step_definition_id BIGSERIAL NOT NULL
-		CONSTRAINT step_csv_file_input_pk
+	process_name TEXT NOT NULL
+		CONSTRAINT csv_file_input_step_process_process_name_fk
+			REFERENCES process
+			ON UPDATE CASCADE
+			ON DELETE CASCADE
+			DEFERRABLE INITIALLY DEFERRED,
+	step_id BIGSERIAL NOT NULL
+		CONSTRAINT csv_file_input_step_pk
 			PRIMARY KEY,
-	smcfi_filename TEXT,
-	smcfi_header_present TEXT,
-	smcfi_csv_delimiter TEXT,
-	smcfi_enclosure TEXT,
-	smcfi_buffersize INTEGER,
-	smcfi_lazy_conversion TEXT,
-	smcfi_newline_possible TEXT,
-	smcfi_add_filename_result TEXT,
-	smcfi_file_format TEXT,
-	smcfi_file_encoding TEXT,
-	smcfi_include_filename TEXT,
-	smcfi_process_in_parallel TEXT,
-	smcfi_process_name TEXT,
-	smcfi_filename_field TEXT,
-	smcfi_row_num_field TEXT
+	filename TEXT NOT NULL,
+	header_present mdi.PENTAHO_Y_OR_N NOT NULL,
+	csv_delimiter CHAR NOT NULL,
+	enclosure CHAR NOT NULL,
+	buffersize INTEGER NOT NULL,
+	lazy_conversion mdi.PENTAHO_Y_OR_N NOT NULL,
+	newline_possible mdi.PENTAHO_Y_OR_N NOT NULL,
+	add_filename_result mdi.PENTAHO_Y_OR_N NOT NULL,
+	file_format mdi.PENTAHO_FIELD_FORMAT NOT NULL,
+	file_encoding TEXT NOT NULL,
+	include_filename mdi.PENTAHO_Y_OR_N NOT NULL,
+	process_in_parallel mdi.PENTAHO_Y_OR_N NOT NULL,
+	filename_field TEXT,
+	row_num_field TEXT
 );
 
-
-
-CREATE TABLE mdi.row_metadata_csv_file_input
+CREATE TABLE csv_file_input_field
 (
-	rmcfi_row_definition_id BIGSERIAL NOT NULL
-		CONSTRAINT step_data_csv_file_input_pk
+	step_id BIGINT NOT NULL
+		CONSTRAINT csv_file_input_field_csv_file_input_step_step_id_fk
+			REFERENCES csv_file_input_step
+			ON UPDATE CASCADE
+			ON DELETE CASCADE
+			DEFERRABLE INITIALLY DEFERRED,
+	row_id BIGSERIAL NOT NULL
+		CONSTRAINT csv_file_input_field_pk
 			PRIMARY KEY,
-	rmcfi_field_name TEXT,
-	rmcfi_field_type TEXT,
-	rmcfi_field_format TEXT,
-	rmcfi_field_length TEXT,
-	rmcfi_field_precision TEXT,
-	rmcfi_field_currency TEXT,
-	rmcfi_field_decimal TEXT,
-	rmcfi_field_group TEXT,
-	rmcfi_field_trim_type TEXT,
-	rmcfi_step_definition_id BIGSERIAL NOT NULL,
-	rmcfi_field_order NUMERIC NOT NULL
+	field_name TEXT NOT NULL,
+	field_type mdi.PENTAHO_FIELD_TYPE NOT NULL,
+	field_format TEXT,
+	field_length INTEGER,
+	field_precision INTEGER,
+	field_currency mdi.PENTAHO_FIELD_CURRENCY,
+	field_decimal mdi.PENTAHO_FIELD_DECIMAL,
+	field_group TEXT,
+	field_trim_type mdi.PENTAHO_TRIM_TYPE,
+	field_order NUMERIC NOT NULL
 );
 
-
-
-CREATE TABLE mdi.step_metadata_table_output
+CREATE TABLE excel_file_input_step
 (
-	smto_step_definition_id BIGSERIAL NOT NULL
-		CONSTRAINT step_definition_table_output_pk
+	process_name TEXT NOT NULL
+		CONSTRAINT excel_input_step_process_process_name_fk
+			REFERENCES process
+			ON UPDATE CASCADE
+			ON DELETE CASCADE
+			DEFERRABLE INITIALLY DEFERRED,
+	step_id BIGINT NOT NULL
+		CONSTRAINT excel_input_step_pk
 			PRIMARY KEY,
-	smto_target_schema TEXT,
-	smto_target_table TEXT,
-	smto_commit_size TEXT,
-	smto_partitioning_field TEXT,
-	smto_table_name_field TEXT,
-	smto_auto_generated_key_field TEXT,
-	smto_partition_data_per TEXT,
-	smto_table_name_defined_in_field TEXT,
-	smto_return_auto_generated_key_field TEXT,
-	smto_truncate_table TEXT,
-	smto_connectionname TEXT,
-	smto_partition_over_tables TEXT,
-	smto_specify_database_fields TEXT,
-	smto_ignore_insert_errors TEXT,
-	smto_use_batch_update TEXT,
-	smto_process_name TEXT
+	spreadsheet_type mdi.PENTAHO_SPREADSHEET_TYPE NOT NULL,
+	filename TEXT NOT NULL,
+	filemask TEXT,
+	exclude_filemask TEXT,
+	file_required mdi.PENTAHO_Y_OR_N NOT NULL,
+	include_subfolders mdi.PENTAHO_Y_OR_N NOT NULL,
+	sheet_name TEXT NOT NULL,
+	sheet_start_row INTEGER NOT NULL,
+	sheet_start_col INTEGER NOT NULL
 );
 
-
-
-CREATE TABLE mdi.row_metadata_table_output
+CREATE TABLE excel_file_input_field
 (
-	rmto_row_definition_id BIGSERIAL NOT NULL
-		CONSTRAINT step_data_table_output_pk
+	step_id BIGINT NOT NULL
+		CONSTRAINT excel_file_input_field_excel_file_input_step_step_id_fk
+			REFERENCES excel_file_input_step
+			ON UPDATE CASCADE
+			ON DELETE CASCADE
+			DEFERRABLE INITIALLY DEFERRED,
+	row_id BIGSERIAL NOT NULL
+		CONSTRAINT excel_file_input_field_pk
 			PRIMARY KEY,
-	rmto_field_name_in_db TEXT,
-	rmto_field_name_in_stream TEXT,
-	rmto_step_definition_id BIGINT,
-	rmto_field_order NUMERIC NOT NULL
+	field_name TEXT NOT NULL,
+	field_type mdi.PENTAHO_FIELD_TYPE NOT NULL,
+	field_format TEXT,
+	field_length INTEGER,
+	field_precision INTEGER,
+	field_currency mdi.PENTAHO_FIELD_CURRENCY,
+	field_decimal mdi.PENTAHO_FIELD_DECIMAL,
+	field_group TEXT,
+	field_trim_type mdi.PENTAHO_TRIM_TYPE,
+	field_order NUMERIC NOT NULL
 );
 
-
-
-CREATE TABLE mdi.step_metadata_excel_input
+CREATE TABLE table_output_step
 (
-	smei_step_id BIGSERIAL NOT NULL
-		CONSTRAINT step_metadata_excel_input_pk
+	process_name TEXT
+		CONSTRAINT table_output_step_process_process_name_fk
+			REFERENCES process
+			ON UPDATE CASCADE
+			ON DELETE CASCADE
+			DEFERRABLE INITIALLY DEFERRED,
+	step_id BIGINT NOT NULL
+		CONSTRAINT table_output_step_pk
 			PRIMARY KEY,
-	smei_spreadsheet_type TEXT,
-	smei_process_name TEXT NOT NULL,
-	smei_filename TEXT,
-	smei_filemask TEXT,
-	smei_exclude_filemask TEXT,
-	smei_file_required TEXT,
-	smei_include_subfolders TEXT,
-	smei_sheet_name TEXT,
-	smei_sheet_start_row INTEGER,
-	smei_sheet_start_col INTEGER
+	target_schema TEXT NOT NULL,
+	target_table TEXT NOT NULL,
+	commit_size INTEGER NOT NULL,
+	partitioning_field TEXT,
+	table_name_field TEXT,
+	auto_generated_key_field TEXT,
+	partition_data_per TEXT,
+	table_name_defined_in_field mdi.PENTAHO_Y_OR_N NOT NULL,
+	return_auto_generated_key_field TEXT,
+	truncate_table mdi.PENTAHO_Y_OR_N NOT NULL,
+	connectionname TEXT NOT NULL,
+	partition_over_tables mdi.PENTAHO_Y_OR_N,
+	specify_database_fields mdi.PENTAHO_Y_OR_N NOT NULL,
+	ignore_insert_errors mdi.PENTAHO_Y_OR_N NOT NULL,
+	use_batch_update mdi.PENTAHO_Y_OR_N NOT NULL
 );
 
-
-
-CREATE UNIQUE INDEX step_metadata_excel_input_smei_step_id_uindex
-	ON mdi.step_metadata_excel_input (smei_step_id);
-
-CREATE TABLE mdi.row_metadata_excel_input
+CREATE TABLE table_output_field
 (
-	rmei_row_definition_id BIGSERIAL NOT NULL
-		CONSTRAINT row_metadata_excel_input_pk
+	step_id BIGINT NOT NULL
+		CONSTRAINT table_output_field_table_output_step_step_id_fk
+			REFERENCES table_output_step
+			ON UPDATE CASCADE
+			ON DELETE CASCADE
+			DEFERRABLE INITIALLY DEFERRED,
+	row_id BIGSERIAL NOT NULL
+		CONSTRAINT table_output_field_pk
 			PRIMARY KEY,
-	rmei_field_name TEXT,
-	rmei_field_type TEXT,
-	rmei_field_format TEXT,
-	rmei_field_length TEXT,
-	rmei_field_precision TEXT,
-	rmei_field_currency TEXT,
-	rmei_field_decimal TEXT,
-	rmei_field_group TEXT,
-	rmei_field_trim_type TEXT,
-	rmei_step_definition_id BIGSERIAL NOT NULL,
-	rmei_field_order NUMERIC
+	field_name_in_db TEXT NOT NULL,
+	field_name_in_stream TEXT NOT NULL,
+	field_order NUMERIC NOT NULL
 );
-
-
-
