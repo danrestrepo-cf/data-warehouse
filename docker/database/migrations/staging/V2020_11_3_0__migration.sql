8df90610bc0fdae4e82ccf9fa30de7af441e9573
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2020-11-17 11:45:05 -0500

    [#1193468447043569] updating migration script name

--- docker/database/migrations/staging/V2020_11_3_0__migration.sql
+++ docker/database/migrations/staging/V2020_11_3_0__migration.sql
@@ -1,4 +1,8 @@
+---
+--- Add Encompass MCR related fields to EDW: https://app.asana.com/0/0/1193468447043569
+---
 
+-- add fields to encompass_loan
 alter table loan.encompass_loan add el_closing_disclosure_sent_date timestamp;
 alter table loan.encompass_loan add el_closing_disclosure_received_date timestamp;
 alter table loan.encompass_loan add el_revised_closing_disclosure_sent_date timestamp;
