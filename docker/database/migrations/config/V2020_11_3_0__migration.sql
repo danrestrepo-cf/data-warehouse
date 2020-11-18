88ccecdc5948b89e96eb0eb7a3cb60df92accab4
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2020-11-18 17:06:21 -0500

    [#1199150384475333] correcting field text and IDs in sql

--- docker/database/migrations/config/V2020_11_3_0__migration.sql
+++ docker/database/migrations/config/V2020_11_3_0__migration.sql
@@ -22,7 +22,8 @@ create table mdi.table_input_step
 INSERT INTO mdi.process
 (dwid, name, description)
 VALUES
-(5, 'SP8.2', 'Transfer DMI NMLS Call Report data to staging db - state');
+(5, 'SP8.2', 'Transfer DMI NMLS Call Report data to staging db - state')
+;
 
 INSERT INTO mdi.table_input_step
 (dwid, process_dwid, sql, limit_size, execute_for_each_row, replace_variables_in_script, enable_lazy_conversion,
@@ -48,7 +49,7 @@ INSERT INTO mdi.table_output_step (dwid, process_dwid, target_schema, target_tab
                                    connectionname, partition_over_tables, specify_database_fields, ignore_insert_errors,
                                    use_batch_update)
 VALUES
-(5, 5, 'mcr', 'nmls_call_report_national', 1000, null, null, null, null, 'N', null, 'Y', 'Staging DB Connection', 'N',
+(5, 5, 'mcr', 'nmls_call_report_state', 1000, null, null, null, null, 'N', null, 'Y', 'Staging DB Connection', 'N',
  'Y', 'N', 'N')
 ;
 
@@ -57,12 +58,12 @@ INSERT INTO mdi.table_output_field
 VALUES
 (29, 5, 'mcr_code', 'mcr_code', 1)
 , (30, 5, 'mcr_desc', 'mcr_desc', 2)
-, (30, 5, 'state_type', 'state_type', 3)
-, (31, 5, 'unpaid_balance', 'unpaid_balance', 4)
-, (32, 5, 'loan_count', 'loan_count', 5)
-, (33, 5, 'avg_loan_size', 'avg_loan_size', 6)
-, (34, 5, 'etl_batch_id', 'etl_batch_id', 7)
-, (35, 5, 'data_source_dwid', 'data_source_dwid', 8)
+, (31, 5, 'state_type', 'state_type', 3)
+, (32, 5, 'unpaid_balance', 'unpaid_balance', 4)
+, (33, 5, 'loan_count', 'loan_count', 5)
+, (34, 5, 'avg_loan_size', 'avg_loan_size', 6)
+, (35, 5, 'etl_batch_id', 'etl_batch_id', 7)
+, (36, 5, 'data_source_dwid', 'data_source_dwid', 8)
 ;
 
 --
@@ -71,7 +72,8 @@ VALUES
 INSERT INTO mdi.process
     (dwid, name, description)
 VALUES
-    (4, 'SP9.2', 'Transfer DMI NMLS Call Report data to staging db - national');
+    (4, 'SP9.2', 'Transfer DMI NMLS Call Report data to staging db - national')
+;
 
 INSERT INTO mdi.table_input_step
 (dwid, process_dwid, sql, limit_size, execute_for_each_row, replace_variables_in_script, enable_lazy_conversion,
@@ -96,7 +98,8 @@ INSERT INTO mdi.table_output_step (dwid, process_dwid, target_schema, target_tab
     ignore_insert_errors, use_batch_update)
 VALUES
 (4, 4, 'mcr', 'nmls_call_report_national', 1000, null, null, null, null, 'N', null, 'Y', 'Staging DB Connection',
-  'N', 'Y', 'N', 'N');                                                                                                                                                                                                                                                                                                                                                                          (4, 4, 'mcr', 'nmls_call_report_national', 1000, null, null, null, null, 'N', null, 'Y', 'Staging DB Connection', 'N', 'Y', 'N', 'N');
+  'N', 'Y', 'N', 'N')
+;                                                                                                                                                                                                                                                                                                                                                                          (4, 4, 'mcr', 'nmls_call_report_national', 1000, null, null, null, null, 'N', null, 'Y', 'Staging DB Connection', 'N', 'Y', 'N', 'N');
 
 INSERT INTO mdi.table_output_field
     (dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order)
@@ -116,7 +119,8 @@ VALUES
 INSERT INTO mdi.process
 (dwid, name, description)
 VALUES
-(6, 'SP8.2', 'Transfer DMI NMLS Call Report data to staging db - S540a');
+(6, 'SP8.2', 'Transfer DMI NMLS Call Report data to staging db - S540a')
+;
 
 INSERT INTO mdi.table_input_step
 (dwid, process_dwid, sql, limit_size, execute_for_each_row, replace_variables_in_script, enable_lazy_conversion,
@@ -143,20 +147,21 @@ INSERT INTO mdi.table_output_step (dwid, process_dwid, target_schema, target_tab
                                    connectionname, partition_over_tables, specify_database_fields, ignore_insert_errors,
                                    use_batch_update)
 VALUES
-(6, 6, 'mcr', 'nmls_call_report_national', 1000, null, null, null, null, 'N', null, 'Y', 'Staging DB Connection', 'N',
- 'Y', 'N', 'N');                                                                                                                                                                                                                                                                                                                                                                          (4, 4, 'mcr', 'nmls_call_report_national', 1000, null, null, null, null, 'N', null, 'Y', 'Staging DB Connection', 'N', 'Y', 'N', 'N');
+(6, 6, 'mcr', 'nmls_call_report_s540a', 1000, null, null, null, null, 'N', null, 'Y', 'Staging DB Connection', 'N',
+ 'Y', 'N', 'N')
+;                                                                                                                                                                                                                                                                                                                                                                          (4, 4, 'mcr', 'nmls_call_report_national', 1000, null, null, null, null, 'N', null, 'Y', 'Staging DB Connection', 'N', 'Y', 'N', 'N');
 
 INSERT INTO mdi.table_output_field
 (dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order)
 VALUES
-(36, 6, 'state_type', 'state_type', 1)
-     , (37, 6, 'mcr_desc', 'mcr_desc', 2)
-     , (38, 6, 'item_id', 'item_id', 3)
-     , (39, 6, 'servicer_id', 'servicer_id', 4)
-     , (40, 6, 'servicer_name', 'servicer_name', 5)
-     , (41, 6, 'pool_number', 'pool_number', 6)
-     , (42, 6, 'data_source_dwid', 'data_source_dwid', 7)
-     , (43, 6, 'servicer_name', 'servicer_name', 8)
-     , (44, 6, 'pool_number', 'pool_number', 9)
-     , (45, 6, 'data_source_dwid', 'data_source_dwid', 10)
+(37, 6, 'state_type', 'state_type', 1)
+     , (38, 6, 'mcr_desc', 'mcr_desc', 2)
+     , (39, 6, 'item_id', 'item_id', 3)
+     , (40, 6, 'servicer_id', 'servicer_id', 4)
+     , (41, 6, 'servicer_name', 'servicer_name', 5)
+     , (42, 6, 'pool_number', 'pool_number', 6)
+     , (43, 6, 'data_source_dwid', 'data_source_dwid', 7)
+     , (44, 6, 'servicer_name', 'servicer_name', 8)
+     , (45, 6, 'pool_number', 'pool_number', 9)
+     , (46, 6, 'data_source_dwid', 'data_source_dwid', 10)
 ;
