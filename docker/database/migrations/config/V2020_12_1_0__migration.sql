3df92b3cb69b6d45009de9549e71f39516128577
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2020-12-01 11:13:46 -0500

    [#1199238747879871] removed manually set sensitive fields

--- docker/database/migrations/config/V2020_12_1_0__migration.sql
+++ docker/database/migrations/config/V2020_12_1_0__migration.sql
@@ -18,15 +18,15 @@ SET
     is_sensitive=FALSE
 WHERE 1=1;
 
--- manually set sensitive fields
-UPDATE
-    mdi.table_output_field
-SET
-    is_sensitive=TRUE
-WHERE
-        dwid IN
-        (
-              4 -- unpaid_balance in SP8.1
-            ,13 -- unpaid_balance in SP9.1
-            ,18 -- unpaid_balance in SP10.1
-        );
+-- -- manually set sensitive fields used for testing
+-- UPDATE
+--     mdi.table_output_field
+-- SET
+--     is_sensitive=TRUE
+-- WHERE
+--         dwid IN
+--         (
+--               4 -- unpaid_balance in SP8.1
+--             ,13 -- unpaid_balance in SP9.1
+--             ,18 -- unpaid_balance in SP10.1
+--         );
