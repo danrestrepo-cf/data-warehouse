63f79adf10e11bdc15846a7da6f90ad059edde97
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2020-12-07 14:49:40 -0500

    [#1199150384475333] Initial controller changes

--- docker/database/migrations/config/V2020_12_2_0__migration.sql
+++ docker/database/migrations/config/V2020_12_2_0__migration.sql
@@ -40,6 +40,7 @@ CREATE TABLE mdi.table_input_step
         CONSTRAINT table_input_step_pk
             PRIMARY KEY,
     process_dwid BIGINT NOT NULL,
+    data_source_dwid BIGINT,
     sql TEXT NOT NULL,
     limit_size INTEGER DEFAULT 0, -- default to zero so Pentaho will return ALL rows queried
     execute_for_each_row mdi.PENTAHO_Y_OR_N DEFAULT 'N'::mdi.PENTAHO_Y_OR_N NOT NULL,
@@ -84,14 +85,14 @@ VALUES ( 5, 5, 'mcr', 'nmls_call_report_state', 1000, NULL, NULL, NULL, NULL, 'N
 
 INSERT INTO mdi.table_output_field
 ( dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order, is_sensitive )
-VALUES ( 29, 5, 'mcr_code', 'mcr_code', 1,0  )
-     , ( 30, 5, 'mcr_desc', 'mcr_desc', 2,0 )
-     , ( 31, 5, 'state_type', 'state_type', 3,0 )
-     , ( 32, 5, 'unpaid_balance', 'unpaid_balance', 4,0 )
-     , ( 33, 5, 'loan_count', 'loan_count', 5,0 )
-     , ( 34, 5, 'avg_loan_size', 'avg_loan_size', 6,0 )
-     , ( 35, 5, 'etl_batch_id', 'etl_batch_id', 7 ,0)
-     , ( 36, 5, 'data_source_dwid', 'data_source_dwid', 8 ,0)
+VALUES ( 29, 5, 'mcr_code', 'mcr_code', 1,FALSE)
+     , ( 30, 5, 'mcr_desc', 'mcr_desc', 2,FALSE)
+     , ( 31, 5, 'state_type', 'state_type', 3,FALSE)
+     , ( 32, 5, 'unpaid_balance', 'unpaid_balance', 4,FALSE)
+     , ( 33, 5, 'loan_count', 'loan_count', 5,FALSE)
+     , ( 34, 5, 'avg_loan_size', 'avg_loan_size', 6,FALSE)
+     , ( 35, 5, 'etl_batch_id', 'etl_batch_id', 7 ,FALSE)
+     , ( 36, 5, 'data_source_dwid', 'data_source_dwid', 8 ,FALSE)
 ;
 
 --
@@ -128,13 +129,13 @@ VALUES ( 4, 4, 'mcr', 'nmls_call_report_national', 1000, NULL, NULL, NULL, NULL,
 
 INSERT INTO mdi.table_output_field
 ( dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order,is_sensitive )
-VALUES ( 22, 4, 'mcr_code', 'mcr_code', 1 ,0)
-     , ( 23, 4, 'mcr_desc', 'mcr_desc', 2 ,0)
-     , ( 24, 4, 'unpaid_balance', 'unpaid_balance', 3 ,0)
-     , ( 25, 4, 'loan_count', 'loan_count', 4 ,0)
-     , ( 26, 4, 'avg_loan_size', 'avg_loan_size', 5 ,0)
-     , ( 27, 4, 'etl_batch_id', 'etl_batch_id', 6 ,0)
-     , ( 28, 4, 'data_source_dwid', 'data_source_dwid', 7 ,0)
+VALUES ( 22, 4, 'mcr_code', 'mcr_code', 1 ,FALSE)
+     , ( 23, 4, 'mcr_desc', 'mcr_desc', 2 ,FALSE)
+     , ( 24, 4, 'unpaid_balance', 'unpaid_balance', 3 ,FALSE)
+     , ( 25, 4, 'loan_count', 'loan_count', 4 ,FALSE)
+     , ( 26, 4, 'avg_loan_size', 'avg_loan_size', 5 ,FALSE)
+     , ( 27, 4, 'etl_batch_id', 'etl_batch_id', 6 ,FALSE)
+     , ( 28, 4, 'data_source_dwid', 'data_source_dwid', 7 ,FALSE)
 ;
 
 --
@@ -142,7 +143,7 @@ VALUES ( 22, 4, 'mcr_code', 'mcr_code', 1 ,0)
 --
 INSERT INTO mdi.process
     ( dwid, name, description )
-VALUES ( 6, 'SP8.2', 'Transfer DMI NMLS Call Report data to staging db - S540a' )
+VALUES ( 6, 'SP10.2', 'Transfer DMI NMLS Call Report data to staging db - S540a' )
 ;
 
 INSERT INTO mdi.table_input_step
@@ -173,14 +174,14 @@ VALUES ( 6, 6, 'mcr', 'nmls_call_report_s540a', 1000, NULL, NULL, NULL, NULL, 'N
 
 INSERT INTO mdi.table_output_field
 ( dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order,is_sensitive )
-VALUES ( 37, 6, 'state_type', 'state_type', 1,0 )
-     , ( 38, 6, 'mcr_desc', 'mcr_desc', 2,0 )
-     , ( 39, 6, 'item_id', 'item_id', 3,0 )
-     , ( 40, 6, 'servicer_id', 'servicer_id', 4,0 )
-     , ( 41, 6, 'servicer_name', 'servicer_name', 5,0 )
-     , ( 42, 6, 'pool_number', 'pool_number', 6,0 )
-     , ( 43, 6, 'data_source_dwid', 'data_source_dwid', 7 ,0)
-     , ( 44, 6, 'servicer_name', 'servicer_name', 8,0 )
-     , ( 45, 6, 'pool_number', 'pool_number', 9,0 )
-     , ( 46, 6, 'data_source_dwid', 'data_source_dwid', 10 ,0)
+VALUES ( 37, 6, 'state_type', 'state_type', 1,FALSE)
+     , ( 38, 6, 'mcr_desc', 'mcr_desc', 2,FALSE)
+     , ( 39, 6, 'item_id', 'item_id', 3,FALSE)
+     , ( 40, 6, 'servicer_id', 'servicer_id', 4,FALSE)
+     , ( 41, 6, 'servicer_name', 'servicer_name', 5,FALSE)
+     , ( 42, 6, 'pool_number', 'pool_number', 6,FALSE)
+     , ( 43, 6, 'data_source_dwid', 'data_source_dwid', 7 ,FALSE)
+     , ( 44, 6, 'servicer_name', 'servicer_name', 8,FALSE)
+     , ( 45, 6, 'pool_number', 'pool_number', 9,FALSE)
+     , ( 46, 6, 'data_source_dwid', 'data_source_dwid', 10 ,FALSE)
 ;
