e8fe50356c6f902b8480b32269ccd2aeced06746
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2020-12-18 08:27:46 -0500

    [#1198960130092081] adding permission file

---
+++ docker/database/baseline/staging-permissions/V2020_12_3_0__migration.sql
@@ -0,0 +1,4 @@
+GRANT USAGE ON SCHEMA staging_compliance to dmi;
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON staging_compliance.nmls_call_report_national
+                                                , staging_compliance.nmls_call_report_s540a
+                                                , staging_compliance.nmls_call_report_state TO dmi;
