aedf487a542db5b7036a5977c40bf230ff877bb7
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-01-12 14:09:53 -0500

    [#1198960130092081] updated with changes from code review

--- docker/database/migrations/config/V2021_1_1_0__migration.sql
+++ docker/database/migrations/config/V2021_1_1_0__migration.sql
@@ -67,7 +67,7 @@ VALUES ( 2, 5, 3,'select
    , state_type
    , unpaid_balance
    , loan_count
-   , average_loan_size
+   , avg_loan_size
    , etl_batch_id
 from
    dmi.nmls_call_report_state_raw
@@ -79,19 +79,19 @@ INSERT INTO mdi.table_output_step ( dwid, process_dwid, target_schema, target_ta
                                   , table_name_defined_in_field, return_auto_generated_key_field, truncate_table
                                   , connectionname, partition_over_tables, specify_database_fields, ignore_insert_errors
                                   , use_batch_update )
-VALUES ( 5, 5, 'staging_compliance', 'nmls_call_report_state', 1000, NULL, NULL, NULL, NULL, 'N', NULL, 'N', 'Staging DB Connection', 'N', 'Y', 'N', 'N' )
+VALUES ( 5, 5, 'staging_compliance', 'nmls_call_report_state', 1000, NULL, NULL, NULL, NULL, 'N', NULL, 'Y', 'Staging DB Connection', 'N', 'Y', 'N', 'N' )
 ;
 
 INSERT INTO mdi.table_output_field
 ( dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order, is_sensitive )
-VALUES ( 29, 5, 'mcr_code', 'mcr_code', 1,FALSE)
-     , ( 30, 5, 'mcr_desc', 'mcr_desc', 2,FALSE)
-     , ( 31, 5, 'state_type', 'state_type', 3,FALSE)
-     , ( 32, 5, 'unpaid_balance', 'unpaid_balance', 4,FALSE)
-     , ( 33, 5, 'loan_count', 'loan_count', 5,FALSE)
-     , ( 34, 5, 'avg_loan_size', 'average_loan_size', 6,FALSE)
-     , ( 35, 5, 'etl_batch_id', 'etl_batch_id', 7 ,FALSE)
-     , ( 36, 5, 'data_source_dwid', 'data_source_dwid', 8 ,FALSE)
+VALUES ( 29, 5, 'mcr_code', 'mcr_code', 1, FALSE)
+     , ( 30, 5, 'mcr_description', 'mcr_desc', 2, FALSE)
+     , ( 31, 5, 'state_type', 'state_type', 3, FALSE)
+     , ( 32, 5, 'unpaid_balance', 'unpaid_balance', 4, FALSE)
+     , ( 33, 5, 'loan_count', 'loan_count', 5, FALSE)
+     , ( 34, 5, 'average_loan_size', 'avg_loan_size', 6, FALSE)
+     , ( 35, 5, 'etl_batch_id', 'etl_batch_id', 7 , FALSE)
+     , ( 36, 5, 'data_source_dwid', 'data_source_dwid', 8 , FALSE)
 ;
 
 --
@@ -109,7 +109,7 @@ VALUES ( 1, 4, 3, 'select
    , mcr_desc
    , unpaid_balance
    , loan_count
-   , average_loan_size
+   , avg_loan_size
    , etl_batch_id
 from
    dmi.nmls_call_report_national_raw
@@ -121,18 +121,18 @@ INSERT INTO mdi.table_output_step ( dwid, process_dwid, target_schema, target_ta
                                   , table_name_defined_in_field, return_auto_generated_key_field, truncate_table
                                   , connectionname, partition_over_tables, specify_database_fields, ignore_insert_errors
                                   , use_batch_update )
-VALUES ( 4, 4, 'staging_compliance', 'nmls_call_report_national', 1000, NULL, NULL, NULL, NULL, 'N', NULL, 'N', 'Staging DB Connection', 'N', 'Y', 'N', 'N' )
+VALUES ( 4, 4, 'staging_compliance', 'nmls_call_report_national', 1000, NULL, NULL, NULL, NULL, 'N', NULL, 'Y', 'Staging DB Connection', 'N', 'Y', 'N', 'N' )
 ;
 
 INSERT INTO mdi.table_output_field
-( dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order,is_sensitive )
-VALUES ( 22, 4, 'mcr_code', 'mcr_code', 1 ,FALSE)
-     , ( 23, 4, 'mcr_desc', 'mcr_desc', 2 ,FALSE)
-     , ( 24, 4, 'unpaid_balance', 'unpaid_balance', 3 ,FALSE)
-     , ( 25, 4, 'loan_count', 'loan_count', 4 ,FALSE)
-     , ( 26, 4, 'avg_loan_size', 'average_loan_size', 5 ,FALSE)
-     , ( 27, 4, 'etl_batch_id', 'etl_batch_id', 6 ,FALSE)
-     , ( 28, 4, 'data_source_dwid', 'data_source_dwid', 7 ,FALSE)
+( dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order, is_sensitive )
+VALUES ( 22, 4, 'mcr_code', 'mcr_code', 1 , FALSE)
+     , ( 23, 4, 'mcr_description', 'mcr_desc', 2 , FALSE)
+     , ( 24, 4, 'unpaid_balance', 'unpaid_balance', 3 , FALSE)
+     , ( 25, 4, 'loan_count', 'loan_count', 4 , FALSE)
+     , ( 26, 4, 'average_loan_size', 'avg_loan_size', 5 , FALSE)
+     , ( 27, 4, 'etl_batch_id', 'etl_batch_id', 6 , FALSE)
+     , ( 28, 4, 'data_source_dwid', 'data_source_dwid', 7 , FALSE)
 ;
 
 --
@@ -164,20 +164,20 @@ INSERT INTO mdi.table_output_step ( dwid, process_dwid, target_schema, target_ta
                                   , table_name_defined_in_field, return_auto_generated_key_field, truncate_table
                                   , connectionname, partition_over_tables, specify_database_fields, ignore_insert_errors
                                   , use_batch_update )
-VALUES ( 6, 6, 'staging_compliance', 'nmls_call_report_s540a', 1000, NULL, NULL, NULL, NULL, 'N', NULL, 'N', 'Staging DB Connection', 'N', 'Y', 'N', 'N' )
+VALUES ( 6, 6, 'staging_compliance', 'nmls_call_report_s540a', 1000, NULL, NULL, NULL, NULL, 'N', NULL, 'Y', 'Staging DB Connection', 'N', 'Y', 'N', 'N' )
 ;
 
 INSERT INTO mdi.table_output_field
-( dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order,is_sensitive )
-VALUES ( 37, 6, 'state_type', 'state_type', 1,FALSE)
-     , ( 38, 6, 'item_id', 'item_id', 3,FALSE)
-     , ( 39, 6, 'servicer_id', 'servicer_nmls_id', 4,FALSE)
-     , ( 40, 6, 'servicer_name', 'servicer_name', 5,FALSE)
-     , ( 41, 6, 'pool_number', 'pool_number', 6,FALSE)
-     , ( 42, 6, 'unpaid_balance', 'unpaid_balance', 7 ,FALSE)
-     , ( 43, 6, 'loan_count', 'loan_count', 8,FALSE)
-     , ( 44, 6, 'etl_batch_id', 'etl_batch_id', 9,FALSE)
-     , ( 45, 6, 'data_source_dwid', 'data_source_dwid', 10 ,FALSE)
+( dwid, table_output_step_dwid, database_field_name, database_stream_name, field_order, is_sensitive )
+VALUES ( 37, 6, 'state_type', 'state_type', 1, FALSE)
+     , ( 38, 6, 'item_id', 'item_id', 3, FALSE)
+     , ( 39, 6, 'servicer_nmls_id', 'servicer_id', 4, FALSE)
+     , ( 40, 6, 'servicer_name', 'servicer_name', 5, FALSE)
+     , ( 41, 6, 'pool_number', 'pool_number', 6, FALSE)
+     , ( 42, 6, 'unpaid_balance', 'unpaid_balance', 7 , FALSE)
+     , ( 43, 6, 'loan_count', 'loan_count', 8, FALSE)
+     , ( 44, 6, 'etl_batch_id', 'etl_batch_id', 9, FALSE)
+     , ( 45, 6, 'data_source_dwid', 'data_source_dwid', 10 , FALSE)
 ;
 
 -- Reset sequences since we haven't been using them
