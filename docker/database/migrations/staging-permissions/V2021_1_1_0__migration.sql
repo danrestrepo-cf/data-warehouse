5794826b808b4d4c2f28c7dae6b946dec96ad7b8
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-01-13 15:05:54 -0500

    [#1198960130092081] permissions sql

--- docker/database/migrations/staging-permissions/V2021_1_1_0__migration.sql
+++ docker/database/migrations/staging-permissions/V2021_1_1_0__migration.sql
@@ -1,2 +1,4 @@
-GRANT USAGE ON SCHEMA staging_compliance TO readonly;
-GRANT SELECT ON ALL TABLES IN SCHEMA staging_compliance TO readonly;
+GRANT USAGE ON SCHEMA staging_compliance TO dmi;
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON staging_compliance.nmls_call_report_national
+                                                , staging_compliance.nmls_call_report_s540a
+                                                , staging_compliance.nmls_call_report_state TO dmi;
