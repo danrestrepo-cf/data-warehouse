aff87e513323120460bb50c0b8b9cefebce0ad5d
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-01-29 16:16:16 -0500

    [#1199603976631049] removing unneeded migration script

--- docker/database/migrations/staging/V2021_2_1_0__migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE staging_compliance.nmls_call_report_national ADD COLUMN imported_filename text NOT NULL;
-ALTER TABLE staging_compliance.nmls_call_report_s540a ADD COLUMN imported_filename text NOT NULL;
-ALTER TABLE staging_compliance.nmls_call_report_state ADD COLUMN imported_filename text NOT NULL;
