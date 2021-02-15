67faca7fa15773b070cc7a0874656fec4cd58268
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-02-15 10:48:54 -0500

    [#1199933501989661] updated text in print_usage() function

--- docker/psql.sh
+++ docker/psql.sh
@@ -2,31 +2,27 @@
 
 # display script usage
 function print_usage() {
-  script_filename=${0##*/}
+  echo " "
   echo "${script_filename} -- script usage"
+  echo "Usage: ${script_filename} [database name] [database username]"
   echo " "
-  echo " Bash Mode - run docker and launch bash instead of executing a Pentaho process."
-  echo "    Usage: ${script_filename} bash"
-  echo "    Example: ./${script_filename} bash"
-  echo " "
+  echo "The first parameter is the database name to connect to (like ingress, config, staging, etc)."
+  echo "The second parameter is the name of the user used to connect to the database. If the second parameter is not provided it defaults to the value '${default_db_username}'."
 }
 
 # execute psql docker image using db_username, project_name, and database variables
 function run_docker() {
-  echo_connection_details
+  echo "Logging into PostgreSQL with username: ${db_username} and database: ${database}"
   docker run -it \
     --network ${project_name}_default \
     --rm postgres:11 psql -U ${db_username} "postgresql://${project_name}_database_1:5432/${database}"
-}
-
-function echo_connection_details() {
-  echo "Logging into PostgreSQL with username: ${db_username} and database: ${database}"
 }
 
 # set variable defaults
 project_name=edw
 default_db_username=postgres
 database="${1}" # first parameter is always the database we connect to
+script_filename=${0##*/}
 
 # validate input parameters
 case "$#" in
@@ -39,7 +35,7 @@ case "$#" in
     run_docker
     ;;
   *)
-    echo "ERROR: Could not understand input parameters. ${script_filename} expects 1 parameter where the first parameter is the name of the database to connect to but found ${#} parameters total."
+    echo "ERROR: Could not understand input parameters. ${script_filename} expects 1 or 2 parameters where the first parameter is the name of the database to connect (required) and the second is the name of the user to use to connect to the database (optional). Found ${#} parameters total were provided."
     echo "Displaying script usage:"
     print_usage
     exit 1
