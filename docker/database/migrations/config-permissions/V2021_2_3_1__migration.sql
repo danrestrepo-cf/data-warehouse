9490eef012b70cbccdf48664fa9864ad4bc86078
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-02-26 13:32:55 -0500

    [#1199565206309934] Grant readonly user permissions on new EDW tables

---
+++ docker/database/migrations/config-permissions/V2021_2_3_1__migration.sql
@@ -0,0 +1,2 @@
+-- Grant SELECT to readonly for new mdi.config tables
+GRANT SELECT ON mdi.insert_update_step, mdi.insert_update_key, mdi.insert_update_field TO readonly;
---
+++ docker/database/migrations/ingress-permissions/V2021_2_3_1__migration.sql
@@ -0,0 +1,2 @@
+-- Grant SELECT to readonly for new ingress.test tables
+GRANT SELECT ON test.unit_test_tools_source, test.unit_test_tools_target TO readonly;
