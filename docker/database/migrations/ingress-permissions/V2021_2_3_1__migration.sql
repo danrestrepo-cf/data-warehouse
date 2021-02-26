6ab1c7901b69ffa872f6891d44bcffe091fab547
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-02-26 13:43:19 -0500

    [#1199565206309934] Additional grants to mditest user

--- docker/database/migrations/ingress-permissions/V2021_2_3_1__migration.sql
+++ docker/database/migrations/ingress-permissions/V2021_2_3_1__migration.sql
@@ -1,2 +1,3 @@
 -- Grant SELECT to readonly for new ingress.test tables
+GRANT SELECT ON test.unit_test_tools_source, test.unit_test_tools_target TO mditest;
 GRANT SELECT ON test.unit_test_tools_source, test.unit_test_tools_target TO readonly;
