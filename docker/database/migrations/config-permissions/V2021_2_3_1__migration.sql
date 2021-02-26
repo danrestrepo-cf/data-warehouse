f4ca9b3a72769fb0a4292b5af6c9d53b77f83a3c
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-02-26 13:47:53 -0500

    [#1199565206309934] Amended permissions, comment

--- docker/database/migrations/config-permissions/V2021_2_3_1__migration.sql
+++ docker/database/migrations/config-permissions/V2021_2_3_1__migration.sql
@@ -1,3 +1,3 @@
--- Grant SELECT to readonly for new mdi.config tables
+-- Grant permissions to mditest, readonly new mdi.config tables
 GRANT SELECT ON mdi.insert_update_step, mdi.insert_update_key, mdi.insert_update_field TO mditest;
 GRANT SELECT ON mdi.insert_update_step, mdi.insert_update_key, mdi.insert_update_field TO readonly;
