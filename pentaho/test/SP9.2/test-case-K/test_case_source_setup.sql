ec53868e7cdda6454526c3f973314e17841e48d6
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-03-16 16:14:50 -0400

    [#1199980645336559] test cases for .2 prod SPs

---
+++ pentaho/test/SP9.2/test-case-K/test_case_source_setup.sql
@@ -0,0 +1,3 @@
+INSERT INTO dmi.nmls_call_report_national ( etl_batch_id, data_source_dwid, input_filename, mcr_code, mcr_description,
+                                            sum_of_upb, loan_count, avg_loan_size, report_quarter)
+    VALUES ('dummy_etl_batch_id', 3,'/input//test_case_source_file.ext','S1','Drainage of Left Abdomen Bursa and Ligament, Percutaneous Approach',1685486.5,5856,3531297.7,'2021Q1');
