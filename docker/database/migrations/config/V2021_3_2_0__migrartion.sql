bfc16f26842e5ac6c136423011d1d84a13ec2175
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-03-22 16:15:51 -0400

    [#1199980645336559] sp-0.1 refactor finished; first test works

--- docker/database/migrations/config/V2021_3_2_0__migrartion.sql
+++ docker/database/migrations/config/V2021_3_2_0__migrartion.sql
@@ -43,13 +43,13 @@ DO $$
         -- Table output field records for SP-0.5
         INSERT INTO mdi.table_output_field (table_output_step_dwid, database_field_name, database_stream_name, field_order, is_sensitive)
         VALUES (sp_table_output_step_dwid, 'tool_pid', 'tool_pid', 1, FALSE)
-             ,      (sp_table_output_step_dwid, 'tool_code', 'tool_code', 2, FALSE)
-             ,      (sp_table_output_step_dwid, 'tool_type', 'tool_type', 3, FALSE)
-             ,      (sp_table_output_step_dwid, 'tool_price', 'tool_price', 4, FALSE)
-             ,      (sp_table_output_step_dwid, 'tool_inventory_start_date', 'tool_inventory_start_date', 5, FALSE)
-             ,      (sp_table_output_step_dwid, 'tool_inventory_end_date', 'tool_inventory_end_date', 6, FALSE)
-             ,      (sp_table_output_step_dwid, 'data_source_dwid', 'data_source_dwid_value', 7, FALSE)
-             ,      (sp_table_output_step_dwid, 'etl_batch_id', 'etl_batch_id', 8, FALSE);
+             , (sp_table_output_step_dwid, 'tool_code', 'tool_code', 2, FALSE)
+             , (sp_table_output_step_dwid, 'tool_type', 'tool_type', 3, FALSE)
+             , (sp_table_output_step_dwid, 'tool_price', 'tool_price', 4, FALSE)
+             , (sp_table_output_step_dwid, 'tool_inventory_start_date', 'tool_inventory_start_date', 5, FALSE)
+             , (sp_table_output_step_dwid, 'tool_inventory_end_date', 'tool_inventory_end_date', 6, FALSE)
+             , (sp_table_output_step_dwid, 'data_source_dwid', 'data_source_dwid_value', 7, FALSE)
+             , (sp_table_output_step_dwid, 'etl_batch_id', 'etl_batch_id', 8, FALSE);
 
     END
 $$;
@@ -99,17 +99,16 @@ DO $$
 
         -- Insert / Update records for SP-0.6
         INSERT INTO mdi.insert_update_field (insert_update_step_dwid, update_lookup, update_stream, update_flag, is_sensitive)
-        VALUES (sp_insert_update_step_dwid, 'tool_pid', 'tool_pid', 'N', FALSE)
-             ,      (sp_insert_update_step_dwid, 'tool_code', 'tool_code', 'Y', FALSE)
-             ,      (sp_insert_update_step_dwid, 'tool_type', 'tool_type', 'Y', FALSE)
-             ,      (sp_insert_update_step_dwid, 'tool_price', 'tool_price', 'Y', FALSE)
-             ,      (sp_insert_update_step_dwid, 'tool_inventory_start_date', 'tool_inventory_start_date', 'Y', FALSE)
-             ,      (sp_insert_update_step_dwid, 'tool_inventory_end_date', 'tool_inventory_end_date', 'Y', FALSE)
-             ,      (sp_insert_update_step_dwid, 'data_source_dwid', 'data_source_dwid_value', 'Y', FALSE)
-             ,      (sp_insert_update_step_dwid, 'etl_batch_id', 'etl_batch_id', 'Y', FALSE);
+        VALUES    (sp_insert_update_step_dwid, 'tool_pid', 'tool_pid', 'N', FALSE)
+                , (sp_insert_update_step_dwid, 'tool_code', 'tool_code', 'Y', FALSE)
+                , (sp_insert_update_step_dwid, 'tool_type', 'tool_type', 'Y', FALSE)
+                , (sp_insert_update_step_dwid, 'tool_price', 'tool_price', 'Y', FALSE)
+                , (sp_insert_update_step_dwid, 'tool_inventory_start_date', 'tool_inventory_start_date', 'Y', FALSE)
+                , (sp_insert_update_step_dwid, 'tool_inventory_end_date', 'tool_inventory_end_date', 'Y', FALSE)
+                , (sp_insert_update_step_dwid, 'data_source_dwid', 'data_source_dwid_value', 'Y', FALSE)
+                , (sp_insert_update_step_dwid, 'etl_batch_id', 'etl_batch_id', 'Y', FALSE);
     END
 $$;
-
 
 
 -- modify SP-0.1 config to use purpose built test tables
@@ -126,19 +125,17 @@ DO $$
         -- modify SP-0.1 source file configs
         DELETE FROM mdi.csv_file_input_field WHERE csv_file_input_step_dwid = sp_csv_file_input_step_dwid;
         INSERT INTO mdi.csv_file_input_field (csv_file_input_step_dwid, field_name, field_type, field_format, field_length, field_precision, field_currency, field_decimal, field_group, field_trim_type, field_order)
-            VALUES
-              (sp_csv_file_input_step_dwid, 'tool_pid', 'Integer', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 1)
-            , (sp_csv_file_input_step_dwid, 'tool_code', 'String', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 2)
-            , (sp_csv_file_input_step_dwid, 'tool_type', 'String', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 3)
-            , (sp_csv_file_input_step_dwid, 'tool_price', 'Integer', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 4)
-            , (sp_csv_file_input_step_dwid, 'tool_inventory_start_date', 'Date', 'yyyy-mm-dd', NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 5)
-            , (sp_csv_file_input_step_dwid, 'tool_inventory_end_date', 'Date', 'yyyy-mm-dd', NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 6);
+            VALUES (sp_csv_file_input_step_dwid, 'tool_pid', 'Integer', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 1)
+                 , (sp_csv_file_input_step_dwid, 'tool_code', 'String', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 2)
+                 , (sp_csv_file_input_step_dwid, 'tool_type', 'String', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 3)
+                 , (sp_csv_file_input_step_dwid, 'tool_price', 'Integer', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 4)
+                 , (sp_csv_file_input_step_dwid, 'tool_inventory_start_date', 'Date', 'yyyy-mm-dd', NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 5)
+                 , (sp_csv_file_input_step_dwid, 'tool_inventory_end_date', 'Date', 'yyyy-mm-dd', NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 6);
 
         -- modify SP-0.1 target table configs
         DELETE FROM mdi.table_output_field WHERE table_output_step_dwid = sp_table_output_step_dwid;
         INSERT INTO mdi.table_output_field (table_output_step_dwid, database_field_name, database_stream_name, field_order, is_sensitive)
-        VALUES
-             (sp_table_output_step_dwid, 'tool_pid', 'tool_pid', 1, FALSE)
+        VALUES (sp_table_output_step_dwid, 'tool_pid', 'tool_pid', 1, FALSE)
              , (sp_table_output_step_dwid, 'tool_code', 'tool_code', 2, FALSE)
              , (sp_table_output_step_dwid, 'tool_type', 'tool_type', 3, FALSE)
              , (sp_table_output_step_dwid, 'tool_price', 'tool_price', 4, FALSE)
@@ -147,16 +144,46 @@ DO $$
              , (sp_table_output_step_dwid, 'data_source_dwid', 'data_source_dwid', 7, FALSE)
              , (sp_table_output_step_dwid, 'etl_batch_id', 'etl_batch_id', 7, FALSE);
 
+        -- update name of target table to use
+        UPDATE mdi.table_output_step SET target_table = 'unit_test_tools_target' WHERE process_dwid = sp_process_dwid;
+    END
+$$;
 
 
+-- modify SP-0.2 config to use purpose built test tables
+DO $$
+    DECLARE sp_process_dwid BIGINT;
+    DECLARE sp_microsoft_excel_input_step_dwid BIGINT;
+    DECLARE sp_table_output_step_dwid BIGINT;
 
--- source
---         tool_pid
---             , tool_code
---             , tool_type
---             , tool_price
---             , tool_inventory_start_date
---             , tool_inventory_end_date
+    BEGIN
+        sp_process_dwid = (SELECT dwid FROM mdi.process WHERE name ='SP-0.2');
+        sp_microsoft_excel_input_step_dwid = (SELECT dwid FROM mdi.microsoft_excel_input_step WHERE process_dwid = sp_process_dwid);
+        sp_table_output_step_dwid = (SELECT dwid FROM mdi.table_output_step WHERE process_dwid = sp_process_dwid);
 
+        -- modify SP-0.2 source file configs
+        DELETE FROM mdi.microsoft_excel_input_field WHERE microsoft_excel_input_step_dwid = sp_microsoft_excel_input_step_dwid;
+        INSERT INTO mdi.microsoft_excel_input_field (microsoft_excel_input_step_dwid, field_name, field_type, field_format, field_length, field_precision, field_currency, field_decimal, field_group, field_trim_type, field_order)
+        VALUES (sp_microsoft_excel_input_step_dwid, 'tool_pid', 'Integer', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 1)
+             , (sp_microsoft_excel_input_step_dwid, 'tool_code', 'String', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 2)
+             , (sp_microsoft_excel_input_step_dwid, 'tool_type', 'String', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 3)
+             , (sp_microsoft_excel_input_step_dwid, 'tool_price', 'Integer', NULL, NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 4)
+             , (sp_microsoft_excel_input_step_dwid, 'tool_inventory_start_date', 'Date', 'yyyy-mm-dd', NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 5)
+             , (sp_microsoft_excel_input_step_dwid, 'tool_inventory_end_date', 'Date', 'yyyy-mm-dd', NULL, -1, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 6);
+
+        -- modify SP-0.2 target table configs
+        DELETE FROM mdi.table_output_field WHERE table_output_step_dwid = sp_table_output_step_dwid;
+        INSERT INTO mdi.table_output_field (table_output_step_dwid, database_field_name, database_stream_name, field_order, is_sensitive)
+        VALUES (sp_table_output_step_dwid, 'tool_pid', 'tool_pid', 1, FALSE)
+             , (sp_table_output_step_dwid, 'tool_code', 'tool_code', 2, FALSE)
+             , (sp_table_output_step_dwid, 'tool_type', 'tool_type', 3, FALSE)
+             , (sp_table_output_step_dwid, 'tool_price', 'tool_price', 4, FALSE)
+             , (sp_table_output_step_dwid, 'tool_inventory_start_date', 'tool_inventory_start_date', 5, FALSE)
+             , (sp_table_output_step_dwid, 'tool_inventory_end_date', 'tool_inventory_end_date', 6, FALSE)
+             , (sp_table_output_step_dwid, 'data_source_dwid', 'data_source_dwid', 7, FALSE)
+             , (sp_table_output_step_dwid, 'etl_batch_id', 'etl_batch_id', 7, FALSE);
+
+        -- update name of target table to use
+        UPDATE mdi.table_output_step SET target_table = 'unit_test_tools_target' WHERE process_dwid = sp_process_dwid;
     END
 $$;
--- pentaho/test/SP-0.1/test-case-G/test_case_output_setup.sql
+++ pentaho/test/SP-0.1/test-case-G/test_case_output_setup.sql
@@ -1,1 +1,1 @@
-\COPY dmi.nmls_call_report_state (data_source_dwid, input_filename, mcr_code, mcr_description, state_code, sum_of_upb, loan_count, avg_loan_size, report_quarter) TO '/input/actual_output.csv' WITH (FORMAT CSV,HEADER);
+\COPY test.unit_test_tools_target (tool_pid,tool_code,tool_type,tool_price,tool_inventory_start_date,tool_inventory_end_date,data_source_dwid) TO '/input/actual_output.csv' WITH (FORMAT CSV,HEADER);
