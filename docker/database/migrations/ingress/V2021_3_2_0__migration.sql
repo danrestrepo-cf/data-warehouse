2272678951eb991b83154b225d40b76bd5d831f5
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-03-23 15:23:45 -0400

    [#1199980645336559] fix typo in comment

--- docker/database/migrations/ingress/V2021_3_2_0__migration.sql
+++ docker/database/migrations/ingress/V2021_3_2_0__migration.sql
@@ -2,6 +2,6 @@
 -- EDW - Add test cases to EDW unit test runner script: https://app.asana.com/0/0/1199980645336559
 --
 
--- drop source tables that is no longer needed.
+-- drop source tables that are no longer needed
 DROP TABLE test.sp_0_1_mdi_csv_file_to_table;
 DROP TABLE test.sp_0_2_mdi_microsoft_excel_to_table;
