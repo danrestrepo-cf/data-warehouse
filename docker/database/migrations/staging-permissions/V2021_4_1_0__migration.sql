063201b0812f8a118df10d6a19a7d7d04be0b7a3
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-03-29 18:01:37 -0400

    [#1200119921931317] updating permissions for octane_dms_writer

--- docker/database/migrations/staging-permissions/V2021_4_1_0__migration.sql
+++ docker/database/migrations/staging-permissions/V2021_4_1_0__migration.sql
@@ -3,3 +3,10 @@
 --
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA staging_octane GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON TABLES TO dms_octane_writer;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON
+    staging_octane.borrower_declarations
+    , staging_octane.minimum_required_residual_income_table
+    , staging_octane.minimum_required_residual_income_row
+    , staging_octane.new_lock_only_add_on
+TO dms_octane_writer;
