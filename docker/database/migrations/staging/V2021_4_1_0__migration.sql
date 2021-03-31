be93460d8f473c3810d569f99ad47e8174f01745
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-03-31 16:02:00 -0400

    [#1200071325587092] Removed not null constraints from date_dim, inserted zero record

--- docker/database/migrations/staging/V2021_4_1_0__migration.sql
+++ docker/database/migrations/staging/V2021_4_1_0__migration.sql
@@ -21,3 +21,44 @@ SELECT nmls_call_report_state.mcr_field_id,
        nmls_call_report_state.report_quarter
 FROM staging_compliance.nmls_call_report_state;
 
+
+--
+-- EDW | Add zero record to date dimension table, update weekday_chronological field
+--
+
+-- Alter date dimension table to remove NOT NULL constraints from all non-dwid fields with NOT NULL constraints
+ALTER TABLE star_common.date_dim ALTER COLUMN value DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN date_format_iso_8601 DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN date_format_us DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN day_of_month_suffix DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN day_chronological DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN day_of_year DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN day_of_quarter DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN day_of_month DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN day_of_week DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_first_day_of_year DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_last_day_of_year DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_first_day_of_quarter DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_last_day_of_quarter DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_first_day_of_month DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_last_day_of_month DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_first_day_of_week DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_last_day_of_week DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_weekday DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_weekend DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN day_of_week_name DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN day_of_week_name_short DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN day_of_week_name_letter DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_leap_year DROP NOT NULL;
+ALTER TABLE star_common.date_dim ALTER COLUMN is_leap_day DROP NOT NULL;
+
+-- Insert a row into the star_common.date_dim table that has a dwid value of 0 and NULLs in all other fields
+INSERT INTO star_common.date_dim (dwid, value, date_format_iso_8601, date_format_us, day_of_month_suffix, day_chronological,
+    weekday_chronological, day_of_year, day_of_quarter, day_of_month, day_of_week, is_first_day_of_year, is_last_day_of_year,
+    is_first_day_of_quarter, is_last_day_of_quarter, is_first_day_of_month, is_last_day_of_month, is_first_day_of_week,
+    is_last_day_of_week, is_weekday, is_weekend, day_of_week_name, day_of_week_name_short, day_of_week_name_letter,
+    is_leap_year, is_leap_day, week_of_year, quarter_of_year, quarter_chronological, quarter_name, year, month_of_year,
+    days_in_month, first_day_of_month, last_day_of_month, first_day_of_week, week_name, week_format_iso_8601)
+VALUES (0 ,NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
