b7e297c5232f20afdf2f2954ddd56935746c59e8
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-04-01 10:30:09 -0400

    [#1200094798056228] Reconfigured update for table_output_field record

--- docker/database/migrations/config/V2021_4_1_0__migration.sql
+++ docker/database/migrations/config/V2021_4_1_0__migration.sql
@@ -11,7 +11,14 @@ DROP SEQUENCE mdi.csv_file_input_field_csv_file_input_step_dwid_seq;
 --
 
 -- Update mdi.config.table_output_field table
--- For dwid = 27 change the database_field_name column’s value from average_loan_size to average_unpaid_balance
+-- For NMLS Call Report State, change the database_field_name column’s value from average_loan_size to average_unpaid_balance
 UPDATE mdi.table_output_field
     SET database_field_name = 'average_unpaid_balance'
-    WHERE dwid = 27;
+    WHERE dwid = (
+        SELECT table_output_field.dwid
+        FROM mdi.process
+            JOIN mdi.table_output_step ON process.dwid = table_output_step.process_dwid
+            JOIN mdi.table_output_field ON table_output_step.dwid = table_output_field.table_output_step_dwid
+                AND table_output_field.database_field_name = 'average_loan_size'
+        WHERE process.name = 'SP8.2' -- Process to transfer DMI NMLS Call Report data to staging db - state
+        );
