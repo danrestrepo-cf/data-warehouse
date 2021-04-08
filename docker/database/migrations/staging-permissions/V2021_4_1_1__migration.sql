a121e02df9dc7b93bd2719417f2c5da329eca866
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-04-08 09:13:07 -0400

    [#1199659841029429] Added missing semicolon to staging-permissions file

--- docker/database/migrations/staging-permissions/V2021_4_1_1__migration.sql
+++ docker/database/migrations/staging-permissions/V2021_4_1_1__migration.sql
@@ -3,5 +3,5 @@
 -- (https://app.asana.com/0/0/1199659841029429)
 --
 GRANT USAGE ON SCHEMA star_loan TO readonly;
-GRANT SELECT ON ALL TABLES IN SCHEMA star_loan TO readonly
+GRANT SELECT ON ALL TABLES IN SCHEMA star_loan TO readonly;
 ALTER DEFAULT PRIVILEGES IN SCHEMA star_loan GRANT SELECT ON TABLES TO readonly;
