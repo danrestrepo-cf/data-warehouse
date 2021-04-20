87d2e3de65c89b87cc805f6d136751d00ced812e
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-04-19 21:11:14 -0400

    init.sql updates
    deployer role and admin user creation

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -5,3 +5,9 @@ CREATE DATABASE config;
 -- GRANT ALL PRIVILEGES ON DATABASE config TO postgres;
 CREATE DATABASE staging;
 -- GRANT ALL PRIVILEGES ON DATABASE staging TO postgres;
+
+CREATE ROLE deployer;
+CREATE USER admin PASSWORD 'testonly';
+GRANT deployer TO admin;
+
+GRANT ALL PRIVILEGES ON DATABASE ingress, config, staging to deployer;
