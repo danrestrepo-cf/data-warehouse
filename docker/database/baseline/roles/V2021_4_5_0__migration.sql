141ffd95cc30c54d3a62b4fbc664d48ace97eb07
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-04-23 09:11:03 -0400

    [#1200143939898023] change new user name from svc_looker to svc_looker_operations

--- docker/database/baseline/roles/V2021_4_5_0__migration.sql
+++ docker/database/baseline/roles/V2021_4_5_0__migration.sql
@@ -2,4 +2,4 @@
 -- EDW - define looker database user and access permissions
 -- (https://app.asana.com/0/0/1200143939898023)
 --
-CREATE USER svc_looker PASSWORD 'testonly';
+CREATE USER svc_looker_operations PASSWORD 'testonly';
--- docker/database/migrations/staging-permissions/V2021_4_5_0__migration.sql
+++ docker/database/migrations/staging-permissions/V2021_4_5_0__migration.sql
@@ -2,10 +2,10 @@
 -- EDW - define looker database user and access permissions
 -- (https://app.asana.com/0/0/1200143939898023)
 --
-GRANT USAGE ON SCHEMA star_common TO svc_looker;
-GRANT SELECT ON ALL TABLES IN SCHEMA star_common TO svc_looker;
-ALTER DEFAULT PRIVILEGES FOR ROLE deployer IN SCHEMA star_common GRANT SELECT ON TABLES TO svc_looker;
+GRANT USAGE ON SCHEMA star_common TO svc_looker_operations;
+GRANT SELECT ON ALL TABLES IN SCHEMA star_common TO svc_looker_operations;
+ALTER DEFAULT PRIVILEGES FOR ROLE deployer IN SCHEMA star_common GRANT SELECT ON TABLES TO svc_looker_operations;
 
-GRANT USAGE ON SCHEMA star_loan TO svc_looker;
-GRANT SELECT ON ALL TABLES IN SCHEMA star_loan TO svc_looker;
-ALTER DEFAULT PRIVILEGES FOR ROLE deployer IN SCHEMA star_loan GRANT SELECT ON TABLES TO svc_looker;
+GRANT USAGE ON SCHEMA star_loan TO svc_looker_operations;
+GRANT SELECT ON ALL TABLES IN SCHEMA star_loan TO svc_looker_operations;
+ALTER DEFAULT PRIVILEGES FOR ROLE deployer IN SCHEMA star_loan GRANT SELECT ON TABLES TO svc_looker_operations;
