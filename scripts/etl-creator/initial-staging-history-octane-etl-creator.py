5a645ee1774a283809b7b0807d966483c5186c5c
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-05-06 16:55:14 -0400

    [#1200245179995890] input data prepped for DimensionETLCreator class

--- scripts/etl-creator/initial-staging-history-octane-etl-creator.py
+++ scripts/etl-creator/initial-staging-history-octane-etl-creator.py
@@ -1,7 +1,176 @@
-
 import psycopg2
 from datetime import date
 import sys, getopt
+
+class EDW:
+    database_name = ""
+    database_username = ""
+    database_password = ""
+    database_hostname = ""
+    database_port = ""
+
+    def __init__(self,
+                 db_name="config",
+                 db_username="postgres",
+                 db_password="testonly",
+                 db_hostname="localhost",
+                 db_port="5432"):
+        self.database_name = db_name
+        self.database_username = db_username
+        self.database_password = db_password
+        self.database_hostname = db_hostname
+        self.database_port = db_port
+
+    def execute_select_query(self, query: str, database=database_name):
+        conn = psycopg2.connect(database=database,
+                                user=self.database_username,
+                                password=self.database_password,
+                                host=self.database_hostname,
+                                port=self.database_port)
+        cur = conn.cursor()
+        cur.execute(query)
+        rows = cur.fetchall()
+        conn.close()
+        return rows
+
+    def execute_parameterized_query(self, query: str, *parameters):
+        conn = psycopg2.connect(database=self.database_name,
+                                user=self.database_username,
+                                password=self.database_password,
+                                host=self.database_hostname,
+                                port=self.database_port)
+        cur = conn.cursor()
+        cur.execute(query, parameters)
+        conn.commit()
+        rows = cur.fetchall()
+        conn.close()
+        return rows
+
+    def get_table_list_from_edw_table_definition(self, schema_name) -> list:
+        query = '''SELECT
+                        edw_table_definition.dwid
+                        , edw_table_definition.database_name
+                        , edw_table_definition.schema_name
+                        , edw_table_definition.table_name
+                        , edw_table_definition.primary_source_edw_table_definition_dwid
+                    FROM
+                        mdi.edw_table_definition
+                    WHERE
+                        schema_name = %s
+                    ORDER BY
+                        schema_name ASC;'''
+        return self.execute_parameterized_query(query, (schema_name))
+
+    def get_target_field_list_from_edw_field_definition(self, edw_table_definition_dwid: int) -> list:
+        '''
+        This returns data from the edw_table_definition and edw_field_definition tables.
+
+        :param edw_table_definition_dwid: int, the primary key of the table definition row
+        :return: a list of tuples in the format [(database_name, schema_name, table_name, field_name, key_field_flag)]
+        '''
+
+        query = '''SELECT
+                        edw_table_definition.database_name
+                        , edw_table_definition.schema_name
+                        , edw_table_definition.table_name
+                        , edw_field_definition.field_name
+                        , edw_field_definition.key_field_flag
+                    FROM
+                        mdi.edw_table_definition
+                        JOIN mdi.edw_field_definition on edw_table_definition.dwid = edw_field_definition.edw_table_definition_dwid
+                    WHERE
+                        edw_table_definition.dwid = %s
+                    ORDER BY
+                        edw_field_definition.key_field_flag DESC
+                        , edw_field_definition.field_name ASC
+                    ;'''
+        return self.execute_parameterized_query(query, edw_table_definition_dwid)
+
+    def get_field_list_from_edw_field_definition(self, edw_table_definition_dwid: int) -> list:
+        '''
+        This returns all data needed to create a table-to-table/table-to-table-json MDI process. * Only supports root_join_dwids currently! *
+
+        :param edw_table_definition_dwid: int, the primary key of the table definition row
+        :return: a list of tuples in the format [(table_input_database_name, table_input_schema_name, table_input_table_name, table_input_field_name, table_output_field_source_calculation
+        table_input_field_source_calculation, has_table_output_edw_join_tree_definition, join_type, join_condition, table_output_database_name, table_output_schema_name, table_output_table_name,
+        table_output_field_name, table_output_field_name, table_output_key_field_flag
+        '''
+
+        query = '''SELECT
+                        CASE WHEN table_input_edw_field_definition.source_edw_field_definition_dwid IS NULL THEN 0 ELSE 1 END as has_table_input_source_definition
+                        , table_input_edw_table_definition.database_name as table_input_database_name
+                        , table_input_edw_table_definition.schema_name as table_input_schema_name
+                        , table_input_edw_table_definition.table_name as table_input_table_name
+                        , table_input_edw_field_definition.field_name as table_input_field_name
+                        , table_output_edw_field_definition.field_source_calculation as table_output_field_source_calculation
+                    
+                        , table_input_edw_field_definition.field_source_calculation as table_input_field_source_calculation
+                        , CASE WHEN table_output_edw_field_definition.source_edw_join_tree_definition_dwid IS NULL THEN 0 ELSE 1 END as has_table_output_edw_join_tree_definition
+                        , table_input_join_definition.join_type
+                        , table_input_join_definition.join_condition
+                        , edw_table_definition.database_name as table_output_database_name
+                        , edw_table_definition.schema_name as table_output_schema_name
+                        , edw_table_definition.table_name as table_output_table_name
+                        , table_output_edw_field_definition.field_name as table_output_field_name
+                        , table_output_edw_field_definition.key_field_flag as table_output_key_field_flag
+                    FROM
+                        mdi.edw_table_definition
+                            JOIN mdi.edw_field_definition table_output_edw_field_definition ON edw_table_definition.dwid = table_output_edw_field_definition.edw_table_definition_dwid
+                            LEFT JOIN mdi.edw_field_definition table_input_edw_field_definition ON table_output_edw_field_definition.source_edw_field_definition_dwid = table_input_edw_field_definition.dwid
+                            LEFT JOIN mdi.edw_table_definition table_input_edw_table_definition ON table_input_edw_field_definition.edw_table_definition_dwid = table_input_edw_table_definition.dwid
+                            LEFT JOIN mdi.edw_join_tree_definition table_input_join_tree_definition ON table_output_edw_field_definition.source_edw_join_tree_definition_dwid = table_input_join_tree_definition.dwid
+                            LEFT JOIN mdi.edw_join_definition table_input_join_definition ON table_input_join_tree_definition.root_join_dwid = table_input_join_definition.dwid
+                    WHERE
+                        edw_table_definition.dwid = %s
+                        AND table_output_edw_field_definition.field_name not in ('dwid', 'data_source_dwid','data_source_integration_columns','data_source_integration_id','data_source_modified_datetime','edw_created_datetime', 'edw_modified_datetime', 'etl_batch_id') -- exclude these in the join if the table input field name is null?
+                    ORDER BY
+                        table_output_edw_field_definition.key_field_flag DESC
+                        , table_output_edw_field_definition.field_name ASC
+                        , has_table_input_source_definition;'''
+
+        return self.execute_parameterized_query(query, edw_table_definition_dwid)
+
+    def get_all_staging_tables(self, schema):
+        # returns a list of 3-tuples
+        #   if table has a primary key - [(name of table, key field name, version field name)]
+        #   if table is a 'type' table - [(name of table, code field name, null)]
+        rows = self.execute_select_query(f'''
+        SELECT t.relname AS name, a.attname as key_field, version.attname as version_field
+        FROM pg_class t
+         join pg_index ix on t.oid = ix.indrelid
+         join pg_class i on i.oid = ix.indexrelid
+         join pg_attribute a on a.attrelid = t.oid and a.attnum = ANY(ix.indkey)
+         join pg_namespace on t.relnamespace = pg_namespace.oid
+         join pg_attribute version on version.attrelid = t.oid AND version.attname = left(a.attname, length(a.attname)-3)||'version'
+        WHERE pg_namespace.nspname = 'staging_octane'
+          and t.relkind = 'r'
+          and (i.relname like '%_pkey' OR i.relname like 'pk_%')
+         -- 371
+        union all
+        select tables.table_name as name, 'code' as key_field, null as version_field
+        from information_schema.tables
+        join information_schema.columns col1 on tables.table_name = col1.table_name and tables.table_schema = col1.table_schema and col1.column_name = 'code'
+        join information_schema.columns col2 on tables.table_name = col2.table_name and tables.table_schema = col2.table_schema and col2.column_name = 'value'
+        where tables.table_name like '%type'
+            and tables.table_schema = 'staging_octane'
+        --425 (2 of these have 3 columns each)
+        ;
+        ''', 'staging')
+        return rows
+
+    def get_all_table_fields(self, schema, table):
+        columns = []
+        rows = self.execute_select_query(f'''
+            SELECT columns.column_name
+            FROM information_schema.columns
+            WHERE columns.table_schema = '{schema}'
+                AND columns.table_name = '{table}'
+            ;
+		''', 'staging')
+        for row in rows:
+            columns.append(row[0])
+        return columns
+
 
 class ETL_config:
     process_name = ""
@@ -110,8 +279,27 @@ class ETL_config:
 
 
 class DimensionETLCreator(ETL_config):
-    def __init__(self):
-        pass
+    def __init__(self, table_metadata: list, process_name: str, edw_connection: EDW):
+        if len(table_metadata) == 0:
+            raise(ValueError("table_metadata should contain at least one item in the list"))
+
+        # table: table_input_step/field and table_output_step/field
+        self.table_metadata = table_metadata
+        self.table_input_step_connection = "Staging DB Connection"
+        self.table_output_step_connection = "Staging DB Connection"
+        self.target_table_output_name = ""
+        self.source_key_field_table_name = ""
+
+        # table: process
+        self.process_name = process_name
+        self.process_description = f"Dimension ETL to populate {self.target_table_output_name} from history_octane.{self.source_key_field_table_name}"
+
+        # table: json_output_field
+        self.json_output_step_field = "self.main_pid"
+        self.state_machine_name = f""
+        self.state_machine_comment = self.process_description
+
+
 
 
 class Staging_to_History_ETL(ETL_config):
@@ -178,122 +366,6 @@ class Staging_to_History_ETL(ETL_config):
                          , self.state_machine_comment)
 
 
-class EDW:
-    database_name = ""
-    database_username = ""
-    database_password = ""
-    database_hostname = ""
-    database_port = ""
-
-    def __init__(self,
-                 db_name="config",
-                 db_username="postgres",
-                 db_password="testonly",
-                 db_hostname="localhost",
-                 db_port="5432"):
-        self.database_name = db_name
-        self.database_username = db_username
-        self.database_password = db_password
-        self.database_hostname = db_hostname
-        self.database_port = db_port
-
-    def execute_select_query(self, query: str, database=database_name):
-        conn = psycopg2.connect(database=database,
-                                user=self.database_username,
-                                password=self.database_password,
-                                host=self.database_hostname,
-                                port=self.database_port)
-        cur = conn.cursor()
-        cur.execute(query)
-        rows = cur.fetchall()
-        conn.close()
-        return rows
-
-    def execute_parameterized_query(self, query: str, *parameters):
-        conn = psycopg2.connect(database=self.database_name,
-                                user=self.database_username,
-                                password=self.database_password,
-                                host=self.database_hostname,
-                                port=self.database_port)
-        cur = conn.cursor()
-        cur.execute(query, parameters)
-        conn.commit()
-        rows = cur.fetchall()
-        conn.close()
-        return rows
-
-    def get_table_list_from_edw_table_definition(self, schema_name) -> list:
-        query = '''SELECT
-                        dwid
-                        , database_name
-                        , schema_name
-                        , table_name
-                        , primary_source_edw_table_definition_dwid
-                    FROM
-                        mdi.edw_table_definition
-                    WHERE
-                        schema_name = %s
-                    ORDER BY
-                        schema_name ASC;'''
-        return self.execute_parameterized_query(query, schema_name)
-
-    def get_field_list_from_edw_field_definition(self, edw_table_definition_dwid: int) -> list:
-        query = '''SELECT
-                        edw_table_definition.database_name
-                        , edw_table_definition.schema_name
-                        , edw_table_definition.table_name
-                        , edw_field_definition.field_name
-                        , edw_field_definition.key_field_flag
-                    FROM
-                        mdi.edw_table_definition
-                        JOIN mdi.edw_field_definition on edw_table_definition.dwid = edw_field_definition.edw_table_definition_dwid
-                    WHERE
-                        edw_table_definition.dwid = %s
-                    ORDER BY
-                        edw_field_definition.key_field_flag DESC
-                        , edw_field_definition.field_name ASC
-                    ;'''
-        return self.execute_parameterized_query(query, edw_table_definition_dwid)
-
-    def get_all_staging_tables(self, schema):
-        rows = self.execute_select_query(f'''
-        SELECT t.relname AS name, a.attname as key_field, version.attname as version_field
-        FROM pg_class t
-         join pg_index ix on t.oid = ix.indrelid
-         join pg_class i on i.oid = ix.indexrelid
-         join pg_attribute a on a.attrelid = t.oid and a.attnum = ANY(ix.indkey)
-         join pg_namespace on t.relnamespace = pg_namespace.oid
-         join pg_attribute version on version.attrelid = t.oid AND version.attname = left(a.attname, length(a.attname)-3)||'version'
-        WHERE pg_namespace.nspname = 'staging_octane'
-          and t.relkind = 'r'
-          and (i.relname like '%_pkey' OR i.relname like 'pk_%')
-         -- 371
-        union all
-        select tables.table_name as name, 'code' as key_field, null as version_field
-        from information_schema.tables
-        join information_schema.columns col1 on tables.table_name = col1.table_name and tables.table_schema = col1.table_schema and col1.column_name = 'code'
-        join information_schema.columns col2 on tables.table_name = col2.table_name and tables.table_schema = col2.table_schema and col2.column_name = 'value'
-        where tables.table_name like '%type'
-            and tables.table_schema = 'staging_octane'
-        --425 (2 of these have 3 columns each)
-        ;
-        ''', 'staging')
-        return rows
-
-    def get_all_table_fields(self, schema, table):
-        columns = []
-        rows = self.execute_select_query(f'''
-            SELECT columns.column_name
-            FROM information_schema.columns
-            WHERE columns.table_schema = '{schema}'
-                AND columns.table_name = '{table}'
-            ;
-		''', 'staging')
-        for row in rows:
-            columns.append(row[0])
-        return columns
-
-
 def main(argv):
     script_name = argv[0]
     argv = argv[1:]
@@ -324,15 +396,19 @@ def generate_mdi_configs_based_on_table_definition(schema_name_to_process: str) 
     edw = EDW(db_name="config")
     table_list = edw.get_table_list_from_edw_table_definition(schema_name_to_process)
 
-    for table in table_list:
+    for index, table in enumerate(table_list, start=200000):
         edw_table_definition_dwid = table[0]
         database_name = table[1]
         schema_name = table[2]
         table_name = table[3]
         primary_source_edw_table_definition_dwid = table[4]
 
-        fields = edw.get_field_list_from_edw_field_definition(primary_source_edw_table_definition_dwid)
-        pass
+        process_name = f"SP-{index}"
+
+        fields = edw.get_field_list_from_edw_field_definition(edw_table_definition_dwid)
+        etl_config = DimensionETLCreator(fields, process_name, EDW(db_name="staging"))
+
+        print("")
 
 def generate_mdi_configs_based_on_information_schema():
     edw = EDW(db_name="staging")
