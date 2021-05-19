443c47cfc1a01d735feabc54271d9e29065d0bf1
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-05-19 14:49:00 -0400

    [#1200245179995890] removed unneeded classes/functions

--- scripts/etl-creator/initial-staging-history-octane-etl-creator.py
+++ scripts/etl-creator/initial-staging-history-octane-etl-creator.py
@@ -267,178 +267,7 @@ ORDER BY
             columns.append(row[0])
         return columns
 
-
-class ETL_config:
-    process_name = ""
-    process_description = ""
-    table_input_step_sql = ""
-    table_input_step_connection = ""
-    table_output_step_schema = ""
-    table_output_step_table = ""
-    table_output_step_connection = ""
-    table_output_step_fields = []
-    json_output_step_field = ""
-    staging_connection = ""
-    state_machine_name = ""
-    state_machine_comment = ""
-
-    process_dwid = ""
-    table_input_step_dwid = ""
-    table_output_step_dwid = ""
-    json_output_field_dwid = ""
-
-    process_insert = ""
-    table_input_step_insert = ""
-    table_output_step_insert = ""
-    table_output_field_steps = ""
-
-    def __init__ (self
-                  , process_name: str
-                  , process_description: str
-                  , table_input_step_connection: str
-                  , table_input_step_sql: str
-                  , table_output_step_connection: str
-                  , table_output_step_schema: str
-                  , table_output_step_table: str
-                  , table_output_step_fields: list
-                  , json_output_step_field: str
-                  , staging_connection: str
-                  , state_machine_name: str
-                  , state_machine_comment: str):
-        self.process_name = process_name
-        self.process_description = process_description
-        self.table_input_step_connection = table_input_step_connection
-        self.table_input_step_sql = table_input_step_sql
-        self.table_output_step_connection = table_output_step_connection
-        self.table_output_step_schema = table_output_step_schema
-        self.table_output_step_table = table_output_step_table
-        self.table_output_step_fields = table_output_step_fields
-        self.json_output_step_field = json_output_step_field
-        self.staging_connection = staging_connection
-        self.state_machine_name = state_machine_name
-        self.state_machine_comment = state_machine_comment
-
-    def create_config_insert_statements(self):
-        config_insert = ""
-        config_insert += f'''
-            -- The following statement adds a configuration for '{self.table_output_step_table}'
-            with temp_process as (INSERT INTO mdi.process (name, description) 
-                VALUES ('{self.process_name}', '{self.process_description}')
-                RETURNING dwid 
-            )'''
-        config_insert += f'''
-            , temp_table_input_step as (INSERT INTO mdi.table_input_step (process_dwid, data_source_dwid, sql, limit_size, connectionname) 
-                select temp_process.dwid, 0, '{self.table_input_step_sql}', 0, '{self.table_input_step_connection}'
-                from temp_process
-                RETURNING dwid 
-            )'''
-        config_insert += f'''
-            , temp_table_output_step as (INSERT INTO mdi.table_output_step (process_dwid, target_schema, target_table, commit_size, partitioning_field, table_name_field, auto_generated_key_field, partition_data_per, 
-                table_name_defined_in_field, return_auto_generated_key_field, truncate_table, connectionname, partition_over_tables, specify_database_fields, ignore_insert_errors, use_batch_update) 
-                SELECT temp_process.dwid, '{self.table_output_step_schema}', '{self.table_output_step_table}', 1000, NULL, NULL, NULL, NULL
-                , 'N', NULL, 'N', '{self.table_output_step_connection}', 'N', 'Y', 'N', 'N'
-                FROM temp_process
-                RETURNING dwid
-            )'''
-        self.table_output_step_fields.append('data_source_updated_datetime')
-        self.table_output_step_fields.append('data_source_deleted_flag')
-        config_insert +=  f'''
-            , temp_table_output_field as (INSERT INTO mdi.table_output_field (
-                table_output_step_dwid, database_field_name, database_stream_name, field_order, 
-                is_sensitive 
-                ) '''
-        config_insert_field = []
-        for i, field in enumerate(self.table_output_step_fields):
-                config_insert_field.append( f'''     
-                    SELECT temp_table_output_step.dwid, '{field}', '{field}', {i}, False
-                    from temp_table_output_step
-                ''')
-        config_insert += ' UNION ALL '.join(config_insert_field)
-        config_insert += ')'
-        config_insert += f'''
-            , temp_json_output as (INSERT INTO mdi.json_output_field (process_dwid, field_name) 
-                select temp_process.dwid, '{self.json_output_step_field}'
-                from temp_process)'''
-
-        config_insert += f'''
-            , temp_state_machine_definition as (INSERT INTO mdi.state_machine_definition (process_dwid, name, comment)
-                SELECT temp_process.dwid, '{self.state_machine_name}', '{self.state_machine_comment}'
-                from temp_process)'''
-
-        config_insert += f'''
-            INSERT INTO mdi.state_machine_step (process_dwid, next_process_dwid)
-                SELECT temp_process.dwid, NULL
-                from temp_process;
-
-            '''
-        return config_insert
-
-
-class Staging_to_History_ETL(ETL_config):
-    def __init__(self, staging_table_metadata, process_name, staging_connection):
-        self.staging_table_metadata = staging_table_metadata
-        self.staging_table_name = self.staging_table_metadata[0]
-        self.process_name = process_name
-        self.main_pid = self.staging_table_metadata[1]
-        self.version_pid = self.staging_table_metadata[2]
-        self.process_description = f'ETL to copy {self.staging_table_name} data from staging_octane to history_octane'
-        self.table_input_step_connection = 'Staging DB Connection'
-        self.table_output_step_fields = staging_connection.get_all_table_fields("staging_octane", self.staging_table_name)
-        if self.version_pid is None:
-            join_condition = ""
-            for i, field in enumerate(self.table_output_step_fields):
-                if(i>0):
-                    join_condition +=' AND '
-                join_condition +='staging_table.'+field+' = history_table.'+field
-            self.table_input_step_sql = f'''
-            SELECT staging_table.{', staging_table.'.join(self.table_output_step_fields)}, FALSE as data_source_deleted_flag, now() AS data_source_updated_datetime
-            FROM staging_octane.{self.staging_table_name} staging_table
-            LEFT JOIN history_octane.{self.staging_table_name} history_table on {join_condition}
-            WHERE history_table.{self.main_pid} is NULL
-            '''
-        else:
-            list_of_fields = ""
-            for i, field in enumerate(self.table_output_step_fields):
-                if(i>0):
-                    list_of_fields +=', '
-                list_of_fields += 'history_table.'+field
-                if(field==self.version_pid):
-                    list_of_fields += '+1'
-            self.table_input_step_sql = f'''
-            --finding records to insert into hitory_octane.{self.staging_table_name}
-            SELECT staging_table.{', staging_table.'.join(self.table_output_step_fields)}, FALSE as data_source_deleted_flag, now() AS data_source_updated_datetime
-            FROM staging_octane.{self.staging_table_name} staging_table
-            LEFT JOIN history_octane.{self.staging_table_name} history_table on staging_table.{self.main_pid} = history_table.{self.main_pid} and staging_table.{self.version_pid} = history_table.{self.version_pid}
-            WHERE history_table.{self.main_pid} is NULL
-            UNION ALL
-            SELECT {list_of_fields}, TRUE as data_source_deleted_flag, now() AS data_source_updated_datetime
-            FROM history_octane.{self.staging_table_name} history_table
-            LEFT JOIN staging_octane.{self.staging_table_name} staging_table on staging_table.{self.main_pid} = history_table.{self.main_pid}
-            WHERE staging_table.{self.main_pid} is NULL
-                AND not exists (select 1 from history_octane.{self.staging_table_name} deleted_records where deleted_records.{self.main_pid} = history_table.{self.main_pid} and deleted_records.data_source_deleted_flag = True)
-            '''
-        self.table_output_step_connection = 'Staging DB Connection'
-        self.table_output_step_schema = "history_octane"
-        self.table_output_step_table = self.staging_table_name
-        self.staging_connection = staging_connection
-        self.json_output_step_field = self.main_pid
-        self.state_machine_name = f'Octane {self.staging_table_name}'
-        self.state_machine_comment = self.process_description
-        super().__init__(self.process_name
-                         , self.process_description
-                         , self.table_input_step_connection
-                         , self.table_input_step_sql
-                         , self.table_output_step_connection
-                         , self.table_output_step_schema
-                         , self.table_output_step_table
-                         , self.table_output_step_fields
-                         , self.json_output_step_field
-                         , self.staging_connection
-                         , self.state_machine_name
-                         , self.state_machine_comment)
-
-
-class DimensionETLCreator():
+class ETL_creator():
     def __init__(self, field_metadata: list, process_name: str, edw_connection: EDW, insert_update_table_name: str,
                  edw_table_definition_dwid: int, table_input_step_connection: str = "Staging DB Connection",
                  insert_update_step_connection: str = "Staging DB Connection",
@@ -660,13 +489,7 @@ class DimensionETLCreator():
 {self.indent*5}history_records.{child_join_details[0]["target_field_name"]} IS NULL
 {self.indent*3}) AS t{child_join_details[0]["child_join_tree_definition_root_join_dwid"]} ON {child_join_details[0]["join_condition"]}
 {self.indent*3}-- child join end    
-    
-    
-    
-    
-    
-    
-    '''
+'''
         return child_join_query_template
 
     def has_child_join(self, field_definition: dict) -> bool:
@@ -678,6 +501,8 @@ class DimensionETLCreator():
         elif len(child_join_data) == 1:
             return True
         else:
+            print(f'Unexpected error occurred. Expected child_join_data to have 0 or 1 rows but {len(child_join_data)} rows were found for edw_join_definition.dwid={field_definition["join_alias"]}.')
+            print(field_definition)
             raise ValueError("Expected child_join_data to have 0 or 1 rows but more than 1 row was found. Script is not written to handle this scenario.")
 
 
@@ -851,7 +676,7 @@ def main(argv):
     argv = argv[1:]
 
     try:
-        opts, args = getopt.getopt(argv, "ht:i",["help","table","information_schema"])
+        opts, args = getopt.getopt(argv, "ht:",["help","information_schema"])
     except getopt.GetopsError:
         display_usage(script_name)
         exit(-1)
@@ -859,15 +684,12 @@ def main(argv):
         if opt in ("-h", "--h", "-help", "--help"):
             display_usage(script_name)
             exit(0)
-        elif opt in ("-i", "--i", "-information_schema", "--information_schema"):
-            generate_mdi_configs_based_on_information_schema()
         elif opt in ("-t", "--t", "-table_definition", "--table_definition"):
             generate_mdi_configs_based_on_table_definition(arg)
 
 def display_usage(script_name: str) -> None:
     print(f'''{script_name} script usage:
     -h      print usage information
-    -i      create configs based on staging db information_schema
     -t <schema name>   create configs based on mdi.edw_table_definition''')
 
 def generate_mdi_configs_based_on_table_definition(schema_name_to_process: str) -> None:
@@ -887,7 +709,7 @@ def generate_mdi_configs_based_on_table_definition(schema_name_to_process: str) 
         process_name = f"SP-{index}"
 
         fields = edw.get_field_list_from_edw_field_definition(edw_table_definition_dwid)
-        etl_config = DimensionETLCreator(field_metadata=fields,
+        etl_config = ETL_creator(field_metadata=fields,
                                          process_name=process_name,
                                          edw_connection=EDW(db_name=database_name),
                                          insert_update_table_name = table_name,
@@ -895,18 +717,6 @@ def generate_mdi_configs_based_on_table_definition(schema_name_to_process: str) 
                                          edw_table_definition_dwid=edw_table_definition_dwid)
         sql_configuration = etl_config.create_table_input_to_insert_update_sql()
         print(sql_configuration)
-
-def generate_mdi_configs_based_on_information_schema():
-    edw = EDW(db_name="staging")
-    staging_tables = edw.get_all_staging_tables('staging_octane')
-    full_config_insert_script = ""
-    for i, staging_table_metadata in enumerate(staging_tables, start=100001):
-        etl_config = Staging_to_History_ETL(staging_table_metadata, f'SP-{i}', edw)
-        config_insert = etl_config.create_config_insert_statements()
-        full_config_insert_script+=config_insert # ********* This is the main output of this script *********
-    f = open("config_insert_"+date.today().strftime("%Y%m%d")+".sql", "w")
-    f.write(full_config_insert_script)
-    f.close()
 
 if __name__ == "__main__":
     main(sys.argv)
