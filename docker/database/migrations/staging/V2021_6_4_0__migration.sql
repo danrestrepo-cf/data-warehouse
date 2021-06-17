08608646bfa2ef95daeff8d4b69e10d4ad2e38c3
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-06-17 16:39:43 -0400

    [#1200416935542073] Updates per 2021.06.17 code review

--- docker/database/migrations/staging/V2021_6_4_0__migration.sql
+++ docker/database/migrations/staging/V2021_6_4_0__migration.sql
@@ -28,6 +28,11 @@ DO $$
                    ', data_source_integration_id = CONCAT(data_source_integration_id, ''~0'')' ||
                    ', edw_modified_datetime = NOW()' ||
                    ', data_source_modified_datetime = NOW()' ||
+                    /*
+                    Below WHERE uses data_source_integration_id to:
+                        - ensure only records with invalid data_source_integration_id values are updated
+                        - avoid hardcoded dwid usage
+                    */
                    ' WHERE data_source_integration_id = ''0''' AS update_statement
             FROM information_schema.tables
             WHERE table_schema = 'star_loan'
@@ -44,10 +49,21 @@ DO $$
 UPDATE star_loan.hmda_purchaser_of_loan_dim
     SET data_source_integration_columns = 'code_2017~code_2018'
         , data_source_integration_id = '~'
-    WHERE dwid = 0;
+    /*
+    Below WHERE uses data_source_integration_columns and data_source_integration_id to:
+        - ensure the only record with invalid values in these fields is updated
+        - avoid hardcoded dwid usage
+    */
+    WHERE data_source_integration_columns = 'code~value~year'
+        AND data_source_integration_id = '~~';
 
 -- Update the data_source_integration_columns field value for the star_loan.borrower_lending_profile_dim table to
 -- replace the invalid “|” character with a valid “~” character
 UPDATE star_loan.borrower_lending_profile_dim
     SET data_source_integration_columns = REPLACE(data_source_integration_columns, '|', '~')
-    WHERE dwid = 0;
+    /*
+    Below WHERE uses data_source_integration_columns to:
+        - ensure only the record with invalid value in data_source_integration_columns is updated
+        - avoid hardcoded dwid usage
+    */
+    WHERE data_source_integration_columns LIKE '%|%';
