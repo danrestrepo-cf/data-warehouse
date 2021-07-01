9a995c2dfc662f76e1d4b4596020f794d85d6a1f
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-07-01 13:40:43 -0400

    [#1200548658779340] Updates per 2021.07.01 code review

--- docker/database/migrations/config/V2021_7_1_0__migration.sql
+++ docker/database/migrations/config/V2021_7_1_0__migration.sql
@@ -15,6 +15,21 @@ INSERT INTO mdi.insert_update_field (insert_update_step_dwid, update_lookup, upd
         AND edw_table_definition.table_name = insert_update_step.table_name
              LEFT JOIN mdi.insert_update_field ON insert_update_step.dwid = insert_update_field.insert_update_step_dwid
         AND edw_field_definition.field_name = insert_update_field.update_lookup
-    WHERE edw_field_definition.field_name <> 'dwid'
-      AND insert_update_step.schema_name = 'star_loan'
-      AND insert_update_field.dwid IS NULL;
+    WHERE insert_update_step.schema_name = 'star_loan'
+      AND edw_field_definition.field_name IN ('race_other_pacific_islander_description_flag'
+        , 'piggyback_flag'
+        , 'party_to_lawsuit_explanation_flag'
+        , 'ethnicity_other_hispanic_or_latino_description_flag'
+        , 'borrowed_down_payment_explanation_flag'
+        , 'race_other_american_indian_or_alaska_native_description_flag'
+        , 'note_endorser_explanation_flag'
+        , 'other_race_national_origin_description_flag'
+        , 'race_other_asian_description_flag'
+        , 'obligated_loan_foreclosure_explanation_flag'
+        , 'outstanding_judgments_explanation_flag'
+        , 'los_loan_number'
+        , 'presently_delinquent_explanation_flag'
+        , 'bankruptcy_explanation_flag'
+        , 'property_foreclosure_explanation_flag'
+        , 'alimony_child_support_explanation_flag'
+        );
