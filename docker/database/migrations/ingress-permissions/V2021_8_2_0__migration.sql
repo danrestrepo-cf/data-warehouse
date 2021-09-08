93a55063afa282cb56aa6f2581604e00e8f78e73
Derek Kammerer <derek.kammerer@cardinalfinancial.com>
2021-09-08 16:02:51 -0400

    Correct usage of WITH GRANT OPTION implemented

--- docker/database/migrations/ingress-permissions/V2021_8_2_0__migration.sql
+++ docker/database/migrations/ingress-permissions/V2021_8_2_0__migration.sql
@@ -39,4 +39,4 @@ ALTER DEFAULT PRIVILEGES FOR ROLE deployer IN SCHEMA warehouse_banks GRANT SELEC
 GRANT USAGE, CREATE ON SCHEMA warehouse_banks to deployer;
 
 -- admin permissions
-GRANT USAGE ON SCHEMA warehouse_banks to admin;
+GRANT USAGE ON SCHEMA warehouse_banks to admin WITH GRANT OPTION;
