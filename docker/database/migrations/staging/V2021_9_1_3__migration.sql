4d9449e31904be47ccf8266350173530fe04931e
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-09-14 10:22:16 -0400

    [#1200975391827399] Bringing index names to convetion
    [#1200975391827399] idx_ prefix, additional underscore

--- docker/database/migrations/staging/V2021_9_1_3__migration.sql
+++ docker/database/migrations/staging/V2021_9_1_3__migration.sql
@@ -3,22 +3,39 @@
 -- https://app.asana.com/0/0/1200975391827399
 --
 
-CREATE INDEX application_dim_data_source_integration_id ON star_loan.application_dim USING btree(data_source_integration_id);
-CREATE INDEX borrower_demographics_dim_data_source_integration_id ON star_loan.borrower_demographics_dim USING btree(data_source_integration_id);
-CREATE INDEX borrower_dim_data_source_integration_id ON star_loan.borrower_dim USING btree(data_source_integration_id);
-CREATE INDEX borrower_lending_profile_dim_data_source_integration_id ON star_loan.borrower_lending_profile_dim USING btree(data_source_integration_id);
-CREATE INDEX hmda_purchaser_of_loan_dim_data_source_integration_id ON star_loan.hmda_purchaser_of_loan_dim USING btree(data_source_integration_id);
-CREATE INDEX interim_funder_dim_data_source_integration_id ON star_loan.interim_funder_dim USING btree(data_source_integration_id);
-CREATE INDEX investor_dim_data_source_integration_id ON star_loan.investor_dim USING btree(data_source_integration_id);
-CREATE INDEX lender_user_dim_data_source_integration_id ON star_loan.lender_user_dim USING btree(data_source_integration_id);
-CREATE INDEX loan_beneficiary_dim_data_source_integration_id ON star_loan.loan_beneficiary_dim USING btree(data_source_integration_id);
-CREATE INDEX loan_dim_data_source_integration_id ON star_loan.loan_dim USING btree(data_source_integration_id);
-CREATE INDEX loan_fact_data_source_integration_id ON star_loan.loan_fact USING btree(data_source_integration_id);
-CREATE INDEX loan_funding_dim_data_source_integration_id ON star_loan.loan_funding_dim USING btree(data_source_integration_id);
-CREATE INDEX loan_junk_dim_data_source_integration_id ON star_loan.loan_junk_dim USING btree(data_source_integration_id);
-CREATE INDEX mortgage_insurance_dim_data_source_integration_id ON star_loan.mortgage_insurance_dim USING btree(data_source_integration_id);
-CREATE INDEX product_choice_dim_data_source_integration_id ON star_loan.product_choice_dim USING btree(data_source_integration_id);
-CREATE INDEX product_dim_data_source_integration_id ON star_loan.product_dim USING btree(data_source_integration_id);
-CREATE INDEX product_terms_dim_data_source_integration_id ON star_loan.product_terms_dim USING btree(data_source_integration_id);
-CREATE INDEX transaction_dim_data_source_integration_id ON star_loan.transaction_dim USING btree(data_source_integration_id);
-CREATE INDEX transaction_junk_dim_data_source_integration_id ON star_loan.transaction_junk_dim USING btree(data_source_integration_id);
+CREATE INDEX idx_application_dim__data_source_integration_id ON star_loan.application_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_borrower_demographics_dim__data_source_integration_id ON star_loan.borrower_demographics_dim USING
+    btree(data_source_integration_id);
+CREATE INDEX idx_borrower_dim__data_source_integration_id ON star_loan.borrower_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_borrower_lending_profile_dim__data_source_integration_id ON star_loan.borrower_lending_profile_dim
+    USING btree(data_source_integration_id);
+CREATE INDEX idx_hmda_purchaser_of_loan_dim__data_source_integration_id ON star_loan.hmda_purchaser_of_loan_dim USING
+    btree(data_source_integration_id);
+CREATE INDEX idx_interim_funder_dim__data_source_integration_id ON star_loan.interim_funder_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_investor_dim__data_source_integration_id ON star_loan.investor_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_lender_user_dim__data_source_integration_id ON star_loan.lender_user_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_loan_beneficiary_dim__data_source_integration_id ON star_loan.loan_beneficiary_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_loan_dim__data_source_integration_id ON star_loan.loan_dim USING btree(data_source_integration_id);
+CREATE INDEX idx_loan_fact__data_source_integration_id ON star_loan.loan_fact USING btree(data_source_integration_id);
+CREATE INDEX idx_loan_funding_dim__data_source_integration_id ON star_loan.loan_funding_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_loan_junk_dim__data_source_integration_id ON star_loan.loan_junk_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_mortgage_insurance_dim__data_source_integration_id ON star_loan.mortgage_insurance_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_product_choice_dim__data_source_integration_id ON star_loan.product_choice_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_product_dim__data_source_integration_id ON star_loan.product_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_product_terms_dim__data_source_integration_id ON star_loan.product_terms_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_transaction_dim__data_source_integration_id ON star_loan.transaction_dim USING btree
+    (data_source_integration_id);
+CREATE INDEX idx_transaction_junk_dim__data_source_integration_id ON star_loan.transaction_junk_dim USING btree
+    (data_source_integration_id);
