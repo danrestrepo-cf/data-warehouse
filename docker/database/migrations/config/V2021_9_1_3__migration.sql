880dad1bc764be1839182f9458c0b684c0b5e9b4
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-09-14 15:24:06 -0400

    [#1200970669432550] undo autoformatting changes to previous PR code

--- docker/database/migrations/config/V2021_9_1_3__migration.sql
+++ docker/database/migrations/config/V2021_9_1_3__migration.sql
@@ -3,14 +3,12 @@
 -- https://app.asana.com/0/0/1200975391827399
 --
 
-DELETE
-FROM mdi.insert_update_key
+DELETE FROM mdi.insert_update_key
 WHERE dwid = (
     SELECT insert_update_key.dwid
     FROM mdi.insert_update_step
-    JOIN mdi.insert_update_key
-         ON insert_update_step.dwid = insert_update_key.insert_update_step_dwid
-             AND insert_update_key.key_lookup = 'data_source_dwid'
+             JOIN mdi.insert_update_key ON insert_update_step.dwid = insert_update_key.insert_update_step_dwid
+        AND insert_update_key.key_lookup = 'data_source_dwid'
     WHERE insert_update_step.table_name = 'loan_fact'
 );
 
@@ -20,9 +18,8 @@ SET key_lookup = 'data_source_integration_id'
 WHERE dwid = (
     SELECT insert_update_key.dwid
     FROM mdi.insert_update_step
-    JOIN mdi.insert_update_key
-         ON insert_update_step.dwid = insert_update_key.insert_update_step_dwid
-             AND insert_update_key.key_lookup = 'loan_pid'
+             JOIN mdi.insert_update_key ON insert_update_step.dwid = insert_update_key.insert_update_step_dwid
+        AND insert_update_key.key_lookup = 'loan_pid'
     WHERE insert_update_step.table_name = 'loan_fact'
 );
 
