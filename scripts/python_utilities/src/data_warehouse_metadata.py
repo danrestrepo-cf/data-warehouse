753c6c8c11048474dc113308682f7f81d0f5a84e
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-10-05 10:41:11 -0400

    [#1200845872653274] complete first iteration of metadata class family

--- scripts/python_utilities/src/data_warehouse_metadata.py
+++ scripts/python_utilities/src/data_warehouse_metadata.py
@@ -1,22 +1,114 @@
+from enum import Enum
+from dataclasses import dataclass
 from typing import List
 
 
+@dataclass
+class TableAddress:
+    database: str
+    schema: str
+    table: str
+
+
+@dataclass
+class ForeignColumnPath:
+    fk_steps: List[str]
+    column_name: str
+
+
+@dataclass
+class ForeignKeyMetadata:
+    name: str
+    table: TableAddress
+    native_columns: List[str]
+    foreign_columns: List[str]
+
+
+class ETLDataSource(Enum):
+    OCTANE = 1
+
+
+class ETLInputType(Enum):
+    TABLE = 'table'
+
+
+class ETLOutputType(Enum):
+    TABLE = 'table'
+
+
+@dataclass
 class ETLMetadata:
-    pass
+    process_name: str
+    hardcoded_data_source: ETLDataSource = ETLDataSource.OCTANE
+    input_type: ETLInputType = ETLInputType.TABLE
+    output_type: ETLOutputType = ETLOutputType.TABLE
+    json_output_field: str = None
+    truncate_table: bool = None
+    insert_update_keys: List[str] = None
+    delete_keys: List[str] = None
+    input_sql: str = None
 
 
+@dataclass
 class ColumnMetadata:
-    pass
+    name: str
+    data_type: str = None
+    source_field: ForeignColumnPath = None
 
 
 class TableMetadata:
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, primary_source_table: TableAddress = None):
         self.name = name
+        self.primary_source_table = primary_source_table
+        self.next_etls = []
+        self._columns = {}
+        self._etls = {}
+        self._foreign_keys = {}
 
-    def __eq__(self, other: 'TableMetadata'):
+    def add_column(self, column: ColumnMetadata):
+        self._columns[column.name] = column
+
+    def add_etl(self, etl: ETLMetadata):
+        self._etls[etl.process_name] = etl
+
+    def add_foreign_key(self, foreign_key: ForeignKeyMetadata):
+        self._foreign_keys[foreign_key.name] = foreign_key
+
+    def get_column(self, column_name: str) -> ColumnMetadata:
+        if column_name not in self._columns:
+            raise InvalidMetadataKeyException('table', self.name, 'column', column_name)
+        else:
+            return self._columns[column_name]
+
+    def get_etl(self, etl_process_name: str) -> ETLMetadata:
+        if etl_process_name not in self._etls:
+            raise InvalidMetadataKeyException('table', self.name, 'etl', etl_process_name)
+        else:
+            return self._etls[etl_process_name]
+
+    def get_foreign_key(self, foreign_key_name: str) -> ETLMetadata:
+        if foreign_key_name not in self._foreign_keys:
+            raise InvalidMetadataKeyException('table', self.name, 'foreign_key', foreign_key_name)
+        else:
+            return self._foreign_keys[foreign_key_name]
+
+    @property
+    def columns(self) -> List[ColumnMetadata]:
+        return list(self._columns.values())
+
+    @property
+    def etls(self) -> List[ETLMetadata]:
+        return list(self._etls.values())
+
+    @property
+    def foreign_keys(self) -> List[ForeignKeyMetadata]:
+        return list(self._foreign_keys.values())
+
+    def __eq__(self, other: 'TableMetadata') -> bool:
         return isinstance(other, TableMetadata) and \
-               self.name == other.name
+               self.name == other.name and \
+               self.primary_source_table == other.primary_source_table
 
 
 class SchemaMetadata:
@@ -25,12 +117,12 @@ class SchemaMetadata:
         self.name = name
         self._tables = {}
 
-    def add_table_metadata(self, table: TableMetadata):
+    def add_table(self, table: TableMetadata):
         self._tables[table.name] = table
 
-    def get_table_metadata(self, table_name: str) -> TableMetadata:
+    def get_table(self, table_name: str) -> TableMetadata:
         if table_name not in self._tables:
-            raise InvalidMetadataKeyException('database', self.name, 'schema', table_name)
+            raise InvalidMetadataKeyException('schema', self.name, 'table', table_name)
         else:
             return self._tables[table_name]
 
@@ -38,7 +130,7 @@ class SchemaMetadata:
     def tables(self) -> List[TableMetadata]:
         return list(self._tables.values())
 
-    def __eq__(self, other: 'SchemaMetadata'):
+    def __eq__(self, other: 'SchemaMetadata') -> bool:
         return isinstance(other, SchemaMetadata) and \
                self.name == other.name and \
                self._tables == other._tables
@@ -50,10 +142,10 @@ class DatabaseMetadata:
         self.name = name
         self._schemas = {}
 
-    def add_schema_metadata(self, schema: SchemaMetadata):
+    def add_schema(self, schema: SchemaMetadata):
         self._schemas[schema.name] = schema
 
-    def get_schema_metadata(self, schema_name: str) -> SchemaMetadata:
+    def get_schema(self, schema_name: str) -> SchemaMetadata:
         if schema_name not in self._schemas:
             raise InvalidMetadataKeyException('database', self.name, 'schema', schema_name)
         else:
@@ -63,7 +155,7 @@ class DatabaseMetadata:
     def schemas(self) -> List[SchemaMetadata]:
         return list(self._schemas.values())
 
-    def __eq__(self, other: 'DatabaseMetadata'):
+    def __eq__(self, other: 'DatabaseMetadata') -> bool:
         return isinstance(other, DatabaseMetadata) and \
                self.name == other.name and \
                self._schemas == other._schemas
@@ -75,10 +167,10 @@ class DataWarehouseMetadata:
         self.name = name
         self._databases = {}
 
-    def add_database_metadata(self, database: DatabaseMetadata):
+    def add_database(self, database: DatabaseMetadata):
         self._databases[database.name] = database
 
-    def get_database_metadata(self, database_name: str) -> DatabaseMetadata:
+    def get_database(self, database_name: str) -> DatabaseMetadata:
         if database_name not in self._databases:
             raise InvalidMetadataKeyException('data warehouse', self.name, 'database', database_name)
         else:
@@ -88,7 +180,7 @@ class DataWarehouseMetadata:
     def databases(self) -> List[DatabaseMetadata]:
         return list(self._databases.values())
 
-    def __eq__(self, other: 'DataWarehouseMetadata'):
+    def __eq__(self, other: 'DataWarehouseMetadata') -> bool:
         return isinstance(other, DataWarehouseMetadata) and \
                self.name == other.name and \
                self._databases == other._databases
--- scripts/python_utilities/tests/test_data_warehouse_metadata.py
+++ scripts/python_utilities/tests/test_data_warehouse_metadata.py
@@ -6,6 +6,9 @@ from data_warehouse_metadata import (DataWarehouseMetadata,
                                      SchemaMetadata,
                                      TableMetadata,
                                      ColumnMetadata,
+                                     ETLMetadata,
+                                     ForeignKeyMetadata,
+                                     ForeignColumnPath,
                                      InvalidMetadataKeyException)
 
 
@@ -14,18 +17,18 @@ class TestDataWarehouseMetadata(unittest.TestCase):
     def test_throws_error_if_user_tries_to_get_database_that_doesnt_exist(self):
         dw_metadata = DataWarehouseMetadata('edw')
         with self.assertRaises(InvalidMetadataKeyException):
-            dw_metadata.get_database_metadata('staging')
+            dw_metadata.get_database('staging')
 
     def test_can_get_database_metadata_by_database_name_if_the_database_has_been_added_to_the_metadata(self):
         dw_metadata = DataWarehouseMetadata('edw')
-        dw_metadata.add_database_metadata(DatabaseMetadata('staging'))
-        self.assertEqual(DatabaseMetadata('staging'), dw_metadata.get_database_metadata('staging'))
+        dw_metadata.add_database(DatabaseMetadata('staging'))
+        self.assertEqual(DatabaseMetadata('staging'), dw_metadata.get_database('staging'))
 
     def test_can_iterate_through_all_added_databases(self):
         dw_metadata = DataWarehouseMetadata('edw')
-        dw_metadata.add_database_metadata(DatabaseMetadata('staging'))
-        dw_metadata.add_database_metadata(DatabaseMetadata('config'))
-        dw_metadata.add_database_metadata(DatabaseMetadata('ingress'))
+        dw_metadata.add_database(DatabaseMetadata('staging'))
+        dw_metadata.add_database(DatabaseMetadata('config'))
+        dw_metadata.add_database(DatabaseMetadata('ingress'))
         expected = [DatabaseMetadata('staging'), DatabaseMetadata('config'), DatabaseMetadata('ingress')]
         self.assertEqual(expected, [database for database in dw_metadata.databases])
 
@@ -35,18 +38,18 @@ class TestDatabaseMetadata(unittest.TestCase):
     def test_throws_error_if_user_tries_to_get_schema_that_doesnt_exist(self):
         db_metadata = DatabaseMetadata('staging')
         with self.assertRaises(InvalidMetadataKeyException):
-            db_metadata.get_schema_metadata('staging_octane')
+            db_metadata.get_schema('staging_octane')
 
     def test_can_get_schema_metadata_by_schema_name_if_the_schema_has_been_added_to_the_metadata(self):
         db_metadata = DatabaseMetadata('staging')
-        db_metadata.add_schema_metadata(SchemaMetadata('staging_octane'))
-        self.assertEqual(SchemaMetadata('staging_octane'), db_metadata.get_schema_metadata('staging_octane'))
+        db_metadata.add_schema(SchemaMetadata('staging_octane'))
+        self.assertEqual(SchemaMetadata('staging_octane'), db_metadata.get_schema('staging_octane'))
 
     def test_can_iterate_through_all_added_schemas(self):
         db_metadata = DatabaseMetadata('staging')
-        db_metadata.add_schema_metadata(SchemaMetadata('staging_octane'))
-        db_metadata.add_schema_metadata(SchemaMetadata('history_octane'))
-        db_metadata.add_schema_metadata(SchemaMetadata('star_loan'))
+        db_metadata.add_schema(SchemaMetadata('staging_octane'))
+        db_metadata.add_schema(SchemaMetadata('history_octane'))
+        db_metadata.add_schema(SchemaMetadata('star_loan'))
         expected = [SchemaMetadata('staging_octane'), SchemaMetadata('history_octane'), SchemaMetadata('star_loan')]
         self.assertEqual(expected, [schema for schema in db_metadata.schemas])
 
@@ -56,20 +59,82 @@ class TestSchemaMetadata(unittest.TestCase):
     def test_throws_error_if_user_tries_to_get_table_that_doesnt_exist(self):
         schema_metadata = SchemaMetadata('staging_octane')
         with self.assertRaises(InvalidMetadataKeyException):
-            schema_metadata.get_table_metadata('account')
+            schema_metadata.get_table('account')
 
-    def test_can_get_schema_metadata_by_table_name_if_the_table_has_been_added_to_the_metadata(self):
+    def test_can_get_table_metadata_by_table_name_if_the_table_has_been_added_to_the_metadata(self):
         schema_metadata = SchemaMetadata('staging_octane')
-        schema_metadata.add_table_metadata(TableMetadata('account'))
-        self.assertEqual(TableMetadata('account'), schema_metadata.get_table_metadata('account'))
+        schema_metadata.add_table(TableMetadata('account'))
+        self.assertEqual(TableMetadata('account'), schema_metadata.get_table('account'))
 
     def test_can_iterate_through_all_added_tables(self):
         schema_metadata = SchemaMetadata('staging_octane')
-        schema_metadata.add_table_metadata(TableMetadata('account'))
-        schema_metadata.add_table_metadata(TableMetadata('deal'))
-        schema_metadata.add_table_metadata(TableMetadata('loan'))
+        schema_metadata.add_table(TableMetadata('account'))
+        schema_metadata.add_table(TableMetadata('deal'))
+        schema_metadata.add_table(TableMetadata('loan'))
         expected = [TableMetadata('account'), TableMetadata('deal'), TableMetadata('loan')]
-        self.assertEqual(expected, [tables for tables in schema_metadata.tables])
+        self.assertEqual(expected, [table for table in schema_metadata.tables])
+
+
+class TestTableMetadata(unittest.TestCase):
+
+    def test_throws_error_if_user_tries_to_get_column_that_doesnt_exist(self):
+        table_metadata = TableMetadata('deal')
+        with self.assertRaises(InvalidMetadataKeyException):
+            table_metadata.get_column('d_pid')
+
+    def test_can_get_column_metadata_by_column_name_if_the_column_has_been_added_to_the_metadata(self):
+        table_metadata = TableMetadata('deal')
+        table_metadata.add_column(ColumnMetadata('d_pid'))
+        self.assertEqual(ColumnMetadata('d_pid'), table_metadata.get_column('d_pid'))
+
+    def test_can_iterate_through_all_added_columns(self):
+        table_metadata = TableMetadata('deal')
+        table_metadata.add_column(ColumnMetadata('d_pid'))
+        table_metadata.add_column(ColumnMetadata('d_active_proposal_pid'))
+        table_metadata.add_column(ColumnMetadata('d_test_loan'))
+        expected = [ColumnMetadata('d_pid'), ColumnMetadata('d_active_proposal_pid'), ColumnMetadata('d_test_loan')]
+        self.assertEqual(expected, [column for column in table_metadata.columns])
+
+    def test_throws_error_if_user_tries_to_get_etl_that_doesnt_exist(self):
+        table_metadata = TableMetadata('deal')
+        with self.assertRaises(InvalidMetadataKeyException):
+            table_metadata.get_etl('SP-100000')
+
+    def test_can_get_etl_metadata_by_etl_name_if_the_etl_has_been_added_to_the_metadata(self):
+        table_metadata = TableMetadata('deal')
+        table_metadata.add_etl(ETLMetadata('SP-100000'))
+        self.assertEqual(ETLMetadata('SP-100000'), table_metadata.get_etl('SP-100000'))
+
+    def test_can_iterate_through_all_added_etls(self):
+        table_metadata = TableMetadata('deal')
+        table_metadata.add_etl(ETLMetadata('SP-100000'))
+        table_metadata.add_etl(ETLMetadata('SP-100001'))
+        table_metadata.add_etl(ETLMetadata('SP-100002'))
+        expected = [ETLMetadata('SP-100000'), ETLMetadata('SP-100001'), ETLMetadata('SP-100002')]
+        self.assertEqual(expected, [etl for etl in table_metadata.etls])
+
+    def test_throws_error_if_user_tries_to_get_foreign_key_that_doesnt_exist(self):
+        table_metadata = TableMetadata('deal')
+        with self.assertRaises(InvalidMetadataKeyException):
+            table_metadata.get_foreign_key('fk_0')
+
+    def test_can_get_foreign_key_metadata_by_foreign_key_name_if_the_foreign_key_has_been_added_to_the_metadata(self):
+        table_metadata = TableMetadata('deal')
+        table_metadata.add_foreign_key(ForeignKeyMetadata('fk_0', 'proposal', [], []))
+        self.assertEqual(ForeignKeyMetadata('fk_0', 'proposal', [], []), table_metadata.get_foreign_key('fk_0'))
+
+    def test_can_iterate_through_all_added_foreign_keys(self):
+        table_metadata = TableMetadata('deal')
+        table_metadata.add_foreign_key(ForeignKeyMetadata('fk_0', 'proposal', [], []))
+        table_metadata.add_foreign_key(ForeignKeyMetadata('fk_1', 'proposal', [], []))
+        table_metadata.add_foreign_key(ForeignKeyMetadata('fk_2', 'proposal', [], []))
+        expected = [
+            ForeignKeyMetadata('fk_0', 'proposal', [], []),
+            ForeignKeyMetadata('fk_1', 'proposal', [], []),
+            ForeignKeyMetadata('fk_2', 'proposal', [], [])
+        ]
+        self.assertEqual(expected, [foreign_key for foreign_key in table_metadata.foreign_keys])
+
 
 if __name__ == '__main__':
     unittest.main()
