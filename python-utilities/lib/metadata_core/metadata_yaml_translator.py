e45e0afd59dccbd414c5c76d4b939eeb30208363
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-10-07 13:52:48 -0400

    [#1200845872653274] refactor metadata_yaml_translator to extract logic that builds DataWarehouseMetadata from a dict to facilitate easier unit testing

--- python-utilities/lib/metadata_core/metadata_yaml_translator.py
+++ python-utilities/lib/metadata_core/metadata_yaml_translator.py
@@ -19,75 +19,97 @@ from lib.metadata_core.data_warehouse_metadata import (DataWarehouseMetadata,
 
 
 def generate_data_warehouse_metadata_from_yaml(root_dir_file_path: str) -> DataWarehouseMetadata:
+    return construct_data_warehouse_metadata_from_dict(read_data_warehouse_yaml_files_into_dict(root_dir_file_path))
+
+
+def read_data_warehouse_yaml_files_into_dict(root_dir_file_path: str) -> dict:
     if not os.path.isdir(root_dir_file_path):
         raise FileNotFoundError(f'{root_dir_file_path} is not a valid directory path')
-    data_warehouse_dir_name = pathlib.PurePath(root_dir_file_path).name
-    data_warehouse = DataWarehouseMetadata(data_warehouse_dir_name)
+    data_warehouse_name = pathlib.PurePath(root_dir_file_path).name
+    data_warehouse_dict = {'name': data_warehouse_name, 'databases': []}
     for db_subdir_path in get_subdir_paths_with_prefix(root_dir_file_path, 'db'):
         database_name = strip_dot_prefix(pathlib.PurePath(db_subdir_path).name)
-        database = DatabaseMetadata(database_name)
-        data_warehouse.add_database(database)
+        database_dict = {'name': database_name, 'schemas': []}
+        data_warehouse_dict['databases'].append(database_dict)
         for schema_subdir_path in get_subdir_paths_with_prefix(db_subdir_path, 'schema'):
             schema_name = strip_dot_prefix(pathlib.PurePath(schema_subdir_path).name)
-            schema = SchemaMetadata(schema_name)
-            database.add_schema(schema)
+            schema_dict = {'name': schema_name, 'tables': []}
+            database_dict['schemas'].append(schema_dict)
             for table_yaml_path in get_yaml_paths_with_prefix(schema_subdir_path, 'table'):
-                table_yaml = read_yaml_file(table_yaml_path)
-                table = TableMetadata(table_yaml['name'])
-                if 'primary_source_table' in table_yaml:
-                    if not table_yaml['primary_source_table'] or table_yaml['primary_source_table'].count('.') != 2:
-                        raise InvalidTableYAMLFileException(
-                            f'Primary source table for table "{table_yaml["name"]}" is not in the format "database.schema.table"'
-                        )
-                    source_table_components = table_yaml['primary_source_table'].split('.')
-                    table.primary_source_table = TableAddress(
-                        database=source_table_components[0],
-                        schema=source_table_components[1],
-                        table=source_table_components[2]
-                    )
-                table.primary_key = table_yaml.get('primary_key')
-                if 'foreign_keys' in table_yaml:
-                    for fk_name, fk_data in table_yaml['foreign_keys'].items():
-                        if 'columns' not in fk_data or 'references' not in fk_data or 'schema' not in fk_data['references'] or \
-                                'table' not in fk_data['references']:
-                            raise InvalidTableYAMLFileException(f'Foreign key "{fk_name}" is missing one or more required metadata fields')
-                        table.add_foreign_key(ForeignKeyMetadata(
-                            name=fk_name,
-                            table=TableAddress(
-                                database_name,
-                                fk_data['references']['schema'],
-                                fk_data['references']['table']
-                            ),
-                            native_columns=fk_data['columns'],
-                            foreign_columns=fk_data['references']['columns']
-                        ))
-                if 'columns' in table_yaml:
-                    for column_name, column_data in table_yaml['columns'].items():
-                        column_metadata = ColumnMetadata(
-                            name=column_name,
-                            data_type=column_data.get('data_type')
-                        )
-                        if 'source' in column_data and 'field' in column_data['source']:
-                            column_metadata.source_field = parse_foreign_column_path(column_data['source']['field'])
-                        table.add_column(column_metadata)
-                if 'etls' in table_yaml:
-                    for etl_process_name, etl_data in table_yaml['etls'].items():
-                        if 'hardcoded_data_source' not in etl_data or 'input_type' not in etl_data or 'output_type' not in etl_data:
-                            raise InvalidTableYAMLFileException(f'ETL "{etl_process_name}" is missing one or more required metadata fields')
-                        table.add_etl(ETLMetadata(
-                            process_name=etl_process_name,
-                            hardcoded_data_source=parse_etl_data_source(etl_data.get('hardcoded_data_source')),
-                            input_type=parse_etl_input_type(etl_data.get('input_type')),
-                            output_type=parse_etl_output_type(etl_data.get('output_type')),
-                            json_output_field=etl_data.get('json_output_field'),
-                            truncate_table=etl_data.get('truncate_table'),
-                            insert_update_keys=etl_data.get('insert_update_keys'),
-                            delete_keys=etl_data.get('delete_keys'),
-                            input_sql=etl_data.get('input_sql')
-                        ))
-                table.next_etls = table_yaml.get('next_etls')
-                schema.add_table(table)
-    return data_warehouse
+                table_dict = read_yaml_file(table_yaml_path)
+                schema_dict['tables'].append(table_dict)
+    return data_warehouse_dict
+
+
+def construct_data_warehouse_metadata_from_dict(data_warehouse_dict: dict) -> DataWarehouseMetadata:
+    data_warehouse_metadata = DataWarehouseMetadata(data_warehouse_dict['name'])
+    for database_dict in data_warehouse_dict['databases']:
+        database_metadata = DatabaseMetadata(database_dict['name'])
+        data_warehouse_metadata.add_database(database_metadata)
+        for schema_dict in database_dict['schemas']:
+            schema_metadata = SchemaMetadata(schema_dict['name'])
+            database_metadata.add_schema(schema_metadata)
+            for table_dict in schema_dict['tables']:
+                table_metadata = construct_table_metadata_from_dict(table_dict, database_metadata.name)
+                schema_metadata.add_table(table_metadata)
+    return data_warehouse_metadata
+
+
+def construct_table_metadata_from_dict(table_dict: dict, database_name: str) -> TableMetadata:
+    table = TableMetadata(table_dict['name'])
+    if 'primary_source_table' in table_dict:
+        if not table_dict['primary_source_table'] or table_dict['primary_source_table'].count('.') != 2:
+            raise InvalidTableYAMLFileException(
+                f'Primary source table for table "{table_dict["name"]}" is not in the format "database.schema.table"'
+            )
+        source_table_components = table_dict['primary_source_table'].split('.')
+        table.primary_source_table = TableAddress(
+            database=source_table_components[0],
+            schema=source_table_components[1],
+            table=source_table_components[2]
+        )
+    table.primary_key = table_dict.get('primary_key')
+    if 'foreign_keys' in table_dict:
+        for fk_name, fk_data in table_dict['foreign_keys'].items():
+            if 'columns' not in fk_data or 'references' not in fk_data or 'schema' not in fk_data['references'] or \
+                    'table' not in fk_data['references']:
+                raise InvalidTableYAMLFileException(f'Foreign key "{fk_name}" is missing one or more required metadata fields')
+            table.add_foreign_key(ForeignKeyMetadata(
+                name=fk_name,
+                table=TableAddress(
+                    database_name,
+                    fk_data['references']['schema'],
+                    fk_data['references']['table']
+                ),
+                native_columns=fk_data['columns'],
+                foreign_columns=fk_data['references']['columns']
+            ))
+    if 'columns' in table_dict:
+        for column_name, column_data in table_dict['columns'].items():
+            column_metadata = ColumnMetadata(
+                name=column_name,
+                data_type=column_data.get('data_type')
+            )
+            if 'source' in column_data and 'field' in column_data['source']:
+                column_metadata.source_field = parse_foreign_column_path(column_data['source']['field'])
+            table.add_column(column_metadata)
+    if 'etls' in table_dict:
+        for etl_process_name, etl_data in table_dict['etls'].items():
+            if 'hardcoded_data_source' not in etl_data or 'input_type' not in etl_data or 'output_type' not in etl_data:
+                raise InvalidTableYAMLFileException(f'ETL "{etl_process_name}" is missing one or more required metadata fields')
+            table.add_etl(ETLMetadata(
+                process_name=etl_process_name,
+                hardcoded_data_source=parse_etl_data_source(etl_data.get('hardcoded_data_source')),
+                input_type=parse_etl_input_type(etl_data.get('input_type')),
+                output_type=parse_etl_output_type(etl_data.get('output_type')),
+                json_output_field=etl_data.get('json_output_field'),
+                truncate_table=etl_data.get('truncate_table'),
+                insert_update_keys=etl_data.get('insert_update_keys'),
+                delete_keys=etl_data.get('delete_keys'),
+                input_sql=etl_data.get('input_sql')
+            ))
+    table.next_etls = table_dict.get('next_etls')
+    return table
 
 
 def get_subdir_paths_with_prefix(root_dir: str, prefix: str) -> List[str]:
