9b5b7415785ec4e9a4fd235b83e46d27d93ce3d5
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-10-08 08:09:21 -0400

    [#1200845872653274] add optional command-line argument specifying the metadata directory

--- python-utilities/scripts/generate_sql_to_update_edw_metadata.py
+++ python-utilities/scripts/generate_sql_to_update_edw_metadata.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from lib.db_connections import LocalEDWConnection
 from lib.metadata_core.metadata_yaml_translator import generate_data_warehouse_metadata_from_yaml
@@ -7,9 +8,13 @@ from lib.config_mdi_metadata_maintenance.metadata_comparison_functions import (P
 
 
 def main():
+    default_metadata_root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'metadata', 'edw')
+    argparser = argparse.ArgumentParser(description='Generate SQL to maintain metadata in the config.mdi schema')
+    argparser.add_argument('-d', '--metadata_dir', type=str, default=default_metadata_root_dir)
+    args = argparser.parse_args()
+
     edw_connection = LocalEDWConnection(host='localhost', dbname='config', user='postgres', password='testonly')
-    metadata_root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'metadata', 'edw')
-    data_warehouse_metadata = generate_data_warehouse_metadata_from_yaml(metadata_root_dir)
+    data_warehouse_metadata = generate_data_warehouse_metadata_from_yaml(args.metadata_dir)
     sql_generator = MetadataMaintenanceSQLGenerator(edw_connection, data_warehouse_metadata)
     sql_generator.add_metadata_comparison_functions('process', ProcessMetadataComparisonFunctions())
     metadata_maintenance_sql = sql_generator.generate_all_metadata_maintenance_sql()
