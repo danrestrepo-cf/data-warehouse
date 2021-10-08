8a6670daffda4d89fe0a10dc359980489055bd3f
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-10-08 09:23:05 -0400

    [#1200845872653274] enable script to be run from command line and explicitly filter source metadata to just history_octane and staging_octane tables

--- python-utilities/scripts/generate_sql_to_update_edw_metadata.py
+++ python-utilities/scripts/generate_sql_to_update_edw_metadata.py
@@ -1,7 +1,12 @@
+import sys
 import os
 import argparse
 
+# this line allows the script to import directly from lib when run from the command line
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
 from lib.db_connections import LocalEDWConnection
+from lib.metadata_core.data_warehouse_metadata import DataWarehouseMetadata
 from lib.metadata_core.metadata_yaml_translator import generate_data_warehouse_metadata_from_yaml
 from lib.config_mdi_metadata_maintenance.metadata_maintenance_sql import MetadataMaintenanceSQLGenerator
 from lib.config_mdi_metadata_maintenance.metadata_comparison_functions import (ProcessMetadataComparisonFunctions)
@@ -15,6 +20,7 @@ def main():
 
     edw_connection = LocalEDWConnection(host='localhost', dbname='config', user='postgres', password='testonly')
     data_warehouse_metadata = generate_data_warehouse_metadata_from_yaml(args.metadata_dir)
+    filter_out_metadata_not_from_staging_octane_or_history_octane_schemas(data_warehouse_metadata)
     sql_generator = MetadataMaintenanceSQLGenerator(edw_connection, data_warehouse_metadata)
     sql_generator.add_metadata_comparison_functions('process', ProcessMetadataComparisonFunctions())
     metadata_maintenance_sql = sql_generator.generate_all_metadata_maintenance_sql()
@@ -22,6 +28,25 @@ def main():
         print('No metadata updates are necessary at this time')
     else:
         print(metadata_maintenance_sql)
+
+
+def filter_out_metadata_not_from_staging_octane_or_history_octane_schemas(metadata: DataWarehouseMetadata):
+    """
+    Delete any metadata not from the staging.staging_octane or staging.history_octane schemas
+
+    This is to prevent potentially misleading comparisons involving other schemas while
+    this script is still not properly configured to handle those other schemas. This
+    method and its usages should be REMOVED if/when the script is expanded to handle
+    schema metadata outside of staging_octane and history_octane.
+    """
+    for database in metadata.databases:
+        if database.name != 'staging':
+            metadata.remove_database_metadata(database.name)
+    if 'staging' in metadata.databases:
+        database = metadata.get_database('staging')
+        for schema in database.schemas:
+            if schema.name != 'staging_octane' and schema.name != 'history_octane':
+                database.remove_schema_metadata(schema.name)
 
 
 if __name__ == '__main__':
