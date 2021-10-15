ac094893c1d13adb1a1f128e32474257b8d12b74
Ramon Ecung <ramon.ecung@cardinalfinancial.com>
2021-10-15 16:47:32 -0400

    [#1201176006424816] added newly created tables

--- docker/database/migrations/config/V2021_10_1_1__migration.sql
+++ docker/database/migrations/config/V2021_10_1_1__migration.sql
@@ -251,3 +251,307 @@ select 'Done removing fields from ETLs and metadata tables:
 staging_octane.loan
     l_agency_case_id
     l_agency_case_id_assigned_date';
+
+
+
+/*
+Created Tables:
+staging_octane.smart_message_permission_type
+    code
+    value
+history_octane.smart_message_permission_type
+    code
+    value
+    data_source_updated_datetime
+    data_source_deleted_flag
+
+staging_octane.smart_message_permission
+    smp_pid
+    smp_version
+    smp_smart_message_pid
+    smp_role_pid
+    smp_permission_type
+history_octane.smart_message_permission
+    smp_pid
+    smp_version
+    smp_smart_message_pid
+    smp_role_pid
+    smp_permission_type
+    data_source_updated_datetime
+    data_source_deleted_flag
+
+staging_octane.wf_deal_step_performer_unavailable
+    wdspu_pid
+    wdspu_version
+    wdspu_lender_user_pid
+    wdspu_wf_deal_step_pid
+    wdspu_days_unavailable
+history_octane.wf_deal_step_performer_unavailable
+    wdspu_pid
+    wdspu_version
+    wdspu_lender_user_pid
+    wdspu_wf_deal_step_pid
+    wdspu_days_unavailable
+    data_source_updated_datetime
+    data_source_deleted_flag
+ */
+
+
+-- Insert metadata for new tables: wf_deal_step_performer_unavailable, smart_message_permission, smart_message_permission_type
+WITH new_staging_table_definitions AS (
+    INSERT INTO mdi.edw_table_definition (database_name, schema_name, table_name, primary_source_edw_table_definition_dwid)
+        VALUES ('staging', 'staging_octane', 'smart_message_permission_type', NULL)
+             , ('staging', 'staging_octane', 'smart_message_permission', NULL)
+             , ('staging', 'staging_octane', 'wf_deal_step_performer_unavailable', NULL)
+        RETURNING dwid, table_name
+)
+
+    , new_history_table_definitions AS (
+    INSERT INTO mdi.edw_table_definition (database_name, schema_name, table_name, primary_source_edw_table_definition_dwid)
+        SELECT 'staging'
+            , 'history_octane'
+            , new_staging_table_definitions.table_name
+            , new_staging_table_definitions.dwid
+        FROM new_staging_table_definitions
+        RETURNING dwid, table_name
+)
+
+    , new_fields (schema_name, table_name, field_name, key_field_flag, field_order) AS (
+    VALUES ('staging_octane', 'smart_message_permission_type', 'code', TRUE, NULL)
+         , ('staging_octane', 'smart_message_permission_type', 'value', FALSE, NULL)
+         , ('history_octane', 'smart_message_permission_type', 'code', FALSE, 1)
+         , ('history_octane', 'smart_message_permission_type', 'value', FALSE, 2)
+         , ('history_octane', 'smart_message_permission_type', 'data_source_updated_datetime', FALSE, 3)
+         , ('history_octane', 'smart_message_permission_type', 'data_source_deleted_flag', FALSE, 4)
+
+         , ('staging_octane', 'smart_message_permission', 'smp_pid', FALSE, NULL)
+         , ('staging_octane', 'smart_message_permission', 'smp_version', FALSE, NULL)
+         , ('staging_octane', 'smart_message_permission', 'smp_smart_message_pid', FALSE, NULL)
+         , ('staging_octane', 'smart_message_permission', 'smp_role_pid', FALSE, NULL)
+         , ('staging_octane', 'smart_message_permission', 'smp_permission_type', FALSE, NULL)
+         , ('history_octane', 'smart_message_permission', 'smp_pid', FALSE, 1)
+         , ('history_octane', 'smart_message_permission', 'smp_version', FALSE, 2)
+         , ('history_octane', 'smart_message_permission', 'smp_smart_message_pid', FALSE, 3)
+         , ('history_octane', 'smart_message_permission', 'smp_role_pid', FALSE, 4)
+         , ('history_octane', 'smart_message_permission', 'smp_permission_type', TRUE, 5)
+         , ('history_octane', 'smart_message_permission', 'data_source_updated_datetime', FALSE, 6)
+         , ('history_octane', 'smart_message_permission', 'data_source_deleted_flag', FALSE, 7)
+
+         , ('staging_octane', 'wf_deal_step_performer_unavailable', 'wdspu_pid', FALSE, NULL)
+         , ('staging_octane', 'wf_deal_step_performer_unavailable', 'wdspu_version', FALSE, NULL)
+         , ('staging_octane', 'wf_deal_step_performer_unavailable', 'wdspu_lender_user_pid', FALSE, NULL)
+         , ('staging_octane', 'wf_deal_step_performer_unavailable', 'wdspu_wf_deal_step_pid', FALSE, NULL)
+         , ('staging_octane', 'wf_deal_step_performer_unavailable', 'wdspu_days_unavailable', FALSE, NULL)
+         , ('history_octane', 'wf_deal_step_performer_unavailable', 'wdspu_pid', FALSE, 1)
+         , ('history_octane', 'wf_deal_step_performer_unavailable', 'wdspu_version', FALSE, 2)
+         , ('history_octane', 'wf_deal_step_performer_unavailable', 'wdspu_lender_user_pid', FALSE, 3)
+         , ('history_octane', 'wf_deal_step_performer_unavailable', 'wdspu_wf_deal_step_pid', FALSE, 4)
+         , ('history_octane', 'wf_deal_step_performer_unavailable', 'wdspu_days_unavailable', FALSE, 5)
+         , ('history_octane', 'wf_deal_step_performer_unavailable', 'data_source_updated_datetime', FALSE, 6)
+         , ('history_octane', 'wf_deal_step_performer_unavailable', 'data_source_deleted_flag', FALSE, 7)
+)
+
+    , new_staging_field_definitions AS (
+    INSERT INTO mdi.edw_field_definition (edw_table_definition_dwid, field_name, key_field_flag)
+        SELECT new_staging_table_definitions.dwid, new_fields.field_name, new_fields.key_field_flag
+        FROM new_fields
+            JOIN new_staging_table_definitions
+                ON new_fields.table_name = new_staging_table_definitions.table_name
+        WHERE new_fields.schema_name = 'staging_octane'
+        RETURNING dwid, edw_table_definition_dwid, field_name
+)
+
+    , new_history_field_definitions AS (
+    INSERT INTO mdi.edw_field_definition (edw_table_definition_dwid, field_name, key_field_flag,
+                                          source_edw_field_definition_dwid)
+        SELECT new_history_table_definitions.dwid
+            , new_fields.field_name
+            , new_fields.key_field_flag
+            , new_staging_field_definitions.dwid
+        FROM new_fields
+            JOIN new_staging_table_definitions
+                ON new_fields.table_name = new_staging_table_definitions.table_name
+            JOIN new_history_table_definitions
+                ON new_fields.table_name = new_history_table_definitions.table_name
+            LEFT JOIN new_staging_field_definitions
+                ON new_staging_table_definitions.dwid =
+                   new_staging_field_definitions.edw_table_definition_dwid
+                    AND new_fields.field_name = new_staging_field_definitions.field_name
+        WHERE new_fields.schema_name = 'history_octane'
+)
+
+    , new_process_variables (name, target_table, json_output_field, sql) AS (
+    VALUES ('SP-100862', 'smart_message_permission_type', 'sdvdc_pid', 'SELECT staging_table.code, staging_table.value, FALSE AS data_source_deleted_flag, now() AS data_source_updated_datetime
+FROM staging_octane.smart_message_permission_type staging_table
+    LEFT JOIN history_octane.smart_message_permission_type history_table on staging_table.code = history_table.code AND staging_table.value = history_table.value
+WHERE history_table.code IS NULL')
+         , ('SP-100861', 'wf_deal_step_performer_unavailable', 'wdspu_pid', '--finding records to insert into history_octane.wf_deal_step_performer_unavailable
+SELECT staging_table.wdspu_pid
+, staging_table.wdspu_version
+, staging_table.wdspu_lender_user_pid
+, staging_table.wdspu_wf_deal_step_pid
+, staging_table.wdspu_days_unavailable
+, FALSE AS data_source_deleted_flag
+, NOW( ) AS data_source_updated_datetime
+FROM staging_octane.wf_deal_step_performer_unavailable staging_table
+    LEFT JOIN history_octane.wf_deal_step_performer_unavailable history_table
+        ON staging_table.wdspu_pid = history_table.wdspu_pid AND staging_table.wdspu_version = history_table.wdspu_version
+WHERE history_table.wdspu_pid IS NULL
+UNION ALL
+SELECT history_table.wdspu_pid
+, history_table.wdspu_version + 1
+, history_table.wdspu_lender_user_pid
+, history_table.wdspu_wf_deal_step_pid
+, history_table.wdspu_days_unavailable
+, TRUE AS data_source_deleted_flag
+, NOW( ) AS data_source_updated_datetime
+FROM history_octane.wf_deal_step_performer_unavailable history_table
+    LEFT JOIN staging_octane.wf_deal_step_performer_unavailable staging_table
+        ON staging_table.wdspu_pid = history_table.wdspu_pid
+WHERE staging_table.wdspu_pid IS NULL
+    AND NOT EXISTS( SELECT 1
+                    FROM history_octane.wf_deal_step_performer_unavailable deleted_records
+                    WHERE deleted_records.wdspu_pid = history_table.wdspu_pid AND deleted_records.data_source_deleted_flag = TRUE );')
+         , ('SP-100860', 'smart_message_permission', 'smp_pid', '--finding records to insert into history_octane.smart_message_permission
+SELECT staging_table.smp_pid
+, staging_table.smp_version
+, staging_table.smp_smart_message_pid
+, staging_table.smp_role_pid
+, staging_table.smp_permission_type
+, FALSE AS data_source_deleted_flag
+, NOW( ) AS data_source_updated_datetime
+FROM staging_octane.smart_message_permission staging_table
+    LEFT JOIN history_octane.smart_message_permission history_table
+        ON staging_table.smp_pid = history_table.smp_pid AND staging_table.smp_version = history_table.smp_version
+WHERE history_table.smp_pid IS NULL
+UNION ALL
+SELECT history_table.smp_pid
+, history_table.smp_version + 1
+, history_table.smp_smart_message_pid
+, history_table.smp_role_pid
+, history_table.smp_permission_type
+, TRUE AS data_source_deleted_flag
+, NOW( ) AS data_source_updated_datetime
+FROM history_octane.smart_message_permission history_table
+    LEFT JOIN staging_octane.smart_message_permission staging_table
+        ON staging_table.smp_pid = history_table.smp_pid
+WHERE staging_table.smp_pid IS NULL
+    AND NOT EXISTS( SELECT 1
+                    FROM history_octane.smart_message_permission deleted_records
+                    WHERE deleted_records.smp_pid = history_table.smp_pid AND deleted_records.data_source_deleted_flag = TRUE );')
+)
+    , new_processes AS (
+    INSERT INTO mdi.process (name, description)
+        SELECT new_process_variables.name
+            , 'ETL to copy ' || new_process_variables.target_table || ' data from staging_octane to history_octane'
+        FROM new_process_variables
+        RETURNING dwid, name, description
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    , new_table_input_steps AS (
+    INSERT INTO mdi.table_input_step (process_dwid, data_source_dwid, sql, limit_size, execute_for_each_row, replace_variables,
+                                      enable_lazy_conversion, cached_row_meta, connectionname)
+        SELECT new_processes.dwid
+            , 0
+            , new_process_variables.sql
+            , 0
+            , 'N'
+            , 'N'
+            , 'N'
+            , 'N'
+            , 'Staging DB Connection'
+        FROM new_processes
+            JOIN new_process_variables
+                ON new_processes.name = new_process_variables.name
+)
+
+    , new_table_output_steps AS (
+    INSERT INTO mdi.table_output_step (process_dwid, target_schema, target_table, commit_size, partitioning_field, table_name_field,
+                                       auto_generated_key_field, partition_data_per, table_name_defined_in_field,
+                                       return_auto_generated_key_field, truncate_table, connectionname, partition_over_tables,
+                                       specify_database_fields, ignore_insert_errors, use_batch_update)
+        SELECT new_processes.dwid
+            , 'history_octane'
+            , new_process_variables.target_table
+            , 1000
+            , NULL
+            , NULL
+            , NULL
+            , NULL
+            , 'N'
+            , NULL
+            , 'N'
+            , 'Staging DB Connection'
+            , 'N'
+            , 'Y'
+            , 'N'
+            , 'N'
+        FROM new_processes
+            JOIN new_process_variables
+                ON new_processes.name = new_process_variables.name
+        RETURNING dwid, target_schema, target_table
+)
+
+    , new_table_output_fields AS (
+    INSERT INTO mdi.table_output_field (table_output_step_dwid, database_field_name, database_stream_name, field_order, is_sensitive)
+        SELECT new_table_output_steps.dwid
+            , new_fields.field_name
+            , new_fields.field_name
+            , new_fields.field_order
+            , FALSE
+        FROM new_table_output_steps
+            JOIN new_fields
+                ON new_fields.schema_name = new_table_output_steps.target_schema
+                    AND new_fields.table_name = new_table_output_steps.target_table
+)
+
+    , new_json_output_fields AS (
+    INSERT INTO mdi.json_output_field (process_dwid, field_name)
+        SELECT new_processes.dwid, new_process_variables.json_output_field
+        FROM new_processes
+            JOIN new_process_variables
+                ON new_processes.name = new_process_variables.name
+)
+
+    , new_state_machine_definitions AS (
+    INSERT INTO mdi.state_machine_definition (process_dwid, name, comment)
+        SELECT new_processes.dwid, new_processes.name, new_processes.description
+        FROM new_processes
+)
+SELECT 'Finished inserting metadata for new tables: smart_doc_validity_date_case, proposal_doc_validity';
