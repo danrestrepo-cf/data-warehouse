4ace68b74a18e56a996d33ecfe51de2ae09c56e2
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-10-19 09:00:55 -0400

    [#1200845872653274] refactor metadata builder class into function

--- python-utilities/lib/lura_information_schema_to_yaml/metadata_builders.py
+++ python-utilities/lib/lura_information_schema_to_yaml/metadata_builders.py
@@ -4,70 +4,64 @@ from lib.metadata_core.data_warehouse_metadata import DataWarehouseMetadata
 from lib.metadata_core.metadata_yaml_translator import construct_data_warehouse_metadata_from_dict
 
 
-class StagingOctaneMetadataBuilder:
-
-    def __init__(self, column_metadata: List[dict], foreign_key_metadata: List[dict]):
-        self._column_metadata = column_metadata
-        self._foreign_key_metadata = foreign_key_metadata
-
-    def build_metadata(self) -> DataWarehouseMetadata:
-        tables_dict = {}
-        for row in self._column_metadata:
-            if row['table_name'] not in tables_dict:
-                tables_dict[row['table_name']] = {
-                    'name': row['table_name'],
-                    'primary_key': [],
-                    'foreign_keys': {},
-                    'columns': {}
-                }
-            if row['is_primary_key'] == 1:
-                tables_dict[row['table_name']]['primary_key'].append(row['column_name'])
-            tables_dict[row['table_name']]['columns'][row['column_name']] = {
-                'data_type': self.map_data_type(row['column_type'])
+def build_staging_octane_metadata(column_metadata: List[dict], foreign_key_metadata: List[dict]) -> DataWarehouseMetadata:
+    tables_dict = {}
+    for row in column_metadata:
+        if row['table_name'] not in tables_dict:
+            tables_dict[row['table_name']] = {
+                'name': row['table_name'],
+                'primary_key': [],
+                'foreign_keys': {},
+                'columns': {}
             }
-
-        for row in self._foreign_key_metadata:
-            tables_dict[row['table_name']]['foreign_keys'][row['constraint_name']] = {
-                'columns': [row['column_name']],
-                'references': {
-                    'columns': [row['referenced_column_name']],
-                    'schema': 'staging_octane',
-                    'table': row['referenced_table_name']
-                }
-            }
-
-        metadata_dict = {
-            'name': 'edw',
-            'databases': [
-                {
-                    'name': 'staging',
-                    'schemas': [
-                        {
-                            'name': 'staging_octane',
-                            'tables': tables_dict.values()
-                        }
-                    ]
-                }
-            ]
+        if row['is_primary_key'] == 1:
+            tables_dict[row['table_name']]['primary_key'].append(row['column_name'])
+        tables_dict[row['table_name']]['columns'][row['column_name']] = {
+            'data_type': map_data_type(row['column_type'])
         }
-        return construct_data_warehouse_metadata_from_dict(metadata_dict)
 
-    @staticmethod
-    def map_data_type(data_type: str) -> str:
-        upper_data_type = data_type.upper()
-        if upper_data_type == 'DATE' or upper_data_type == 'TIME' or upper_data_type == 'TEXT' or upper_data_type.startswith('VARCHAR('):
-            return upper_data_type
-        elif upper_data_type == 'DATETIME':
-            return 'TIMESTAMP'
-        elif upper_data_type.startswith('TINYINT('):
-            return 'SMALLINT'
-        elif upper_data_type.startswith('INT('):
-            return 'INTEGER'
-        elif upper_data_type.startswith('BIGINT('):
-            return 'BIGINT'
-        elif upper_data_type.startswith('BIT('):
-            return 'BOOLEAN'
-        elif upper_data_type.startswith('DECIMAL('):
-            return f'NUMERIC{upper_data_type[7:]}'
-        else:
-            raise ValueError(f'Unable to map data type {upper_data_type}')
+    for row in foreign_key_metadata:
+        tables_dict[row['table_name']]['foreign_keys'][row['constraint_name']] = {
+            'columns': [row['column_name']],
+            'references': {
+                'columns': [row['referenced_column_name']],
+                'schema': 'staging_octane',
+                'table': row['referenced_table_name']
+            }
+        }
+
+    metadata_dict = {
+        'name': 'edw',
+        'databases': [
+            {
+                'name': 'staging',
+                'schemas': [
+                    {
+                        'name': 'staging_octane',
+                        'tables': tables_dict.values()
+                    }
+                ]
+            }
+        ]
+    }
+    return construct_data_warehouse_metadata_from_dict(metadata_dict)
+
+
+def map_data_type(data_type: str) -> str:
+    upper_data_type = data_type.upper()
+    if upper_data_type == 'DATE' or upper_data_type == 'TIME' or upper_data_type == 'TEXT' or upper_data_type.startswith('VARCHAR('):
+        return upper_data_type
+    elif upper_data_type == 'DATETIME':
+        return 'TIMESTAMP'
+    elif upper_data_type.startswith('TINYINT('):
+        return 'SMALLINT'
+    elif upper_data_type.startswith('INT('):
+        return 'INTEGER'
+    elif upper_data_type.startswith('BIGINT('):
+        return 'BIGINT'
+    elif upper_data_type.startswith('BIT('):
+        return 'BOOLEAN'
+    elif upper_data_type.startswith('DECIMAL('):
+        return f'NUMERIC{upper_data_type[7:]}'
+    else:
+        raise ValueError(f'Unable to map data type {upper_data_type}')
--- python-utilities/tests/test_lura_information_schema_to_yaml/test_metadata_builders.py
+++ python-utilities/tests/test_lura_information_schema_to_yaml/test_metadata_builders.py
@@ -1,14 +1,13 @@
 import unittest
 
-from lib.lura_information_schema_to_yaml.metadata_builders import StagingOctaneMetadataBuilder
+from lib.lura_information_schema_to_yaml.metadata_builders import build_staging_octane_metadata, map_data_type
 from lib.metadata_core.metadata_yaml_translator import construct_data_warehouse_metadata_from_dict
 
 
 class TestStagingOctaneMetadataBuilder(unittest.TestCase):
 
     def test_creates_staging_octane_data_warehouse_metadata_object_with_only_one_database_and_schema(self):
-        builder = StagingOctaneMetadataBuilder(column_metadata=[], foreign_key_metadata=[])
-        result = builder.build_metadata()
+        result = build_staging_octane_metadata([], [])
         self.assertEqual('edw', result.name)
         self.assertEqual(['staging'], [database.name for database in result.databases])
         self.assertEqual(['staging_octane'], [schema.name for schema in result.get_database('staging').schemas])
@@ -34,8 +33,7 @@ class TestStagingOctaneMetadataBuilder(unittest.TestCase):
                 'is_primary_key': 1
             }
         ]
-        builder = StagingOctaneMetadataBuilder(column_metadata=column_metadata, foreign_key_metadata=[])
-        result = builder.build_metadata()
+        result = build_staging_octane_metadata(column_metadata, [])
         self.assertEqual(['t1', 't2'], [table.name for table in result.get_database('staging').get_schema('staging_octane').tables])
 
     def test_populates_primary_key_and_foreign_key_and_column_metadata_for_each_table(self):
@@ -76,7 +74,6 @@ class TestStagingOctaneMetadataBuilder(unittest.TestCase):
                 'referenced_column_name': 'c20'
             }
         ]
-        builder = StagingOctaneMetadataBuilder(column_metadata=column_metadata, foreign_key_metadata=foreign_key_metadata)
 
         expected = construct_data_warehouse_metadata_from_dict({
             'name': 'edw',
@@ -133,50 +130,50 @@ class TestStagingOctaneMetadataBuilder(unittest.TestCase):
             ]
         })
 
-        self.assertEqual(expected, builder.build_metadata())
+        self.assertEqual(expected, build_staging_octane_metadata(column_metadata, foreign_key_metadata))
 
 
 class TestStagingOctaneMetadataBuilderDataTypeMapping(unittest.TestCase):
 
     def test_maps_date_to_itself(self):
-        self.assertEqual('DATE', StagingOctaneMetadataBuilder.map_data_type('Date'))
+        self.assertEqual('DATE', map_data_type('Date'))
 
     def test_maps_time_to_itself(self):
-        self.assertEqual('TIME', StagingOctaneMetadataBuilder.map_data_type('TIMe'))
+        self.assertEqual('TIME', map_data_type('TIMe'))
 
     def test_maps_text_to_itself(self):
-        self.assertEqual('TEXT', StagingOctaneMetadataBuilder.map_data_type('text'))
+        self.assertEqual('TEXT', map_data_type('text'))
 
     def test_maps_varchar_to_itself(self):
-        self.assertEqual('VARCHAR(16)', StagingOctaneMetadataBuilder.map_data_type('varchar(16)'))
-        self.assertEqual('VARCHAR(256)', StagingOctaneMetadataBuilder.map_data_type('varCHAR(256)'))
+        self.assertEqual('VARCHAR(16)', map_data_type('varchar(16)'))
+        self.assertEqual('VARCHAR(256)', map_data_type('varCHAR(256)'))
 
     def test_maps_datetime_to_timestamp(self):
-        self.assertEqual('TIMESTAMP', StagingOctaneMetadataBuilder.map_data_type('datetime'))
+        self.assertEqual('TIMESTAMP', map_data_type('datetime'))
 
     def test_maps_tinyint_to_smallint(self):
-        self.assertEqual('SMALLINT', StagingOctaneMetadataBuilder.map_data_type('TINYINT(8)'))
-        self.assertEqual('SMALLINT', StagingOctaneMetadataBuilder.map_data_type('TINYINT(64)'))
+        self.assertEqual('SMALLINT', map_data_type('TINYINT(8)'))
+        self.assertEqual('SMALLINT', map_data_type('TINYINT(64)'))
 
     def test_maps_int_to_integer(self):
-        self.assertEqual('INTEGER', StagingOctaneMetadataBuilder.map_data_type('int(32)'))
-        self.assertEqual('INTEGER', StagingOctaneMetadataBuilder.map_data_type('int(64)'))
+        self.assertEqual('INTEGER', map_data_type('int(32)'))
+        self.assertEqual('INTEGER', map_data_type('int(64)'))
 
     def test_maps_bigint_to_bigint(self):
-        self.assertEqual('BIGINT', StagingOctaneMetadataBuilder.map_data_type('bigint(128)'))
-        self.assertEqual('BIGINT', StagingOctaneMetadataBuilder.map_data_type('bigint(64)'))
+        self.assertEqual('BIGINT', map_data_type('bigint(128)'))
+        self.assertEqual('BIGINT', map_data_type('bigint(64)'))
 
     def test_maps_bit_to_boolean(self):
-        self.assertEqual('BOOLEAN', StagingOctaneMetadataBuilder.map_data_type('BIT(1)'))
-        self.assertEqual('BOOLEAN', StagingOctaneMetadataBuilder.map_data_type('BIT(16)'))
+        self.assertEqual('BOOLEAN', map_data_type('BIT(1)'))
+        self.assertEqual('BOOLEAN', map_data_type('BIT(16)'))
 
     def test_maps_decimal_to_numeric(self):
-        self.assertEqual('NUMERIC(11,5)', StagingOctaneMetadataBuilder.map_data_type('DECIMAL(11,5)'))
-        self.assertEqual('NUMERIC(16,30)', StagingOctaneMetadataBuilder.map_data_type('DECIMAL(16,30)'))
+        self.assertEqual('NUMERIC(11,5)', map_data_type('DECIMAL(11,5)'))
+        self.assertEqual('NUMERIC(16,30)', map_data_type('DECIMAL(16,30)'))
 
     def test_raises_error_if_given_unknown_type(self):
         with self.assertRaises(ValueError):
-            StagingOctaneMetadataBuilder.map_data_type('unknown')
+            map_data_type('unknown')
 
 
 if __name__ == '__main__':
