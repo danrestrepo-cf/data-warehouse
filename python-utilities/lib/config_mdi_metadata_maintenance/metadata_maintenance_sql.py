682de23a35556ff661532dcb96cea64546a17827
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-10-26 10:57:10 -0400

    [#1200845872653274] add docstrings

--- python-utilities/lib/config_mdi_metadata_maintenance/metadata_maintenance_sql.py
+++ python-utilities/lib/config_mdi_metadata_maintenance/metadata_maintenance_sql.py
@@ -8,14 +8,36 @@ from lib.config_mdi_metadata_maintenance.metadata_comparison_functions import Me
 
 @dataclass
 class TableMaintenanceSQL:
+    """A collection of insert, update, and delete SQL statements on a single table.
+
+    This data structure was necessary because, while all three statement categories
+    are *generated* together for a given table, they cannot all be *executed* together
+    for a single table. Because of the inherent dependencies involved, all Inserts
+    appear before all Updates, which appear before all Deletes. This data structure
+    is a convenient holding place for a table's maintenance SQL between the time
+    it is generated and the time the statement types are split up into their own
+    groupings.
+    """
     insert_sql: str
     update_sql: str
     delete_sql: str
 
 
 class MetadataMaintenanceSQLGenerator:
+    """A class that compares metadata from EDW's mdi.config schema and YAML files to generate SQL updating mdi.config accordingly.
+
+    This is the primary "connective tissue" class in the process that keeps mdi.config metadata in lockstep with
+    the EDW YAML metadata files. It combines and draws on the functionality of the rest of the config_mdi_metadata_maintenance
+    module to produce the finished product: executable SQL statements.
+    """
 
     def __init__(self, edw_connection: DBConnection, source_metadata: DataWarehouseMetadata):
+        """
+        :param edw_connection: a database connection to EDW's config database
+        :param source_metadata: a DataWarehouseMetadata object containing the
+        "metadata of record" that will be used as a basis for updating the contents
+        of the EDW config.mdi schema.
+        """
         self._edw_connection = edw_connection
         self._source_metadata = source_metadata
         self._comparison_functions = {}
@@ -24,15 +46,24 @@ class MetadataMaintenanceSQLGenerator:
         self._delete_table_order = []
 
     def add_metadata_comparison_functions(self, table_name: str, comparison_functions: MetadataComparisonFunctions):
+        """Add a set of metadata comparison functions to enable generating maintenance SQL for the specified table."""
         self._comparison_functions[table_name] = comparison_functions
         self._insert_table_order.append(table_name)
         self._update_table_order.append(table_name)
         self._delete_table_order.append(table_name)
 
     def generate_all_metadata_maintenance_sql(self) -> str:
+        """Produce the final string of all SQL statements generated using all currently-held set of metadata comparison functions.
+
+        If a given category of SQL statement (e.g. Insertions, Updates, Deletions)
+        has no members, then that category will be omitted from the final result.
+        For example, of no deletions are necessary, there will simply be no "DELETIONS"
+        section in the output.
+        """
         maintenance_sql_statements = {}
         for table_name, comparison_functions in self._comparison_functions.items():
             maintenance_sql_statements[table_name] = self.generate_table_metadata_maintenance_sql(comparison_functions)
+
         statement_separator = '\n\n'
 
         all_sql_strings = {
@@ -52,6 +83,7 @@ class MetadataMaintenanceSQLGenerator:
         return statement_separator.join([f'/*\n{sql_type}\n*/\n\n{sql}' for sql_type, sql in all_sql_strings.items() if sql != ''])
 
     def generate_table_metadata_maintenance_sql(self, comparison_functions: MetadataComparisonFunctions) -> TableMaintenanceSQL:
+        """Produce a collection of Insert, Update, and Delete SQL statements from the given set of metadata comparison functions."""
         config_metadata_table = comparison_functions.construct_metadata_table_from_config_db(self._edw_connection)
         source_metadata_table = comparison_functions.construct_metadata_table_from_source(self._source_metadata)
         insert_row_grouper = comparison_functions.construct_insert_row_grouper(self._source_metadata)
@@ -95,18 +127,22 @@ class MetadataMaintenanceSQLGenerator:
         )
 
     def set_insert_table_order(self, table_order: List[str]):
+        """Set the order in which INSERT SQL statements will be executed (by table name)."""
         self.validate_table_order_list(table_order)
         self._insert_table_order = table_order
 
     def set_update_table_order(self, table_order: List[str]):
+        """Set the order in which UPDATE SQL statements will be executed (by table name)."""
         self.validate_table_order_list(table_order)
         self._update_table_order = table_order
 
     def set_delete_table_order(self, table_order: List[str]):
+        """Set the order in which DELETE SQL statements will be executed (by table name)."""
         self.validate_table_order_list(table_order)
         self._delete_table_order = table_order
 
     def validate_table_order_list(self, table_order: List[str]):
+        """Throw an error if the given list of table names does not exactly match the known list of tables."""
         missing_tables = []
         for table_name in self._comparison_functions.keys():
             if table_name not in table_order:
