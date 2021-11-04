11f6ff4285fa916c5e79e0ea161dbfa2f553aed9
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-11-04 08:10:52 -0400

    [#1201311226688064] add metadata migration for new table "password_settings"

--- docker/database/migrations/config/V2021_10_3_2__migration.sql
+++ docker/database/migrations/config/V2021_10_3_2__migration.sql
@@ -3,6 +3,206 @@
 -- https://app.asana.com/0/0/1201311226688064
 --
 
+-- Insert metadata for new tables: password_settings
+WITH new_staging_table_definitions AS (
+    INSERT INTO mdi.edw_table_definition (database_name, schema_name, table_name,
+                                          primary_source_edw_table_definition_dwid)
+        VALUES ('staging', 'staging_octane', 'password_settings', NULL)
+        RETURNING dwid, table_name
+)
+
+   , new_history_table_definitions AS (
+    INSERT INTO mdi.edw_table_definition (database_name, schema_name, table_name,
+                                          primary_source_edw_table_definition_dwid)
+        SELECT 'staging'
+             , 'history_octane'
+             , new_staging_table_definitions.table_name
+             , new_staging_table_definitions.dwid
+        FROM new_staging_table_definitions
+        RETURNING dwid, table_name
+)
+
+   , new_fields (schema_name, table_name, field_name, key_field_flag, field_order) AS (
+    VALUES ('staging_octane', 'password_settings', 'pws_pid', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_version', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_account_pid', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_lender_user_expire_days', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_lender_user_minimum_change_days', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_lender_user_previous_password_ban', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_minimum_length', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_require_special_character', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_require_uppercase_letter', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_require_lowercase_letter', FALSE, NULL)
+         , ('staging_octane', 'password_settings', 'pws_require_digit', FALSE, NULL)
+         , ('history_octane', 'password_settings', 'pws_pid', FALSE, 1)
+         , ('history_octane', 'password_settings', 'pws_version', FALSE, 2)
+         , ('history_octane', 'password_settings', 'pws_account_pid', FALSE, 3)
+         , ('history_octane', 'password_settings', 'pws_lender_user_expire_days', FALSE, 4)
+         , ('history_octane', 'password_settings', 'pws_lender_user_minimum_change_days', FALSE, 5)
+         , ('history_octane', 'password_settings', 'pws_lender_user_previous_password_ban', FALSE, 6)
+         , ('history_octane', 'password_settings', 'pws_minimum_length', FALSE, 7)
+         , ('history_octane', 'password_settings', 'pws_require_special_character', FALSE, 8)
+         , ('history_octane', 'password_settings', 'pws_require_uppercase_letter', FALSE, 9)
+         , ('history_octane', 'password_settings', 'pws_require_lowercase_letter', FALSE, 10)
+         , ('history_octane', 'password_settings', 'pws_require_digit', FALSE, 11)
+         , ('history_octane', 'password_settings', 'data_source_updated_datetime', FALSE, 12)
+         , ('history_octane', 'password_settings', 'data_source_deleted_flag', FALSE, 13)
+)
+
+   , new_staging_field_definitions AS (
+    INSERT INTO mdi.edw_field_definition (edw_table_definition_dwid, field_name, key_field_flag)
+        SELECT new_staging_table_definitions.dwid, new_fields.field_name, new_fields.key_field_flag
+        FROM new_fields
+        JOIN new_staging_table_definitions
+             ON new_fields.table_name = new_staging_table_definitions.table_name
+        WHERE new_fields.schema_name = 'staging_octane'
+        RETURNING dwid, edw_table_definition_dwid, field_name
+)
+
+   , new_history_field_definitions AS (
+    INSERT INTO mdi.edw_field_definition (edw_table_definition_dwid, field_name, key_field_flag,
+                                          source_edw_field_definition_dwid)
+        SELECT new_history_table_definitions.dwid
+             , new_fields.field_name
+             , new_fields.key_field_flag
+             , new_staging_field_definitions.dwid
+        FROM new_fields
+        JOIN new_staging_table_definitions
+             ON new_fields.table_name = new_staging_table_definitions.table_name
+        JOIN new_history_table_definitions
+             ON new_fields.table_name = new_history_table_definitions.table_name
+        LEFT JOIN new_staging_field_definitions
+                  ON new_staging_table_definitions.dwid =
+                     new_staging_field_definitions.edw_table_definition_dwid
+                      AND new_fields.field_name = new_staging_field_definitions.field_name
+        WHERE new_fields.schema_name = 'history_octane'
+)
+
+   , new_process_variables (name, target_table, json_output_field, sql) AS (
+    VALUES ('SP-100869', 'password_settings', 'pws_pid', '--finding records to insert into history_octane.password_settings
+SELECT staging_table.pws_pid
+     , staging_table.pws_version
+     , staging_table.pws_account_pid
+     , staging_table.pws_lender_user_expire_days
+     , staging_table.pws_lender_user_minimum_change_days
+     , staging_table.pws_lender_user_previous_password_ban
+     , staging_table.pws_minimum_length
+     , staging_table.pws_require_special_character
+     , staging_table.pws_require_uppercase_letter
+     , staging_table.pws_require_lowercase_letter
+     , staging_table.pws_require_digit
+     , FALSE AS data_source_deleted_flag
+     , NOW( ) AS data_source_updated_datetime
+FROM staging_octane.password_settings staging_table
+LEFT JOIN history_octane.password_settings history_table
+          ON staging_table.pws_pid = history_table.pws_pid
+              AND staging_table.pws_version = history_table.pws_version
+WHERE history_table.pws_pid IS NULL
+UNION ALL
+SELECT history_table.pws_pid
+     , history_table.pws_version + 1
+     , history_table.pws_account_pid
+     , history_table.pws_lender_user_expire_days
+     , history_table.pws_lender_user_minimum_change_days
+     , history_table.pws_lender_user_previous_password_ban
+     , history_table.pws_minimum_length
+     , history_table.pws_require_special_character
+     , history_table.pws_require_uppercase_letter
+     , history_table.pws_require_lowercase_letter
+     , history_table.pws_require_digit
+     , TRUE AS data_source_deleted_flag
+     , NOW( ) AS data_source_updated_datetime
+FROM history_octane.password_settings history_table
+LEFT JOIN staging_octane.password_settings staging_table
+          ON staging_table.pws_pid = history_table.pws_pid
+WHERE staging_table.pws_pid IS NULL
+  AND NOT EXISTS(
+    SELECT 1
+    FROM history_octane.password_settings deleted_records
+    WHERE deleted_records.pws_pid = history_table.pws_pid
+      AND deleted_records.data_source_deleted_flag = TRUE
+    );')
+)
+   , new_processes AS (
+    INSERT INTO mdi.process (name, description)
+        SELECT new_process_variables.name
+             , 'ETL to copy ' || new_process_variables.target_table || ' data from staging_octane to history_octane'
+        FROM new_process_variables
+        RETURNING dwid, name, description
+)
+
+   , new_table_input_steps AS (
+    INSERT INTO mdi.table_input_step (process_dwid, data_source_dwid, sql, limit_size, execute_for_each_row, replace_variables,
+                                      enable_lazy_conversion, cached_row_meta, connectionname)
+        SELECT new_processes.dwid
+             , 0
+             , new_process_variables.sql
+             , 0
+             , 'N'
+             , 'N'
+             , 'N'
+             , 'N'
+             , 'Staging DB Connection'
+        FROM new_processes
+        JOIN new_process_variables
+             ON new_processes.name = new_process_variables.name
+)
+
+   , new_table_output_steps AS (
+    INSERT INTO mdi.table_output_step (process_dwid, target_schema, target_table, commit_size, partitioning_field, table_name_field,
+                                       auto_generated_key_field, partition_data_per, table_name_defined_in_field,
+                                       return_auto_generated_key_field, truncate_table, connectionname, partition_over_tables,
+                                       specify_database_fields, ignore_insert_errors, use_batch_update)
+        SELECT new_processes.dwid
+             , 'history_octane'
+             , new_process_variables.target_table
+             , 1000
+             , NULL
+             , NULL
+             , NULL
+             , NULL
+             , 'N'
+             , NULL
+             , 'N'
+             , 'Staging DB Connection'
+             , 'N'
+             , 'Y'
+             , 'N'
+             , 'N'
+        FROM new_processes
+        JOIN new_process_variables
+             ON new_processes.name = new_process_variables.name
+        RETURNING dwid, target_schema, target_table
+)
+
+   , new_table_output_fields AS (
+    INSERT INTO mdi.table_output_field (table_output_step_dwid, database_field_name, database_stream_name, field_order, is_sensitive)
+        SELECT new_table_output_steps.dwid
+             , new_fields.field_name
+             , new_fields.field_name
+             , new_fields.field_order
+             , FALSE
+        FROM new_table_output_steps
+        JOIN new_fields
+             ON new_fields.schema_name = new_table_output_steps.target_schema
+                 AND new_fields.table_name = new_table_output_steps.target_table
+)
+
+   , new_json_output_fields AS (
+    INSERT INTO mdi.json_output_field (process_dwid, field_name)
+        SELECT new_processes.dwid, new_process_variables.json_output_field
+        FROM new_processes
+        JOIN new_process_variables
+             ON new_processes.name = new_process_variables.name
+)
+
+   , new_state_machine_definitions AS (
+    INSERT INTO mdi.state_machine_definition (process_dwid, name, comment)
+        SELECT new_processes.dwid, new_processes.name, new_processes.description
+        FROM new_processes
+)
+SELECT 'Finished inserting metadata for new tables: password_settings';
+
 -- Insert metadata for new column: proposal.prp_va_energy_efficient_improvements_amount
 WITH new_fields (table_name, field_name, field_order) AS (
     VALUES ('proposal', 'prp_va_energy_efficient_improvements_amount', 242)
