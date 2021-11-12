4e7671c8045a083eebb6eaca11fd2bca298e2dc5
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-11-12 17:31:48 -0500

    [#1201367615312267] add update permissions changes to pentaho_logging role

--- docker/database/migrations/staging-permissions/V2021_11_1_2__migration.sql
+++ docker/database/migrations/staging-permissions/V2021_11_1_2__migration.sql
@@ -5,5 +5,5 @@
 
 -- since etl_loan is a member of pentaho_logging, this will indirectly grant the necessary
 -- permission to etl_loan and be more flexible for other etl_* users in the future
-GRANT INSERT ON star_common.etl_log TO pentaho_logging;
+GRANT INSERT, UPDATE ON star_common.etl_log TO pentaho_logging;
 GRANT USAGE, SELECT ON star_common.etl_log_dwid_seq TO pentaho_logging;
