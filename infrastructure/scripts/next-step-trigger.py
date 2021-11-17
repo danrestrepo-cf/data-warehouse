b9edf940ff9f2f7d8b83819347f7d74e55e689a1
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-11-17 13:11:15 -0500

    [#1201354667788164] Updates per 2021.11.17 code review [#1201354667788164] Comment update [#1201354667788164] More comment updates

--- infrastructure/scripts/next-step-trigger.py
+++ infrastructure/scripts/next-step-trigger.py
@@ -7,7 +7,7 @@ import os
 # Read an SQS message that indicates which job should be triggered next
 #
 # It assumes the following to accomplish this:
-#    1. The ProcessID (ex, SP-100) is pass in as an SQS Message Attribute
+#    1. The ProcessID (ex, SP-100) is passed in as an SQS Message Attribute
 #    2. The Step Function ARN prefix is available as a environment variable, 'sfn_arn_prefix'
 #    3. The Step Function to call is the concatenation of ARN Prefix and ProcessID
 #
@@ -24,48 +24,82 @@ def execute(event, context):
         logger.info("Next Step Input: {}".format(next_step_input))
 
         process_id = record["messageAttributes"]["ProcessId"]["stringValue"]
+        # strip out ETL type suffix where applicable
+        process_id_base = '-'.join(process_id.split('-')[0:2])
         logger.info("ProcessId: {}".format(process_id))
 
         try:
             sfn = boto3.client('stepfunctions')
             state_machine_arn = sfn_arn_prefix + "-" + process_id
+            state_machine_arn_base = sfn_arn_prefix + "-" + process_id_base
+            state_machine_found_flag = False
 
-            # Return dictionary of step functions named under the *OLD* naming scheme, e.g. SP-100320
-            executions_base = sfn.list_executions(
-                stateMachineArn=state_machine_arn,
-                statusFilter='RUNNING',
-                maxResults=1
-            )
-
-            # Return dictionary of insert ETL step functions named under the *NEW* naming scheme, e.g. SP-200001-insert
-            executions_insert = sfn.list_executions(
-                stateMachineArn=(state_machine_arn + '-insert'),
-                statusFilter='RUNNING',
-                maxResults=1
-            )
-
-            # Return dictionary of insert-update ETL step functions named under the *NEW* naming scheme, e.g. SP-300001-insert-update
-            executions_insert_update = sfn.list_executions(
-                stateMachineArn=(state_machine_arn + '-insert-update'),
-                statusFilter='RUNNING',
-                maxResults=1
-            )
-
-            # Return dictionary of delete ETL step functions named under the *NEW* naming scheme, e.g. SP-300001-delete
-            executions_delete = sfn.list_executions(
-                stateMachineArn=(state_machine_arn + '-delete'),
-                statusFilter='RUNNING',
-                maxResults=1
-            )
-
-            num_executions = len(executions_base["executions"]) + len(executions_insert["executions"]) + len(
-                executions_insert_update["executions"]) + len(executions_delete["executions"])
-            if num_executions == 0:
-                logger.info("Running {}".format(state_machine_arn))
-                sfn.start_execution(
-                    stateMachineArn=state_machine_arn,
-                    input=next_step_input
+            # Count running step functions named under the *OLD* naming scheme, e.g. SP-100320
+            try:
+                executions_base = sfn.list_executions(
+                    stateMachineArn=state_machine_arn_base,
+                    statusFilter='RUNNING',
+                    maxResults=1
                 )
+                num_executions_base = len(executions_base["executions"])
+                if state_machine_arn == state_machine_arn_base:
+                    state_machine_found_flag = True
+            except sfn.client.exceptions.StateMachineDoesNotExist:
+                num_executions_base = 0
+
+            # Count running insert ETL step functions named under the *NEW* naming scheme, e.g. SP-200001-insert
+            try:
+                executions_insert = sfn.list_executions(
+                    stateMachineArn=(state_machine_arn_base + '-insert'),
+                    statusFilter='RUNNING',
+                    maxResults=1
+                )
+                num_executions_insert = len(executions_insert["executions"])
+                if state_machine_arn == (state_machine_arn_base + '-insert'):
+                    state_machine_found_flag = True
+            except sfn.client.exceptions.StateMachineDoesNotExist:
+                num_executions_insert = 0
+
+            # Count running insert-update ETL step functions named under the *NEW* naming scheme,
+            # e.g. SP-300001-insert-update
+            try:
+                executions_insert_update = sfn.list_executions(
+                    stateMachineArn=(state_machine_arn_base + '-insert-update'),
+                    statusFilter='RUNNING',
+                    maxResults=1
+                )
+                num_executions_insert_update = len(executions_insert_update["executions"])
+                if state_machine_arn == (state_machine_arn_base + '-insert-update'):
+                    state_machine_found_flag = True
+            except sfn.client.exceptions.StateMachineDoesNotExist:
+                num_executions_insert_update = 0
+
+            # Count running delete ETL step functions named under the *NEW* naming scheme, e.g. SP-300001-delete
+            try:
+                executions_delete = sfn.list_executions(
+                    stateMachineArn=(state_machine_arn_base + '-delete'),
+                    statusFilter='RUNNING',
+                    maxResults=1
+                )
+                num_executions_delete = len(executions_delete["executions"])
+                if state_machine_arn == (state_machine_arn_base + '-delete'):
+                    state_machine_found_flag = True
+            except sfn.client.exceptions.StateMachineDoesNotExist:
+                num_executions_delete = 0
+
+            num_executions = num_executions_base + num_executions_insert + num_executions_insert_update + num_executions_delete
+
+            if num_executions == 0:
+                if state_machine_found_flag:
+                    logger.info("Running {}".format(state_machine_arn))
+                    sfn.start_execution(
+                        stateMachineArn=state_machine_arn,
+                        input=next_step_input
+                    )
+                else:
+                    logger.info("ERROR: State machine not found: {}".format(state_machine_arn))
+                    # Do not raise an exception because we do not want this message to go back onto the queue and
+                    # cause an infinite loop
             else:
                 logger.info("Num running executions is {} for step function {}, sleeping message."
                             .format(num_executions, process_id))
