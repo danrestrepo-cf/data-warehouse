32804e6cae9ab776e61d589867047484068fae79
Cory Boulé <cory.boule@cardinalfinancial.com>
2021-11-17 08:59:49 -0500

    [#1201354667788164] Updates per 2021.11.17 offline code review

--- docker/database/migrations/config/V2021_11_2_0__migration.sql
+++ docker/database/migrations/config/V2021_11_2_0__migration.sql
@@ -3,6 +3,7 @@
 -- https://app.asana.com/0/0/1201354667788164
 --
 
+-- mdi.process
 UPDATE mdi.process
     SET name = 'SP-200001-insert'
     WHERE name = 'SP-200001';
@@ -16,5 +17,23 @@ UPDATE mdi.process
     WHERE name = 'SP-300001';
 
 UPDATE mdi.process
+    SET name = 'SP-300001-delete'
+    WHERE name = 'SP-300002';
+
+
+-- mdi.state_machine_definition
+UPDATE mdi.state_machine_definition
+    SET name = 'SP-200001-insert'
+    WHERE name = 'SP-200001';
+
+UPDATE mdi.state_machine_definition
+    SET name = 'SP-200001-delete'
+    WHERE name = 'SP-200002';
+
+UPDATE mdi.state_machine_definition
+    SET name = 'SP-300001-insert-update'
+    WHERE name = 'SP-300001';
+
+UPDATE mdi.state_machine_definition
     SET name = 'SP-300001-delete'
     WHERE name = 'SP-300002';
