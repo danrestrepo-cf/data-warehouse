5b88afac0b21068d1ba8d3b1bcd1909461741361
Chris Edwards <chris.edwards@cardinalfinancial.com>
2021-11-18 14:06:21 -0500

    [#1201374364166630] add staging db ddl changes

---
+++ docker/database/migrations/staging/V2021_99_0_0__migration.sql
@@ -0,0 +1,81 @@
+--
+-- Main | Octane schemas from prod-release to v2021.11.3.0 on uat
+-- https://app.asana.com/0/0/1201374364166630
+--
+
+--staging_octane changes
+
+ALTER TABLE staging_octane.smart_message
+    ADD COLUMN smsg_allow_custom_text BOOLEAN;
+
+CREATE TABLE staging_octane.smart_message_available_attachment (
+    smaa_pid BIGINT,
+    smaa_version INTEGER,
+    CONSTRAINT pk_smart_message_available_attachment
+        PRIMARY KEY (smaa_pid),
+    smaa_smart_message_pid BIGINT,
+    smaa_smart_doc_pid BIGINT
+);
+
+CREATE INDEX idx_smart_message_available_attachment__pid_version ON staging_octane.smart_message_available_attachment (smaa_pid, smaa_version);
+
+CREATE TABLE staging_octane.deal_message_log_attachment (
+    dmloga_pid BIGINT,
+    dmloga_version INTEGER,
+    CONSTRAINT pk_deal_message_log_attachment
+        PRIMARY KEY (dmloga_pid),
+    dmloga_deal_message_log_pid BIGINT,
+    dmloga_deal_file_pid BIGINT
+);
+
+CREATE INDEX idx_deal_message_log_attachment__pid_version ON staging_octane.deal_message_log_attachment (dmloga_pid, dmloga_version);
+
+ALTER TABLE staging_octane.deal_message_log
+    DROP COLUMN dmlog_attachment_deal_file_pid;
+
+--history_octane changes
+
+ALTER TABLE history_octane.smart_message
+    ADD COLUMN smsg_allow_custom_text BOOLEAN;
+
+CREATE TABLE history_octane.smart_message_available_attachment (
+    smaa_pid BIGINT,
+    smaa_version INTEGER,
+    smaa_smart_message_pid BIGINT,
+    smaa_smart_doc_pid BIGINT,
+    data_source_updated_datetime timestamptz,
+    data_source_deleted_flag BOOLEAN
+);
+
+CREATE INDEX idx_smart_message_available_attachment__pid ON history_octane.smart_message_available_attachment (smaa_pid);
+
+CREATE INDEX idx_fb4b88abd8f35bc1cda1f9493404e8ca ON history_octane.smart_message_available_attachment (data_source_updated_datetime);
+
+CREATE INDEX idx_aeff4b7de9551bb966d2c0647d2077cf ON history_octane.smart_message_available_attachment (data_source_deleted_flag);
+
+CREATE INDEX idx_smart_message_available_attachment__pid_version ON history_octane.smart_message_available_attachment (smaa_pid, smaa_version);
+
+CREATE INDEX fk_smart_message_available_attachment_1 ON history_octane.smart_message_available_attachment (smaa_smart_message_pid);
+
+CREATE INDEX fk_smart_message_available_attachment_2 ON history_octane.smart_message_available_attachment (smaa_smart_doc_pid);
+
+CREATE TABLE history_octane.deal_message_log_attachment (
+    dmloga_pid BIGINT,
+    dmloga_version INTEGER,
+    dmloga_deal_message_log_pid BIGINT,
+    dmloga_deal_file_pid BIGINT,
+    data_source_updated_datetime timestamptz,
+    data_source_deleted_flag BOOLEAN
+);
+
+CREATE INDEX idx_deal_message_log_attachment__pid ON history_octane.deal_message_log_attachment (dmloga_pid);
+
+CREATE INDEX idx_deal_message_log_attachment__data_source_updated_datetime ON history_octane.deal_message_log_attachment (data_source_updated_datetime);
+
+CREATE INDEX idx_deal_message_log_attachment__data_source_deleted_flag ON history_octane.deal_message_log_attachment (data_source_deleted_flag);
+
+CREATE INDEX idx_deal_message_log_attachment__pid_version ON history_octane.deal_message_log_attachment (dmloga_pid, dmloga_version);
+
+CREATE INDEX fk_deal_message_log_attachment_1 ON history_octane.deal_message_log_attachment (dmloga_deal_message_log_pid);
+
+CREATE INDEX fk_deal_message_log_attachment_2 ON history_octane.deal_message_log_attachment (dmloga_deal_file_pid);
