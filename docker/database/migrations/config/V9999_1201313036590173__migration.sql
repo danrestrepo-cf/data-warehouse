bf75e8b5a67748d47cd1c114de6f654d1be63334
Christopher Edwards <chris.edwards@cardinalfinancial.com>
2022-01-06 16:31:22 -0500

    [#1201313036590173] trigger SP-200018 after SP-100813

--- docker/database/migrations/config/V9999_1201313036590173__migration.sql
+++ docker/database/migrations/config/V9999_1201313036590173__migration.sql
@@ -4,6 +4,23 @@
 --
 
 /*
+INSERTIONS
+*/
+
+--state_machine_step
+WITH insert_rows (process_name, next_process_name) AS (
+    VALUES ('SP-100813', 'SP-200018')
+)
+INSERT
+INTO mdi.state_machine_step (process_dwid, next_process_dwid)
+SELECT process.dwid, next_process.dwid
+FROM insert_rows
+JOIN mdi.process
+     ON process.name = insert_rows.process_name
+JOIN mdi.process next_process
+     ON next_process.name = insert_rows.next_process_name;
+
+/*
 UPDATES
 */
 
