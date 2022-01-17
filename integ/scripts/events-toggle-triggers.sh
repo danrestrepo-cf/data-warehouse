104829ad0520063bc6f6a2990fe700e6990ec702
Tom Haley <thomas.haley@cardinalfinancial.com>
2022-01-17 13:39:49 -0500

    [#1200583418301062] paginate event triggers

--- integ/scripts/events-toggle-triggers.sh
+++ integ/scripts/events-toggle-triggers.sh
@@ -22,21 +22,33 @@ fi
 
 name_prefix="${environment}-bi-managed"
 
-echo "Fetching event rules to ${action}, starting with: ${name_prefix}"
-rule_names=$(aws events list-rules --no-paginate --name-prefix "${name_prefix}" \
-  --output text --query 'Rules[].{Name:Name}')
-while read -r rule_name; do
-  if [ "${rule_name}" != "" ]; then
-    if [ "${action}" == "enable" ]; then
-      echo "Enabling rule ${rule_name}"
-      aws events enable-rule --name "${rule_name}"
-    elif [ "${action}" == "disable" ]; then
-      echo "Disabling rule ${rule_name}"
-      aws events disable-rule --name "${rule_name}"
-    fi
-    echo "Rule ${rule_name} has been disabled."
+next_token="first"
+# The next_token has "None None None" in it, which is odd, but we can regex check it
+while [ "${next_token}" != "" ] && [[ ! $next_token =~ "None" ]]; do
+  pagination=""
+  if [ "${next_token}" != "" ] && [ "${next_token}" != "first" ]; then
+    echo "Has next token: ${next_token}"
+    pagination="--starting-token ${next_token}"
   fi
-done <<<"${rule_names}"
+
+  echo "Fetching event rules to ${action}, starting with: ${name_prefix}"
+  rule_names=$(aws events list-rules ${pagination} --name-prefix "${name_prefix}" \
+    --output text --query 'Rules[].{Name:Name}')
+  next_token=$(aws events list-rules ${pagination} --name-prefix "${name_prefix}" \
+    --output text --query 'nextToken')
+  while read -r rule_name; do
+    if [ "${rule_name}" != "" ]; then
+      if [ "${action}" == "enable" ]; then
+        echo "Enabling rule ${rule_name}"
+        aws events enable-rule --name "${rule_name}"
+      elif [ "${action}" == "disable" ]; then
+        echo "Disabling rule ${rule_name}"
+        aws events disable-rule --name "${rule_name}"
+      fi
+      echo "Rule ${rule_name} has been ${action}d."
+    fi
+  done <<<"${rule_names}"
+done
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/events/disable-rule.html
 # "Allow a short period of time for changes to take effect."
