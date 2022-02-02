a396abc9e33dd7571aec6bcf025d9c2613ff80a7
Christopher Edwards <chris.edwards@cardinalfinancial.com>
2022-02-02 11:31:14 -0500

    [#1201056914554058] update DML migration to use simpler "modified date time" comparison when detecting/deleting duplicate rows

--- docker/database/migrations/staging/V9999_1201056914554058__migration.sql
+++ docker/database/migrations/staging/V9999_1201056914554058__migration.sql
@@ -25,46 +25,16 @@ WITH delete_query AS (
     DELETE
         FROM star_loan.hmda_purchaser_of_loan_dim
             USING (
-                SELECT up_to_date_dim_rows.dwid AS current_dwid
+                SELECT MAX( all_rows.dwid ) AS current_dwid
                      , duplicates.dwid AS duplicate_dwid
-                FROM (
-                    SELECT DISTINCT all_rows.dwid
-                                  , all_rows.data_source_integration_id
-                    FROM star_loan.hmda_purchaser_of_loan_dim all_rows
-                    JOIN star_loan.hmda_purchaser_of_loan_dim duplicates
-                         ON all_rows.data_source_integration_id = duplicates.data_source_integration_id
-                             AND all_rows.dwid <> duplicates.dwid
-                ) AS duplicates
-                JOIN (
-                    SELECT hmda_purchaser_of_loan_dim.*
-                    FROM star_loan.hmda_purchaser_of_loan_dim
-                    JOIN (
-                        SELECT hmda_purchaser_of_loan_2017_type.*
-                        FROM history_octane.hmda_purchaser_of_loan_2017_type
-                        LEFT JOIN history_octane.hmda_purchaser_of_loan_2017_type AS history_records
-                                  ON hmda_purchaser_of_loan_2017_type.code = history_records.code
-                                      AND
-                                     hmda_purchaser_of_loan_2017_type.data_source_updated_datetime <
-                                     history_records.data_source_updated_datetime
-                        WHERE history_records.code IS NULL
-                    ) AS most_recent_hmda_purchaser_of_loan_2017_type
-                         ON hmda_purchaser_of_loan_dim.code_2017 = most_recent_hmda_purchaser_of_loan_2017_type.code
-                             AND hmda_purchaser_of_loan_dim.value_2017 = most_recent_hmda_purchaser_of_loan_2017_type.value
-                    JOIN (
-                        SELECT hmda_purchaser_of_loan_2018_type.*
-                        FROM history_octane.hmda_purchaser_of_loan_2018_type
-                        LEFT JOIN history_octane.hmda_purchaser_of_loan_2018_type AS history_records
-                                  ON hmda_purchaser_of_loan_2018_type.code = history_records.code
-                                      AND
-                                     hmda_purchaser_of_loan_2018_type.data_source_updated_datetime <
-                                     history_records.data_source_updated_datetime
-                        WHERE history_records.code IS NULL
-                    ) AS most_recent_hmda_purchaser_of_loan_2018_type
-                         ON hmda_purchaser_of_loan_dim.code_2018 = most_recent_hmda_purchaser_of_loan_2018_type.code
-                             AND hmda_purchaser_of_loan_dim.value_2018 = most_recent_hmda_purchaser_of_loan_2018_type.value
-                ) AS up_to_date_dim_rows
-                     ON up_to_date_dim_rows.data_source_integration_id = duplicates.data_source_integration_id
-                         AND up_to_date_dim_rows.dwid <> duplicates.dwid
+                FROM star_loan.hmda_purchaser_of_loan_dim all_rows
+                JOIN star_loan.hmda_purchaser_of_loan_dim duplicates
+                     ON all_rows.data_source_integration_id = duplicates.data_source_integration_id
+                         AND all_rows.dwid <> duplicates.dwid
+                         AND ((all_rows.edw_modified_datetime > duplicates.edw_modified_datetime)
+                             OR (all_rows.edw_modified_datetime = duplicates.edw_modified_datetime
+                                 AND all_rows.dwid > duplicates.dwid))
+                GROUP BY duplicates.dwid
             ) AS duplicates
             WHERE hmda_purchaser_of_loan_dim.dwid = duplicates.duplicate_dwid
             RETURNING duplicates.current_dwid, duplicates.duplicate_dwid
