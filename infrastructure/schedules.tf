f810ef60db6c616b414d3a1ff5ae4e2beaaa324e
Cory Boulé <cory.boule@cardinalfinancial.com>
2022-02-11 16:34:39 -0500

    [#1201824690875549] remove SP-GROUP-1s; add SP-GROUP-2

--- infrastructure/schedules.tf
+++ infrastructure/schedules.tf
@@ -2,7 +2,6 @@ locals {
   schedules = {
   // Note: EventBridge cron schedules are UTC+0, so an hour of "5" means 12:00 AM in EST, 1:00 AM in EDT
   // see https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-create-rule-schedule.html
-    "SP-GROUP-1-1.json" = "cron(15 5 * * ? *)"
-    "SP-GROUP-1-2.json" = "cron(15 5 * * ? *)"
+    "SP-GROUP-2.json" = "cron(15 5 * * ? *)"
   }
 }
