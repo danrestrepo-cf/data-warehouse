cf1b8356a6be0ca968af47baadbeed8e7fa93c09
Christopher Edwards <chris.edwards@cardinalfinancial.com>
2022-03-07 13:26:09 -0500

    [#1201859471430286] RAW auto-generated metadata maintenance SQL (V3)

--- docker/database/migrations/config/V9999_1201859471430286__migration.sql
+++ docker/database/migrations/config/V9999_1201859471430286__migration.sql
@@ -218,7 +218,7 @@ JOIN (
       AND NOT org_node.data_source_deleted_flag
 ) AS org_node
      ON org_node_lender_user.onlu_org_node_pid = org_node.on_pid
---filter down to current org_node_version
+    --filter down to current org_node_version
 JOIN (
     SELECT org_node_version.*
     FROM history_octane.org_node_version
@@ -240,7 +240,12 @@ JOIN (
     WHERE history_records.code IS NULL
       AND NOT org_node_lender_user_type.data_source_deleted_flag
 ) AS org_node_lender_user_type
-     ON org_node_lender_user.onlu_org_node_lender_user_type = org_node_lender_user_type.code;', 'Staging DB Connection')
+     ON org_node_lender_user.onlu_org_node_lender_user_type = org_node_lender_user_type.code
+LEFT JOIN data_mart_business_applications.edw_current_parent_node_employee_leaders
+          ON org_node.on_org_node_id = edw_current_parent_node_employee_leaders.parent_node_id
+              AND lender_user.lu_company_user_id = edw_current_parent_node_employee_leaders.leader_company_user_id
+              AND org_node_lender_user_type.value = edw_current_parent_node_employee_leaders.leader_type
+WHERE edw_current_parent_node_employee_leaders.dwid IS NULL;', 'Staging DB Connection')
          , ('SP-400003-delete', 0, '--SP-400003-delete
 SELECT edw_current_parent_node_employee_leaders.data_source_integration_id
 FROM data_mart_business_applications.edw_current_parent_node_employee_leaders
@@ -438,7 +443,7 @@ WITH insert_rows (process_name, update_lookup, update_flag) AS (
          , ('SP-400002-insert-update', 'data_source_integration_columns', 'Y')
          , ('SP-400002-insert-update', 'data_source_integration_id', 'N')
          , ('SP-400002-insert-update', 'data_source_modified_datetime', 'Y')
-         , ('SP-400002-insert-update', 'parent_node_id', 'N')
+         , ('SP-400002-insert-update', 'parent_node_id', 'Y')
          , ('SP-400002-insert-update', 'parent_node_name', 'Y')
          , ('SP-400002-insert-update', 'grandparent_node_id', 'Y')
          , ('SP-400002-insert-update', 'grandparent_node_name', 'Y')
@@ -449,8 +454,8 @@ WITH insert_rows (process_name, update_lookup, update_flag) AS (
          , ('SP-400003-insert-update', 'data_source_integration_columns', 'Y')
          , ('SP-400003-insert-update', 'data_source_integration_id', 'N')
          , ('SP-400003-insert-update', 'data_source_modified_datetime', 'Y')
-         , ('SP-400003-insert-update', 'parent_node_id', 'N')
-         , ('SP-400003-insert-update', 'leader_company_user_id', 'N')
+         , ('SP-400003-insert-update', 'parent_node_id', 'Y')
+         , ('SP-400003-insert-update', 'leader_company_user_id', 'Y')
          , ('SP-400003-insert-update', 'leader_type', 'Y')
          , ('SP-400001-insert-update', 'data_source_dwid', 'Y')
          , ('SP-400001-insert-update', 'edw_created_datetime', 'N')
@@ -462,7 +467,7 @@ WITH insert_rows (process_name, update_lookup, update_flag) AS (
          , ('SP-400001-insert-update', 'parent_node_id', 'Y')
          , ('SP-400001-insert-update', 'parent_node_name', 'Y')
          , ('SP-400001-insert-update', 'company_user_id', 'Y')
-         , ('SP-400001-insert-update', 'octane_username', 'N')
+         , ('SP-400001-insert-update', 'octane_username', 'Y')
          , ('SP-400001-insert-update', 'first_name', 'Y')
          , ('SP-400001-insert-update', 'last_name', 'Y')
          , ('SP-400001-insert-update', 'professional_title', 'Y')
@@ -541,7 +546,9 @@ JOIN mdi.process
 
 --state_machine_step
 WITH insert_rows (process_name, next_process_name) AS (
-    VALUES ('SP-100090', 'SP-400001-delete')
+    VALUES ('SP-100092', 'SP-200001-delete')
+         , ('SP-100090', 'SP-200001-delete')
+         , ('SP-100090', 'SP-400001-delete')
          , ('SP-100090', 'SP-400001-insert-update')
          , ('SP-100090', 'SP-400003-delete')
          , ('SP-100090', 'SP-400003-insert-update')
@@ -559,6 +566,8 @@ WITH insert_rows (process_name, next_process_name) AS (
          , ('SP-100369', 'SP-400001-insert-update')
          , ('SP-100369', 'SP-400002-delete')
          , ('SP-100369', 'SP-400002-insert-update')
+         , ('SP-300001-insert-update', 'SP-200001-delete')
+         , ('SP-300001-delete', 'SP-200001-delete')
 )
 INSERT
 INTO mdi.state_machine_step (process_dwid, next_process_dwid)
@@ -568,3 +577,119 @@ JOIN mdi.process
      ON process.name = insert_rows.process_name
 JOIN mdi.process next_process
      ON next_process.name = insert_rows.next_process_name;
+
+/*
+UPDATES
+*/
+
+--delete_step
+WITH update_rows (process_name, connectionname, schema_name, table_name) AS (
+    VALUES ('SP-200001-delete-0', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-1', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-2', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-3', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-4', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-5', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-6', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-7', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-8', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-9', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-10', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-11', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-12', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-13', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-14', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-15', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-16', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-17', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-18', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-19', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-20', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-21', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-22', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-23', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-24', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-25', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-26', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-27', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-28', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-29', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-30', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-31', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-32', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-33', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-34', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-35', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-36', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-37', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-38', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-39', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-40', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-41', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-42', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-43', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-44', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-45', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-46', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-47', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-48', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-49', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-50', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-51', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-52', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-53', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-54', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-55', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-56', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-57', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-58', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-59', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-60', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-61', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-62', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-63', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-64', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-65', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-66', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-67', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-68', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-69', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-70', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-71', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-72', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-73', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-74', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-75', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-76', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-77', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-78', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-79', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-80', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-81', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-82', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-83', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-84', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-85', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-86', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-87', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-88', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-89', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-90', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-91', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-92', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-93', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-94', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-95', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-96', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-97', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-98', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+         , ('SP-200001-delete-99', 'Staging DB Connection', 'star_loan', 'loan_lender_user_access')
+)
+UPDATE mdi.delete_step
+SET connectionname = update_rows.connectionname
+  , schema_name = update_rows.schema_name
+  , table_name = update_rows.table_name
+FROM update_rows
+JOIN mdi.process
+     ON process.name = update_rows.process_name
+WHERE process.dwid = delete_step.process_dwid;
