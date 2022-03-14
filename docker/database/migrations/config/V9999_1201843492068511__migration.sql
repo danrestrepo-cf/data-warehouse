5f06dada0f3ed5b76eb961c90e9897ed3ce5b571
Christopher Edwards <chris.edwards@cardinalfinancial.com>
2022-03-14 14:56:28 -0400

    [#1201843492068511] remove state_machine_definition and state_machine_step tables and all maintenance code related thereto

---
+++ docker/database/migrations/config/V9999_1201843492068511__migration.sql
@@ -0,0 +1,7 @@
+--
+-- EDW | metadata - update YAMLs and python-utilities scripts to accommodate "per partition" ETLs
+-- https://app.asana.com/0/0/1201843492068511
+--
+
+DROP TABLE mdi.state_machine_definition;
+DROP TABLE mdi.state_machine_step;
