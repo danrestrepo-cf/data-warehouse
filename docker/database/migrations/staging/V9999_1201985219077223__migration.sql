2cd85264c5d579427e80508395efb29277b0f7a0
Christopher Edwards <chris.edwards@cardinalfinancial.com>
2022-03-18 14:54:17 -0400

    [#1201985219077223] create new borrower dwid column

--- docker/database/migrations/staging/V9999_1201985219077223__migration.sql
+++ docker/database/migrations/staging/V9999_1201985219077223__migration.sql
@@ -3,6 +3,7 @@
 -- https://app.asana.com/0/0/1201985219077223
 --
 
+--create borrower_hmda_collection_dim table and indexes
 CREATE TABLE star_loan.borrower_hmda_collection_dim (
     dwid BIGSERIAL NOT NULL
         CONSTRAINT pk_borrower_hmda_collection_dim
@@ -40,3 +41,9 @@ CREATE INDEX idx_borrower_hmda_collection_dim__data_source_integration_id ON sta
 INSERT
 INTO star_loan.borrower_hmda_collection_dim (dwid, data_source_dwid, edw_created_datetime, edw_modified_datetime, etl_batch_id, data_source_integration_columns, data_source_integration_id, data_source_modified_datetime, ethnicity_collected_visual_or_surname, ethnicity_collected_visual_or_surname_code, ethnicity_not_obtainable_flag, ethnicity_refused, ethnicity_refused_code, race_collected_visual_or_surname, race_collected_visual_or_surname_code, race_information_not_provided_flag, race_national_origin_refusal_flag, race_not_obtainable_flag, race_refused, race_refused_code, sex_collected_visual_or_surname, sex_collected_visual_or_surname_code, sex_not_obtainable_flag, sex_refused, sex_refused_code)
 VALUES (0, 0, NOW( ), NOW( ), NULL, 'ethnicity_collected_visual_or_surname_code~ethnicity_not_obtainable_flag~ethnicity_refused_code~race_collected_visual_or_surname_code~race_information_not_provided_flag~race_national_origin_refusal_flag~race_not_obtainable_flag~race_refused_code~sex_collected_visual_or_surname_code~sex_not_obtainable_flag~sex_refused_code~data_source_dwid', '<NULL>~<NULL>~<NULL>~<NULL>~<NULL>~<NULL>~<NULL>~<NULL>~<NULL>~<NULL>~<NULL>~0', NOW( ), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+--add associated dwid column and index to borrower_dim table
+ALTER TABLE star_loan.borrower_dim
+    ADD COLUMN borrower_hmda_collection_dwid BIGINT;
+
+CREATE INDEX idx_borrower_dim__borrower_hmda_collection_dwid ON star_loan.borrower_dim (borrower_hmda_collection_dwid);
