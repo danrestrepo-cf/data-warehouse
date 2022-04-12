5d087d0e7c9bee9cf8f8ac1b4efeaa26c90a79ae
Christopher Edwards <chris.edwards@cardinalfinancial.com>
2022-04-12 10:39:59 -0400

    [#1201990575730852] remove duplicate borrower_hmda_collection_dim creation statement

--- docker/database/migrations/staging/V9999_1201990575730852__migration.sql
+++ docker/database/migrations/staging/V9999_1201990575730852__migration.sql
@@ -3,6 +3,10 @@
 -- https://app.asana.com/0/0/1201990575730852
 --
 
+-- TODO: update zero row integration columns and ID (remove values, add "<NULL>" to ids
+-- TODO: seed initial data into new loan_fact columns
+-- TODO: seed initial data into new borrower_dim columns
+
 /*
  update loan_fact columns/indexes
  */
@@ -163,44 +167,6 @@ CREATE INDEX idx_a924558105603ba33130b825269a41e8 ON star_loan.borrower_finances
 INSERT
 INTO star_loan.borrower_finances_declarations_dim (dwid, data_source_dwid, edw_created_datetime, edw_modified_datetime, etl_batch_id, data_source_integration_columns, data_source_integration_id, data_source_modified_datetime, bankruptcy, bankruptcy_code, outstanding_judgements, outstanding_judgements_code, bankruptcy_chapter_7, bankruptcy_chapter_7_code, bankruptcy_chapter_11, bankruptcy_chapter_11_code, bankruptcy_chapter_12, bankruptcy_chapter_12_code, bankruptcy_chapter_13, bankruptcy_chapter_13_code, completed_pre_foreclosure_short_sale, completed_pre_foreclosure_short_sale_code, conveyed_title_in_lieu_of_foreclosure, conveyed_title_in_lieu_of_foreclosure_code, cosigner_undisclosed, cosigner_undisclosed_code, currently_delinquent_federal_debt, currently_delinquent_federal_debt_code, party_to_lawsuit, party_to_lawsuit_code, property_foreclosure, property_foreclosure_code)
 VALUES (0, 0, NOW( ), NOW( ), NULL, 'bankruptcy~bankruptcy_code~outstanding_judgements~outstanding_judgements_code~bankruptcy_chapter_7~bankruptcy_chapter_7_code~bankruptcy_chapter_11~bankruptcy_chapter_11_code~bankruptcy_chapter_12~bankruptcy_chapter_12_code~bankruptcy_chapter_13~bankruptcy_chapter_13_code~completed_pre_foreclosure_short_sale~completed_pre_foreclosure_short_sale_code~conveyed_title_in_lieu_of_foreclosure~conveyed_title_in_lieu_of_foreclosure_code~cosigner_undisclosed~cosigner_undisclosed_code~currently_delinquent_federal_debt~currently_delinquent_federal_debt_code~party_to_lawsuit~party_to_lawsuit_code~property_foreclosure~property_foreclosure_code~data_source_dwid', '~~~~~~~~~~~~~~~~~~~~~~~~', NOW( ), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
---borrower_hmda_collection_dim
-CREATE TABLE star_loan.borrower_hmda_collection_dim (
-    dwid BIGSERIAL NOT NULL
-        CONSTRAINT pk_borrower_hmda_collection_dim
-            PRIMARY KEY,
-    data_source_dwid BIGINT NOT NULL,
-    edw_created_datetime TIMESTAMPTZ,
-    edw_modified_datetime TIMESTAMPTZ,
-    etl_batch_id TEXT,
-    data_source_integration_columns TEXT,
-    data_source_integration_id TEXT NOT NULL,
-    data_source_modified_datetime TIMESTAMPTZ,
-    ethnicity_collected_visual_or_surname VARCHAR(1024),
-    ethnicity_collected_visual_or_surname_code VARCHAR(128),
-    ethnicity_not_obtainable_flag BOOLEAN,
-    ethnicity_refused VARCHAR(1024),
-    ethnicity_refused_code VARCHAR(128),
-    race_collected_visual_or_surname VARCHAR(1024),
-    race_collected_visual_or_surname_code VARCHAR(128),
-    race_information_not_provided_flag BOOLEAN,
-    race_national_origin_refusal_flag BOOLEAN,
-    race_not_obtainable_flag BOOLEAN,
-    race_refused VARCHAR(1024),
-    race_refused_code VARCHAR(128),
-    sex_collected_visual_or_surname VARCHAR(1024),
-    sex_collected_visual_or_surname_code VARCHAR(128),
-    sex_not_obtainable_flag BOOLEAN,
-    sex_refused VARCHAR(1024),
-    sex_refused_code VARCHAR(128)
-);
-
-CREATE INDEX idx_borrower_hmda_collection_dim__etl_batch_id ON star_loan.borrower_hmda_collection_dim (etl_batch_id);
-CREATE INDEX idx_borrower_hmda_collection_dim__data_source_integration_id ON star_loan.borrower_hmda_collection_dim (data_source_integration_id);
-
-INSERT
-INTO star_loan.borrower_hmda_collection_dim (dwid, data_source_dwid, edw_created_datetime, edw_modified_datetime, etl_batch_id, data_source_integration_columns, data_source_integration_id, data_source_modified_datetime, ethnicity_collected_visual_or_surname, ethnicity_collected_visual_or_surname_code, ethnicity_not_obtainable_flag, ethnicity_refused, ethnicity_refused_code, race_collected_visual_or_surname, race_collected_visual_or_surname_code, race_information_not_provided_flag, race_national_origin_refusal_flag, race_not_obtainable_flag, race_refused, race_refused_code, sex_collected_visual_or_surname, sex_collected_visual_or_surname_code, sex_not_obtainable_flag, sex_refused, sex_refused_code)
-VALUES (0, 0, NOW( ), NOW( ), NULL, 'ethnicity_collected_visual_or_surname~ethnicity_collected_visual_or_surname_code~ethnicity_not_obtainable_flag~ethnicity_refused~ethnicity_refused_code~race_collected_visual_or_surname~race_collected_visual_or_surname_code~race_information_not_provided_flag~race_national_origin_refusal_flag~race_not_obtainable_flag~race_refused~race_refused_code~sex_collected_visual_or_surname~sex_collected_visual_or_surname_code~sex_not_obtainable_flag~sex_refused~sex_refused_code~data_source_dwid', '~~~~~~~~~~~~~~~~~', NOW( ), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 --borrower_hmda_ethnicity_dim
 CREATE TABLE star_loan.borrower_hmda_ethnicity_dim (
