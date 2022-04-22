0db6fbeb190f20cb2657d28a924301e61234a604
codyshearer-cf <103470495+codyshearer-cf@users.noreply.github.com>
2022-04-22 08:43:57 -0700

    Update V2022_4_3_0__migration.sql

--- docker/database/migrations/staging/V2022_4_3_0__migration.sql
+++ docker/database/migrations/staging/V2022_4_3_0__migration.sql
@@ -16,6 +16,6 @@ ALTER TABLE history_octane.config_note
     ADD COLUMN etl_batch_id text;
 
 CREATE INDEX idx_config_note__etl_batch_id ON history_octane.config_note (etl_batch_id);
-CREATE INDEX idx_config_note_comment__etl_batch_id ON history_octane.config_note_monitor (etl_batch_id);
+CREATE INDEX idx_config_note_comment__etl_batch_id ON history_octane.config_note_comment (etl_batch_id);
 CREATE INDEX idx_config_note_monitor__etl_batch_id ON history_octane.config_note_monitor (etl_batch_id);
 CREATE INDEX idx_config_note_scope_type__etl_batch_id ON history_octane.config_note_scope_type (etl_batch_id);
